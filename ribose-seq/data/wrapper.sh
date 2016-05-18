63227dc4bee7169bfd47811aab72233bd01a1fd1
Alli Gombolay <agombolay3@gmail.com>
2016-05-18 16:10:38 -0400

    Update wrapper.sh

--- ribose-seq/data/wrapper.sh
+++ ribose-seq/data/wrapper.sh
@@ -4,10 +4,12 @@
 #This program automatically runs input FASTQ.GZ files through the Ribose-seq analysis pipeline
 #Adapted from Jay Hesselberth's code located at https://github.com/hesselberthlab/modmap/tree/snake
 
-#SPECIFY COMMAND LINE OPTIONS
+#COMMAND LINE OPTIONS
 
+#Name of the program given as the first entry on the commnad-line (i.e., getOptions.sh))
 program=$0
 
+#Usage statement for program; will be displayed to standard output if user specifies "-h" option
 function usage () {
         echo "Usage: $program [-a] 'input1 input2 input3 etc.' [-b] 'basename of Bowtie index' [-h]
           -a Runs ribose-seq pipeline on input .fastq files using Bowtie index 
@@ -15,6 +17,7 @@ function usage () {
           -h Displays help menu describing options"
 }
 
+#Use getOpts function to create command-line options (i.e., "-a", "-b", and "-h")
 while getopts "a:b:h::" opt; do
     case $opt in
         g ) input=($OPTARG) #Specify input as an array
