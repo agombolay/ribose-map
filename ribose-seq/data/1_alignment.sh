e3c9f76bfe69d67791686d404412a0aa37ae712e
Alli Gombolay <agombolay3@gmail.com>
2016-05-18 21:26:02 -0400

    Update 1_alignment.sh

--- ribose-seq/data/1_alignment.sh
+++ ribose-seq/data/1_alignment.sh
@@ -4,10 +4,11 @@
 #Adapted from Jay Hesselberth's code located at https://github.com/hesselberthlab/modmap/tree/snake
 #This program removes UMI's from sequencing reads, aligns reads to reference genome, and de-duplicates reads.
 
+#LOCATE INPUT FASTQ FILES
 #Ask the user the path to the input .fastq files
 echo "What is the filepath to the input .fastq files?:"
 
-#"path" is the variable representing the user's answer
+#"directory" is the variable representing the user's answer
 read directory
 
 for samples in ${fastq[@]}; do
