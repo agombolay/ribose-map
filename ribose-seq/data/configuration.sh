7d6a7c20e0f7b6b72202388511be38f062c60772
Alli Gombolay <agombolay3@gmail.com>
2016-05-18 21:20:14 -0400

    Update configuration.sh

--- ribose-seq/data/configuration.sh
+++ ribose-seq/data/configuration.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
 
+#Ask the user the path to the input .fastq files
+echo "What is the filepath to the input .fastq files?:"
 
+#"path" is the variable representing the user's answer
+read path
