5150f64f2b0b398c111795ff70bc88793b53f57a
Alli Gombolay <agombolay3@gmail.com>
2016-05-18 21:37:15 -0400

    Create 2_alignmentCoverage.sh

---
+++ ribose-seq/scripts/2_alignmentCoverage.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+#Author: Alli Gombolay
+#This script calculates the genome coverage from the alignment results.
+#Adapted from Jay Hesselberth's code located at https://github.com/hesselberthlab/modmap/tree/snake
+
+#INPUT
+#chromosomeSizes=$HOME/data/ribose-seq/data/reference/sacCer2.chrom.sizes
+chromosomeSizes=$HOME/data/ribose-seq/data/sacCer2.chrom.sizes
+finalBAM=$HOME/data/ribose-seq/results/$sample/alignment/$sample.final.bam
+
+#OUTPUT BEDGRAPH FILES
+bedGraphBothStrands=$HOME/data/ribose-seq/results/FS1/bedGraphs/$sample.bothStrands.coverage.bg
+bedGraphPositiveStrand=$HOME/data/ribose-seq/results/FS1/bedGraphs/$sample.positiveStrand.coverage.bg
+bedGraphNegativeStrand=$HOME/data/ribose-seq/results/FS1/bedGraphs/$sample.negativeStrand.coverage.bg
+
+#CALCULATE GENOME COVERAGE
+bedtools genomecov -5 -bg -ibam $finalBAM > $bedGraphBothStrands
+bedtools genomecov -5 -strand + -bg -ibam $finalBAM  > $bedGraphPositiveStrand
+bedtools genomecov -5 -strand - -bg -ibam $finalBAM > $bedGraphNegativeStrand
+
+#Explanation of options used in step above:
+#"-5": Calculate coverage of only 5’ positions
+#"-bg": Report coverage in bedGraph file format
+#"-strand": Calculate coverage of + or - strand
+#"-ibam": Specify input file as BAM file format
+
+echo "Calculation of genome coverage from alignment results is complete"
