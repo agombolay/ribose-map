9a02b951468a286c6bbe6880d5dce56f4dfafef7
Alli Gombolay <agombolay3@gmail.com>
2016-05-18 21:24:45 -0400

    Delete configuration.sh

--- ribose-seq/data/configuration.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-#Ask the user the path to the input .fastq files
-echo "What is the filepath to the input .fastq files?:"
-
-#"path" is the variable representing the user's answer
-read directory
