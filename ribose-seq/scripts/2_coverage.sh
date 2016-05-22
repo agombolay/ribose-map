148329e438987c3985dafd4144589fce2d61f53a
Alli Gombolay <agombolay3@gmail.com>
2016-05-22 17:52:50 -0400

    Update 2_coverage.sh

--- ribose-seq/scripts/2_coverage.sh
+++ ribose-seq/scripts/2_coverage.sh
@@ -4,18 +4,51 @@
 #This script calculates the genome coverage based on the alignment results from 1_alignment.sh
 #Adapted from Jay Hesselberth's code located at https://github.com/hesselberthlab/modmap/tree/snake
 
+#COMMAND LINE OPTIONS
+
+#Name of the program (1_alignment.sh)
+program=$0
+
+#Usage statement of the program
+function usage () {
+        echo "Usage: $program [-i] 'sample1 sample2 sample3 etc.' [-r] 'reference' [-d] 'directory' [-h]
+          -i Sample names of input BAM files (i.e, FS1 for FS1.bam)
+          -r File containing sizes in base pairs of chromosomes
+          -d Location of user's local Ribose-seq directory"
+}
+
+#Use getopts function to create the command-line options ([-a], [-b], [-o], and [-h])
+while getopts "a:r:d:h" opt;
+do
+    case $opt in
+        #Specify input as arrays to allow multiple input arguments
+        i ) list=($OPTARG) ;;
+	#Specify input as variable to allow only one input argument
+	r ) reference=$OPTARG ;;
+	d ) directory=$OPTARG ;;
+        #If user specifies [-h], print usage statement
+        h ) usage ;;
+    esac
+done
+
+#Exit program if user specifies [-h]
+if [ "$1" == "-h" ];
+then
+        exit
+fi
+
 for samples in ${fastq[@]};
 do
 
 	#INPUT
 	#Location of BAM files
-	input=$outputDirectory/ribose-seq/results/$samples/alignment/$samples.bam
+	input=$directory/ribose-seq/results/$samples/alignment/$samples.bam
 	
 	#Location of files containing sizes in base pairs of chromosomes
-	chromosomeSizes=$outputDirectory/ribose-seq/data/$reference.chromosome.sizes
+	chromosomeSizes=$directory/ribose-seq/data/$reference.chromosome.sizes
 	
 	#OUTPUT
-	output=$outputDirectory/ribose-seq/results/$samples/bedgraphs
+	output=$directory/ribose-seq/results/$samples/bedgraphs
 
 	if [[ ! -d $output ]];
 	then
@@ -23,9 +56,9 @@ do
 	fi
 
 	#Location of output bedgraph files containing genome coverage information
-	BothStrands=$outputDirectory/ribose-seq/results/$samples/bedGraphs/$samples.bothStrands.coverage.bg
-	PositiveStrands=$outputDirectory/ribose-seq/results/$samples/bedGraphs/$samples.positiveStrands.coverage.bg
-	NegativeStrands=$outputDirectory/ribose-seq/results/$samples/bedGraphs/$samples.negativeStrands.coverage.bg
+	BothStrands=$directory/ribose-seq/results/$samples/bedGraphs/$samples.bothStrands.coverage.bg
+	PositiveStrands=$directory/ribose-seq/results/$samples/bedGraphs/$samples.positiveStrands.coverage.bg
+	NegativeStrands=$directory/ribose-seq/results/$samples/bedGraphs/$samples.negativeStrands.coverage.bg
 
 	#CALCULATE GENOME COVERAGE
 	bedtools genomecov -ibam $input -g $chromosomeSizes -5 -bg > $BothStrands
