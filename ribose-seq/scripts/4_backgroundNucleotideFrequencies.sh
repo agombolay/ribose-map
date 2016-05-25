94a1477706ffff826f8e7aa4fe69769c9f5c9155
Alli Gombolay <agombolay3@gmail.com>
2016-05-24 22:27:37 -0400

    Update 4_backgroundNucleotideFrequencies.sh

--- ribose-seq/scripts/4_backgroundNucleotideFrequencies.sh
+++ ribose-seq/scripts/4_backgroundNucleotideFrequencies.sh
@@ -9,23 +9,25 @@ fasta=$HOME/data/ribose-seq/data/reference/sacCer2.fa
 
 #OUTPUT
 
+#Location of output "ribose-seq" backgroundNucleotideFrequencies directory
 output="$directory/ribose-seq/results/backgroundNucleotideFrequencies"
 
+#Create output directory if it does not already exist
 if [[ ! -d $output ]];
 then
   mkdir $output
 fi
 
-#CALCULATION OF NUCLEOTIDE FREQUENCIES
+#CALCULATION OF BACKGROUND NUCLEOTIDE FREQUENCIES
 
 #Calculate frequencies of nucleotides in sacCer2 genome
-output="$HOME/data/ribose-seq/results/nucleotideFrequencies/genome.nuc.freqs.tab"
-python2.7 4_nucleotideFrequencies.py $fasta --region-size-minimum 1 --region-size-maximum 3 --verbose > $output
+output1="$output/genome.nucleotide.frequencies.tab"
+python2.7 4_nucleotideFrequencies.py $fasta --region-size-minimum 1 --region-size-maximum 3 --verbose > $output1
 
 #Calculate frequencies of nucleotides in chrM
-output="$HOME/data/ribose-seq/results/nucleotideFrequencies/chrM.nuc.freqs.tab"
-python2.7 4_nucleotideFrequencies.py $fasta --region-size-minimum 1 --region-size-maximum 3 --only-chrom chrM --verbose > $output
+output2="$output/chrM.nucleotide.frequencies.tab"
+python2.7 4_nucleotideFrequencies.py $fasta --region-size-minimum 1 --region-size-maximum 3 --only-chrom chrM --verbose > $output2
 
 #Calculate frequencies of nucleotides in 2micron 
-output="$HOME/data/ribose-seq/results/nucleotideFrequencies/2micron.nuc.freqs.tab"
-python2.7 4_nucleotideFrequencies.py $fasta --region-size-minimum 1 --region-size-maximum 3 --only-chrom 2micron --verbose > $output
+output3="$output/2micron.nucleotide.frequencies.tab"
+python2.7 4_nucleotideFrequencies.py $fasta --region-size-minimum 1 --region-size-maximum 3 --only-chrom 2micron --verbose > $output3
