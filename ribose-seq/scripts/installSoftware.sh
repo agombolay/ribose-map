5d024cb5dde412cb5ecf6dfaa919884516750b77
Alli Gombolay <agombolay3@gmail.com>
2016-05-25 14:44:13 -0400

    Update installSoftware.sh

--- ribose-seq/scripts/installSoftware.sh
+++ ribose-seq/scripts/installSoftware.sh
@@ -1,11 +1,5 @@
-```
 wget https://pypi.python.org/packages/9f/99/a8ac96b357f6b0a6f559fe0f5a81bcae12b98579551620ce07c5183aee2c/MACS2-2.1.1.20160309.tar.gz
-```
 
-```
 tar -zxvf MACS2-2.1.1.20160309.tar.gz
-```
 
-```
 python setup.py install --user
-```
