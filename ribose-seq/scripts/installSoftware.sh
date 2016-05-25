f2eb0eaed2675e96aa812f0c425c78957caad7c6
Alli Gombolay <agombolay3@gmail.com>
2016-05-25 15:29:06 -0400

    Update installSoftware.sh

--- ribose-seq/scripts/installSoftware.sh
+++ ribose-seq/scripts/installSoftware.sh
@@ -1,50 +1,59 @@
 #INSTALL MACS2 PROGRAM TO CALL ALIGNMENT PEAKS
 
-#Download the compressed file from the Python MACS2 website
+#1. Download the compressed file from the Python MACS2 website
 wget https://pypi.python.org/packages/9f/99/a8ac96b357f6b0a6f559fe0f5a81bcae12b98579551620ce07c5183aee2c/MACS2-2.1.1.20160309.tar.gz
 
-#Uncompress the downloaded .tar.gz file
+#2. Uncompress the downloaded .tar.gz file
 tar -zxvf MACS2-2.1.1.20160309.tar.gz
 
-#Install the MACS2 program with Python version 2.7
+#3. Install the MACS2 program with Python version 2.7
 #"--user": Allows user to avoid permission issues
 python2.7 setup.py install --user
 
-#Add the path to the MACS2 script to your $PATH via your .bashrc file
+#4. Add the path to the MACS2 script to your $PATH via your .bashrc file
 echo "export PATH="/projects/home/agombolay3/.local/bin:$PATH"" >> ~/.bashrc
 
 #INSTALL UMITOOLS PROGRAM TO REMOVE PCR DUPLICATES
+
+#Part a.)
 
 #Install the pysam program
 #Pip must already be installed
 pip install pysam
 
+#Part b.
+
 #Install the editdist program
-#Download the compressed file from the editdist website
+
+#1. Download the compressed file from the editdist website
 wget https://py-editdist.googlecode.com/files/py-editdist-0.3.tar.gz
 
-#Uncompress the downloaded .tar.gz file
+#2. Uncompress the downloaded .tar.gz file
 tar xzf py-editdist-0.3.tar.gz
 
-#Change working directory to editdist folder
+#3. Change current directory to editdist folder
 cd py-editdist-0.3/
 
-#Install the editdist program with Python version 2.7
-#"--user": Allows user to avoid permission issues
+#4. Install the editdist program with Python version 2.7
+#. "--user": Allows user to avoid permission issues
 python2.7 setup.py install --user
 
-#Download the compressed file from the umitools website
+# Part c.
+
+#Install the umitools program
+
+#1. Download the compressed file from the umitools website
 wget -O umitools-master.zip https://github.com/brwnj/umitools/archive/master.zip
 
-#Uncompress the downloaded .tar.gz file
+#2. Uncompress the downloaded .tar.gz file
 unzip umitools-master.zip
 
-#Change working directory to umitools folder
+#3. Change current directory to umitools folder
 cd umitools-master
 
-#Install the umitools program with Python version 2.7
+#4. Install the umitools program with Python version 2.7
 #"--user": Allows user to avoid permission issues
 python2.7 setup.py install --user
 
-#Add the path to the umitools script to your $PATH via your .bashrc file
+#5. Add the path to the umitools script to your $PATH via your .bashrc file
 echo "export PATH="/projects/home/agombolay3/.local/bin:$PATH"" >> ~/.bashrc
