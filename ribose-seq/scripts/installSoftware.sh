9007c5584980d265b4cbc64760528e4578e95e5b
Alli Gombolay <agombolay3@gmail.com>
2016-05-25 15:00:35 -0400

    Update installSoftware.sh

--- ribose-seq/scripts/installSoftware.sh
+++ ribose-seq/scripts/installSoftware.sh
@@ -10,5 +10,5 @@ tar -zxvf MACS2-2.1.1.20160309.tar.gz
 #"--user": Allows user to avoid permission issues
 python2.7 setup.py install --user
 
-#Add path to macs2 script to your $PATH via your .bashrc file
+#Add the path to the MACS2 script to your $PATH via your .bashrc file
 echo "export PATH="/projects/home/agombolay3/.local/bin:$PATH"" >> ~/.bashrc
