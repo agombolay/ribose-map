85231bee2b92c2b28bc7a739e905548a2a0e7254
Alli Gombolay <agombolay3@gmail.com>
2016-05-25 14:48:38 -0400

    Update installSoftware.sh

--- ribose-seq/scripts/installSoftware.sh
+++ ribose-seq/scripts/installSoftware.sh
@@ -1,6 +1,11 @@
-#Install MACS2 program to call alignment peaks
+#INSTALL MACS2 PROGRAM TO CALL ALIGNMENT PEAKS
+
+#Download file from MACS2 website
 wget https://pypi.python.org/packages/9f/99/a8ac96b357f6b0a6f559fe0f5a81bcae12b98579551620ce07c5183aee2c/MACS2-2.1.1.20160309.tar.gz
 
+#Uncompress downloaded .tar.gz file
 tar -zxvf MACS2-2.1.1.20160309.tar.gz
 
-python setup.py install --user
+#Install MACS2 program with Python version 2.7
+#"--user": Allows user to avoid permission issues
+python2.7 setup.py install --user
