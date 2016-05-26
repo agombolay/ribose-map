6202743535c77d26f71866b1751069de793a3486
Alli Gombolay <agombolay3@gmail.com>
2016-05-26 12:32:54 -0400

    Update and rename 7_peaksSummaryTables.sh to 7_peaksSummary.sh

--- ribose-seq/scripts/7_peaksSummary.sh
+++ ribose-seq/scripts/7_peaksSummary.sh
@@ -1,29 +1,30 @@
 #! /usr/bin/env bash
 
-output=$directory/ribose-seq/results/peaksSummaryTables
+output=$directory/ribose-seq/results/peaksSummary
 
 if [[ ! -d $output ]]; then
     mkdir $output
 fi
 
 strands=("pos" "neg")
+
 for strand in ${strands[@]}; do
     for align_mode in ${ALIGN_MODES[@]}; do
 
         samples=""
         names=""
         for sample in ${SAMPLES[@]}; do
-            peakdir=$RESULT/$sample/peaks
+            peakdir=$directory/ribose-seq/results/$samples/peaks
             exp_name="$sample.align.$align_mode.strand.$strand"
             peakbed="$peakdir/$exp_name""_peaks.narrowPeak"
             samples="$samples $peakbed"
             names="$names $sample"
         done
 
-        result="$results/peaks_summary_table.align.$align_mode.strand.$strand.tab"
+        result="$output/peaks_summary_table.align.$align_mode.strand.$strand.tab"
 
         bedtools multiinter -i $samples -names $names \
             -g $CHROM_SIZES \
-            > $result
+            > $output
     done
 done
