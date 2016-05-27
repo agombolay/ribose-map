8be4c298755270c6a70a809341ca74fac2461080
Alli Gombolay <agombolay3@gmail.com>
2016-05-27 15:38:32 -0400

    Update 6_calculateNucleotideFrequencies.sh

--- ribose-seq/scripts/6_calculateNucleotideFrequencies.sh
+++ ribose-seq/scripts/6_calculateNucleotideFrequencies.sh
@@ -5,7 +5,7 @@ sizes="1 2 3"
 
 
 ignore_modes=("all" "only-mito" "no-mito" "only-2micron")
-ignore_args=("" "--only-chrom chrM" "--ignore-chrom chrM" "--only-chrom 2micron")
+ignore_arguments=("" "--only-chrom chrM" "--ignore-chrom chrM" "--only-chrom 2micron")
 
 input=$directory/ribose-seq/data/$sample/alignment
 
@@ -15,8 +15,8 @@ if [[ ! -d $output ]]; then
     mkdir -p $output
 fi
 
-offset_min=-100
-offset_max=100
+offset_minimum=-100
+offset_maximum=100
 
 
 BAM=$input/$sample.bam
@@ -25,7 +25,7 @@ for index in ${!ignore_modes[@]}; do
 
         ignore_mode=${ignore_modes[$index]}
         
-        ignore_arg=${ignore_args[$index]}
+        ignore_arguments=${ignore_args[$index]}
 
         tables="$output/$sample.ignore.$ignore_mode.nucleotideFrequencies.tab"
         
@@ -40,18 +40,18 @@ for index in ${!ignore_modes[@]}; do
         
         elif [[ $ignore_mode == "only-2micron" ]];
         then
-            BKGD_FREQS="$RESULT/background_nuc_freqs/2micron.nuc.freqs.tab"
+            BackgroundFrequencies="$RESULT/backgroundNucleotideFrequencies/2micron.nuc.freqs.tab"
         
         else
-            BKGD_FREQS="$RESULT/background_nuc_freqs/genome.nuc.freqs.tab"
+            BackgroundFrequencies="$RESULT/backgroundNucleotideFrequencies/genome.nuc.freqs.tab"
         fi
 
-        #Signals need to be reverse complemented since the sequence is the reverse complement of the captured strand
+        #Signals need to be reverse complemented since sequence is reverse complement of the captured strand
         for size in $sizes;
         do
-            python -m modmap.nuc_frequencies $BAM $FASTA --region-size $size $ignore_arg --revcomp-strand \
-            --background-freq-table $BKGD_FREQS --offset-min $offset_min --offset-max $offset_max --verbose \
-            >> $output
+            python -m modmap.nuc_frequencies $BAM $FASTA --region-size $size $ignore_arguments --revcomp-strand \
+            --background-freq-table $BackgroundFrequencies --offset-min $offset_minimum --offset-max $offset_maximum
+            --verbose >> $output
         done
     
 done
