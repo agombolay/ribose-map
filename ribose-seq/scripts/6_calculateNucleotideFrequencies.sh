5ae38b9a4e6140aaa1c03f960b871e9a6f0532e9
Alli Gombolay <agombolay3@gmail.com>
2016-05-29 16:56:26 -0400

    Update 6_calculateNucleotideFrequencies.sh

--- ribose-seq/scripts/6_calculateNucleotideFrequencies.sh
+++ ribose-seq/scripts/6_calculateNucleotideFrequencies.sh
@@ -4,12 +4,12 @@
 #This program calculates the frequencies of the nucleotides located in the assembled samples.
 #Adapted from Jay Hesselberth's code located at https://github.com/hesselberthlab/modmap/tree/snake
 
-# mononucleotides, dinucleotides, and trinucleotides
+# Mononucleotides, dinucleotides, and trinucleotides
 sizes="1 2 3"
 
-ignore_modes=("all" "only-mito" "no-mito" "only-2micron")
+modes=("all" "only-mito" "no-mito" "only-2micron")
 
-ignore_arguments=("" "--only-chrom chrM" "--ignore-chrom chrM" "--only-chrom 2micron")
+arguments=("" "--only-chrom chrM" "--ignore-chrom chrM" "--only-chrom 2micron")
 
 #""= Entire genome ("all")
 #"--only-chrom chrM"= Only chrM ("only-mito")
@@ -29,25 +29,25 @@ offset_maximum=100
 
 BAM=$input/$sample.bam
 
-for index in ${!ignore_modes[@]};
+for index in ${!modes[@]};
 do
 
-        ignore_mode=${ignore_modes[$index]}
+        mode=${ignore_modes[$index]}
         
-        ignore_arguments=${ignore_args[$index]}
+        arguments=${ignore_args[$index]}
 
-        tables="$output/$sample.ignore.$ignore_mode.nucleotideFrequencies.tab"
+        tables="$output/$sample.$mode.nucleotideFrequencies.tab"
         
         if [[ -f $tables ]];
         then
             rm -f $tables
         fi
 
-        if [[ $ignore_mode == "only-mito" ]];
+        if [[ $mode == "only-mito" ]];
         then
             BackgroundFrequencies="$output/backgroundNucleotideFrequencies/chrM.nucleotide.frequencies.tab"
         
-        elif [[ $ignore_mode == "only-2micron" ]];
+        elif [[ $mode == "only-2micron" ]];
         then
             BackgroundFrequencies="$output/backgroundNucleotideFrequencies/2micron.nucleotide.frequencies.tab"
         
@@ -58,7 +58,7 @@ do
         #Signals need to be reverse complemented since sequence is reverse complement of the captured strand
         for size in $sizes;
         do
-            python -m modmap.nuc_frequencies $BAM $FASTA --region-size $size $ignore_arguments --revcomp-strand \
+            python -m modmap.nuc_frequencies $BAM $FASTA --region-size $size $arguments --revcomp-strand \
             --background-freq-table $BackgroundFrequencies --offset-min $offset_minimum --offset-max $offset_maximum \
             --verbose >> $output
         done
