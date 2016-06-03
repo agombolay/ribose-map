5c949cdddd3cb0cb0ca1b844aefc1ccd00698150
Alli Gombolay <agombolay3@gmail.com>
2016-06-02 22:12:58 -0400

    Update 6_plotNucleotideFrequencies.sh

--- ribose-seq/scripts/6_plotNucleotideFrequencies.sh
+++ ribose-seq/scripts/6_plotNucleotideFrequencies.sh
@@ -58,7 +58,7 @@ do
     
     sampleID="$samples.subset-$mode"
     
-    tables="$input/$samples.$mode.nucleotideFrequencies_normalized.tab"
+    tables="$input/$samples.$mode.nucleotideFrequencies.tab"
     
     Rscript 6_plotNucleotideFrequencies.R -n "$sampleID" -d $output --offsetmax $value $tables
     
