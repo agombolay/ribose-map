330084d7be7ea2882330b223a0b8c261c339822e
Alli Gombolay <agombolay3@gmail.com>
2016-06-09 11:52:03 -0400

    Update 1_setUp.sh

--- ribose-seq/scripts/1_setUp.sh
+++ ribose-seq/scripts/1_setUp.sh
@@ -16,7 +16,7 @@ twoBitToFa sacCer2.2bit sacCer2.fa
 bowtie-build sacCer2.fa sacCer2Index
 
 #Download file of chromosome sizes (in bp) the reference genome from UCSC's site
-#Note: fetchChromSizes is an alternative UCSC program to download chrom.sizes file
+#Note: fetchChromSizes is a UCSC program that can also be used to create the file
 wget http://hgdownload.cse.ucsc.edu/goldenPath/sacCer2/bigZips/sacCer2.chrom.sizes
 
 #Sort the reference genome file for processing
