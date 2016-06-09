c88fbaab59cbb6ca6f1081ec734fb2beeee958ff
Alli Gombolay <agombolay3@gmail.com>
2016-06-09 11:10:26 -0400

    Update 1_setUp.sh

--- ribose-seq/scripts/1_setUp.sh
+++ ribose-seq/scripts/1_setUp.sh
@@ -3,19 +3,18 @@
 #Author: Alli Gombolay 
 #Script to download reference genome (sacCer2) files
 
-#Download twoBitToFa program to convert .2bit file to .fa
-#This program is necessary to convert sacCer2.2bit to .fa format
+#Download twoBitToFa program to convert twoBit (.2bit) files to FASTA (.fa)
+#This program is necessary to convert the reference genome sequence file to .fa
 wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/twoBitToFa
 
-#Download the sacCer2 reference genome (Chromosomes I-XVI, ChrM, and 2micron plasmid)
-#sacCer2.2bit contains the sequences of all 16 chromosomes, chr M, and 2micron plasmid
+#Download the reference genome sequence .2bit file from the UCSC Genome Browser
+#sacCer2.2bit contains the sequences of Chromosomes I-XVI, ChrM, and 2micron plasmid
 wget http://hgdownload.cse.ucsc.edu/goldenPath/sacCer2/bigZips/sacCer2.2bit
 
-#Run twoBitToFa program to convert file from twoBit to FASTA format
-#sacCer2.2bit file contains the genomic sequence of the reference, sacCer2
+#Run twoBitToFa program to convert reference genome sequence file from .2bit to .fa
 twoBitToFa sacCer2.2bit sacCer2.fa
 
-#Build Bowtie index for sacCer2
+#Build the Bowtie index for the reference genome from the output .fa file
 bowtie-build sacCer2.fa sacCer2Index
 
 #Download file containing sizes of chromosomes of sacCer2 genome
