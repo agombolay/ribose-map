42fd7cbb496bbdb22e1c86b7bcd560035055a62d
Alli Gombolay <agombolay3@gmail.com>
2016-06-09 11:59:14 -0400

    Update 1_setUp.sh

--- ribose-seq/scripts/1_setUp.sh
+++ ribose-seq/scripts/1_setUp.sh
@@ -3,7 +3,40 @@
 #Author: Alli Gombolay 
 #Script to download the reference genome files required for the Ribose-seq Analysis Pipeline
 
-#Download .2bit file of the reference genome sequence from UCSC's site
+#COMMAND LINE OPTIONS
+
+#Name of the program (2_alignment.sh)
+program=$0
+
+#Usage statement of the program
+function usage () {
+        echo "Usage: $program [-i] '/path/to/file1.fastq etc.' [-b] 'Bowtie index' [-d] 'Ribose-seq directory' [-h]
+          -i Filepaths of input FASTQ files 
+          -b Basename of Bowtie index to be searched
+          -d Location to save local Ribose-seq directory"
+}
+
+#Use getopts function to create the command-line options ([-i], [-b], [-d], and [-h])
+while getopts "i:b:d:h" opt;
+do
+    case $opt in
+        #Specify input as arrays to allow multiple input arguments
+        i ) fastq=($OPTARG) ;;
+	#Specify input as variable to allow only one input argument
+	b ) index=$OPTARG ;;
+	d ) directory=$OPTARG ;;
+        #If user specifies [-h], print usage statement
+        h ) usage ;;
+    esac
+done
+
+#Exit program if user specifies [-h]
+if [ "$1" == "-h" ];
+then
+        exit
+fi
+
+#Download twoBit file of the reference genome sequence from UCSC's site
 wget http://hgdownload.cse.ucsc.edu/goldenPath/sacCer2/bigZips/sacCer2.2bit
 
 #Convert the reference genome sequence file from .2bit to .fa
