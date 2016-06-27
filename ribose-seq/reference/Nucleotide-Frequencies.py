fba414f9ae7cbd4b7daf05c486b336b330eef272
Alli Gombolay <agombolay3@highmemvannberg.(none)>
2016-06-27 01:42:35 -0400

    update program

--- ribose-seq/reference/Nucleotide-Frequencies.py
+++ ribose-seq/reference/Nucleotide-Frequencies.py
@@ -1,11 +1,13 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 
 #Author: Alli Gombolay
 #This program calculates the nucleotide frequencies of an input FASTA file
 
+#Import Python modules
 import sys
 import os
 import argparse
+import tablib
 
 #Use argparse function to create the "help" command-line option ([-h])
 parser = argparse.ArgumentParser()
@@ -15,23 +17,17 @@ args = parser.parse_args()
 #Open input FASTA file and assign it to an object ("r": read file)
 fasta = open(sys.argv[1], "r")
 
-#Set the values of the base counts of nucleotide frequencies to 0
+#Set the values of the base counts of nucleotide numbers to 0
 A=0;
 C=0;
 G=0;
 T=0;
-
-#If necessary, skip the first line (description line) of FASTA file
-#fasta.readline()
 
 for line in fasta:
     
 	#Skip lines in file that start with ">" symbol (i.e., >2micron)
 	if ">" in line:
 		continue
-
-	#Optional: Convert letters in file lowercase for consistency
-	#line = line.lower()
 
 	for character in line:
 		if character == "A":
@@ -46,32 +42,34 @@ for line in fasta:
 #Calculate total number of nucleotides
 total = (A+C+G+T)
 
+#Calculate frequency of each nucleotide
+A_frequency = float(A)/total
+C_frequency = float(C)/total 
+G_frequency = float(G)/total
+T_frequency = float(T)/total
+
+#CREATE EXCEL FILE
+
+data = tablib.Dataset()
+
+#Add data values to Excel file
+data.append_col(['A', 'C', 'G', 'T'], header='Nucleotide')
+data.append_col([A, C, G, T], header='Number of Occurrences')
+data.append_col([A_frequency, C_frequency, G_frequency, T_frequency], header='Frequency')
+data.append_col([total, '     ', '     ', '     '], header='Total Number of Nucleotides')
+
+#Add column headers to Excel file
+data.headers = ['Nucleotide', 'Number', 'Frequency', 'Total Number of Nucleotides']
+
+#NAME EXCEL FILE
+
 #Obtain name of FASTA file without .fa file extension
 filename=os.path.splitext(sys.argv[1])[0]
 
 #Specify name of output file based on input filename
-output=filename+str('.txt')
+output=filename+str('.xls')
 
-#Redirect output to .txt file (i.e., 2micron.txt)
+#Redirect output to .xls file
 sys.stdout=open(output, "w")
 
-#Convert freqeuncies into strings so they can be printed
-print "Number of A's: " + str(A)
-print "Number of C's: " + str(C)
-print "Number of G's: " + str(G)
-print "Number of T's: " + str(T)
-
-print "Total number of nucleotides: " + str(total)
-
-#Calculate ratio of G's and C's to total base content
-#0. = Convert number to a floating point decimal number
-#gc = (G+C+0.) / (A+C+G+T+0.)
-
-#Print GC content value
-#print "GC content: " + str(gc)
-
-print "Frequency of A's: " + str(float(A)/total)
-print "Frequency of C's: " + str(float(C)/total)
-print "Frequency of G's: " + str(float(G)/total)
-print "Frequency of T's: " + str(float(T)/total)
-
+print data.xls
