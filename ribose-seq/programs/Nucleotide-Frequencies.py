26058e5763770d7f3556c8d3a5e4818c51a1812f
Alli Gombolay <agombolay3@gmail.com>
2016-07-19 12:36:20 -0400

    Update Nucleotide-Frequencies.py

--- ribose-seq/programs/Nucleotide-Frequencies.py
+++ ribose-seq/programs/Nucleotide-Frequencies.py
@@ -70,7 +70,7 @@ T_frequency = float(T)/total
 
 #READ EXCEL FILE
 
-background_frequencies = "%s/ribose-seq/results/Background-Nucleotide-Frequencies/%s.Nucleotide.Frequencies.xls" % (directory1, reference)
+background_frequencies = "%s/ribose-seq/results/Background-Nucleotide-Frequencies/%s.Nucleotide.Frequencies.xls" % (directory, reference)
 workbook1 = xlrd.open_workbook(background_frequencies)
 sheet1 = workbook1.sheet_by_index(0)
 
