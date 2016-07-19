1d227851dcd0bfb431e1cfeb56e3258afbe6f8c1
Alli Gombolay <agombolay3@highmemvannberg.localdomain>
2016-07-18 22:39:45 -0400

    add file

---
+++ ribose-seq/programs/Columns.sh
@@ -0,0 +1,79 @@
+#! /usr/bin/env bash
+
+#Author: Alli Gombolay
+#Date: July 18, 2016
+#This program outputs upstream and downstream flanking sequences into tabular format for processing
+
+#COMMAND LINE OPTIONS
+
+#Name of the program (Columns.sh)
+program=$0
+
+#Usage statement of the program
+function usage () {
+	echo "Usage: $program [-i] '/path/to/file1.tab etc.' [-l] 'location' [-r] 'reference' [-d] 'Ribose-seq directory' [-h]
+	-i Filepaths of input tab files
+	-l Upstream or Downstream location
+	-r Reference genome of interest (i.e., sacCer2)
+	-d Location of user's local Ribose-seq directory"
+}
+
+#Use getopts function to create the command-line options ([-i], [-l], [-r], [-d], and [-h])
+while getopts "i:l:r:d:h" opt;
+do
+    case $opt in
+        #Specify input as arrays to allow multiple input arguments
+        i ) tab=($OPTARG) ;;
+	#Specify input as variable to allow only one input argument
+        l ) location=$OPTARG ;;
+	r ) reference=$OPTARG ;;
+	d ) directory=$OPTARG ;;
+        #If user specifies [-h], print usage statement
+        h ) usage ;;
+    esac
+done
+
+#Exit program if user specifies [-h]
+if [ "$1" == "-h" ];
+then
+        exit
+fi
+
+for samples in ${tab[@]};
+do
+
+	#Extract sample names from filepaths
+        filename=$(basename "${tab}")
+        samples="${filename%.flanking.*}"
+
+        #Extract input directories from filepaths
+        inputDirectory=$(dirname "${tab}")
+
+        #INPUT
+	#Location of input tab files
+	input=$inputDirectory/$samples.flanking.$location.sequences.tab
+
+	#OUTPUT
+	#Location of output "ribose-seq" Columns directory
+	output=$directory/ribose-seq/results/$reference/$samples/nucleotideFrequencies/Nucleotides/Columns
+
+	if [[ ! -d $output ]]; then
+    		mkdir -p $output
+	fi
+
+	#Location of output files
+	sequences=$output/$samples.trimmed.$location.sequences.raw.txt
+	columns=$output/$samples.trimmed.$location.sequences.columns.txt
+
+	#Print sequences to new file
+	awk -v "OFS=\t" '{print $2}' $input > $sequences
+
+	#Insert tabs between each nucleotide
+	cat $sequences | sed 's/.../& /2g;s/./& /g' > $columns
+
+		for i in {1..100};
+		do
+			awk -v field=$i '{ print $field }' $columns > $output/column.$i.$location.txt
+		done
+done
+
