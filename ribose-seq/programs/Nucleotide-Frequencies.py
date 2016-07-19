a26b8731ab845f20696536b5bbc8498bb0928b6e
Alli Gombolay <agombolay3@gmail.com>
2016-07-19 13:54:34 -0400

    Update Nucleotide-Frequencies.py

--- ribose-seq/programs/Nucleotide-Frequencies.py
+++ ribose-seq/programs/Nucleotide-Frequencies.py
@@ -114,7 +114,7 @@ output2=path+folder+filename+str('.Nucleotide.Frequencies.xls')
 sys.stdout=open(output1, "w")
 
 #Specify header names and table style
-print tabulate(table, tablefmt="simple", floatfmt=".4f")
+print tabulate(table, tablefmt="plain", floatfmt=".4f")
 
 #Save table to .xls file
 workbook2.save(output2)
