5b9f590d7a616ee225bb3fa8d178e2a7c12b1c42
Alli Gombolay <agombolay3@highmemvannberg.localdomain>
2016-07-20 00:32:26 -0400

    updates

--- ribose-seq/programs/Nucleotide-Frequencies.sh
+++ ribose-seq/programs/Nucleotide-Frequencies.sh
@@ -1,7 +1,55 @@
-background="/projects/home/agombolay3/data/repository/Ribose-seq-Project/ribose-seq/reference/sacCer2.fa"
-nucleotides="/projects/home/agombolay3/data/repository/Ribose-seq-Project/ribose-seq/results/sacCer2/FS15.trimmed/Nucleotide-Frequencies/Nucleotides/Columns/nuclear/upstream/*.txt"
+#background="/projects/home/agombolay3/data/repository/Ribose-seq-Project/ribose-seq/reference/sacCer2.fa"
+#nucleotides="/projects/home/agombolay3/data/repository/Ribose-seq-Project/ribose-seq/results/sacCer2/FS15.trimmed/Nucleotide-Frequencies/Nucleotides/Columns/nuclear/upstream/*.txt"
+#!/usr/bin/env bash
 
-for file in $background;
+#Author: Alli Gombolay
+#This program calculates the frequencies of +/- 100 downstream/upstream nucleotides from ribonucleotides 
+
+#COMMAND LINE OPTIONS
+
+#Name of the program (Nucleotide-Frequencies.sh)
+program=$0
+
+#Usage statement of the program
+function usage () {
+	echo "Usage: $program [-i] '/path/to/*.txt etc.' [-s] 'sample' [-r] 'reference' [-d] 'Ribose-seq directory' [-h]
+	-i Filepath of input text files
+	-s Sample of interest (i.e., FS15.trimmed)
+	-r Reference genome of interest (i.e., sacCer2)
+	-d Location of user's local Ribose-seq directory"
+}
+
+#Use getopts function to create the command-line options ([-i], [-b], [-d], and [-h])
+while getopts "i:s:r:d:h" opt;
+do
+    case $opt in
+        #Specify input as arrays to allow multiple input arguments
+        i ) txt=($OPTARG) ;;
+	#Specify input as variable to allow only one input argument
+	s ) sample=$OPTARG ;;
+	r ) reference=$OPTARG ;;
+	d ) directory=$OPTARG ;;
+        #If user specifies [-h], print usage statement
+        h ) usage ;;
+    esac
+done
+
+#Exit program if user specifies [-h]
+if [ "$1" == "-h" ];
+then
+        exit
+fi
+
+#INPUT
+#Location of FASTA file
+fasta=$directory/ribose-seq/reference/$reference.fa
+echo $fasta
+#OUTPUT
+#Location of output directory
+output1=$directory/ribose-seq/results/Background-Nucleotide-Frequencies
+output2=$directory/ribose-seq/results/$reference/$sample/Nucleotide-Frequencies/Nucleotides
+
+for file in $fasta;
 do
   	A=$(grep -v '>' $file | grep -o 'A' - | wc -l)
         C=$(grep -v '>' $file | grep -o 'C' - | wc -l)
@@ -15,14 +63,14 @@ do
         G_background_frequency=$(bc <<< "scale = 4; `expr $G/$total`")
         T_background_frequency=$(bc <<< "scale = 4; `expr $T/$total`")
 	
-	echo "A Background Frequency: $A_background_frequency" >> background.txt
-	echo "C Background Frequency: $C_background_frequency" >> background.txt
-	echo "G Background Frequency: $G_background_frequency" >> background.txt
-	echo "T Background Frequency: $T_background_frequency" >> background.txt
+	echo "A Background Frequency: $A_background_frequency" >> $output1/Background-Frequencies.txt
+	echo "C Background Frequency: $C_background_frequency" >> $output1/Background-Frequencies.txt
+	echo "G Background Frequency: $G_background_frequency" >> $output1/Background-Frequencies.txt
+	echo "T Background Frequency: $T_background_frequency" >> $output1/Background-Frequencies.txt
 
 done
 
-for file in $nucleotides;
+for file in ${txt[@]};
 do
 	A=$(grep -v '>' $file | grep -o 'A' - | wc -l)
 	C=$(grep -v '>' $file | grep -o 'C' - | wc -l)
@@ -41,11 +89,11 @@ do
         G_normalized_frequency=$(bc <<< "scale = 4; `expr $G_frequency/$G_background_frequency`")
         T_normalized_frequency=$(bc <<< "scale = 4; `expr $T_frequency/$T_background_frequency`")
 
-	echo $A_normalized_frequency >> A_normalized_frequencies.txt
-	echo $C_normalized_frequency >> C_normalized_frequencies.txt
-	echo $G_normalized_frequency >> G_normalized_frequencies.txt
-	echo $T_normalized_frequency >> T_normalized_frequencies.txt
+	echo $A_normalized_frequency >> $output2/A_normalized_frequencies.txt
+	echo $C_normalized_frequency >> $output2/C_normalized_frequencies.txt
+	echo $G_normalized_frequency >> $output2/G_normalized_frequencies.txt
+	echo $T_normalized_frequency >> $output2/T_normalized_frequencies.txt
 
-	paste A_normalized_frequencies.txt C_normalized_frequencies.txt G_normalized_frequencies.txt T_normalized_frequencies.txt > normalized_frequencies.txt
+	paste $output2/A_normalized_frequencies.txt $output2/C_normalized_frequencies.txt $output2/G_normalized_frequencies.txt $output2/T_normalized_frequencies.txt > $output2/Normalized-Frequencies.txt
 
 done
