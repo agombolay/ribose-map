a7e6ad651792016fddbf92ad33f9368b8fb33706
Alli Gombolay <agombolay3@gmail.com>
2016-07-22 01:22:17 -0400

    Update Ribonucleotide-Frequencies.py

--- ribose-seq/programs/Ribonucleotide-Frequencies.py
+++ ribose-seq/programs/Ribonucleotide-Frequencies.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 #Author: Alli Gombolay
-#This program calculates the ribonucleotide frequencies located at 5' position of input BED file
+#This program calculates the ribonucleotide frequencies located at 3' position of input BED file
 
 #Import Python modules
 import sys
@@ -84,7 +84,7 @@ for line in bed:
                 print(line.split()[4])[:1],
 		print(line.split()[5])
 
-#Redirect standard output to file of list of 5' nucleotides
+#Redirect standard output to file of list of 3' nucleotides
 sys.stdout = standard_output
 
 #Close the file
@@ -93,7 +93,7 @@ list1.close()
 #Open list and assign it to a different object ("r": read file)
 list2 = open(list, 'r')
 
-#Set the values of the base counts of 5' nucleotide numbers to 0
+#Set the values of the base counts of 3' nucleotide numbers to 0
 A=0;
 C=0;
 G=0;
