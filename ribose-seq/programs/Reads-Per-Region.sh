878746588904b8e55ae6fd7bd654d264ae21f76a
Alli Gombolay <agombolay3@gmail.com>
2016-08-02 14:55:55 -0400

    Update Reads-Per-Region.sh

--- ribose-seq/programs/Reads-Per-Region.sh
+++ ribose-seq/programs/Reads-Per-Region.sh
@@ -2,7 +2,7 @@
 
 #Author: Alli Gombolay
 #Date: August 2, 2016
-#This program extracts number of reads per chromosome and saves output to text file in same location as bam file
+#This program extracts number of reads per chromosome and saves output to text file in same location as input bam file
 
 #COMMAND LINE OPTIONS
 
