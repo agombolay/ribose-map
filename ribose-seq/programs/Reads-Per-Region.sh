d4db87f88b85744c25261989074b0b857ebfe6e0
Alli Gombolay <agombolay3@highmemvannberg.localdomain>
2016-08-02 14:48:56 -0400

    add program

---
+++ ribose-seq/programs/Reads-Per-Region.sh
@@ -0,0 +1,56 @@
+#! /usr/bin/env bash
+
+#Author: Alli Gombolay
+#Date: August 2, 2016
+#This program extracts the number of regions per chromosome
+
+#COMMAND LINE OPTIONS
+
+#Name of the program (Reads-Per-Region.sh)
+program=$0
+
+#Usage statement of the program
+function usage () {
+	echo "Usage: $program [-i] '/path/to/file1.bam etc.' [-d] 'Ribose-seq directory' [-h]
+	-i Filepaths of input bam file(s)
+	-d Location of user's local Ribose-seq directory"
+}
+
+#Use getopts function to create the command-line options ([-i], [-d], and [-h])
+while getopts "i:d:h" opt;
+do
+    case $opt in
+        #Specify input as arrays to allow multiple input arguments
+        i ) bam=($OPTARG) ;;
+	#Specify input as variable to allow only one input argument
+	d ) directory=$OPTARG ;;
+        #If user specifies [-h], print usage statement
+        h ) usage ;;
+    esac
+done
+
+#Exit program if user specifies [-h]
+if [ "$1" == "-h" ];
+then
+        exit
+fi
+
+for samples in ${bam[@]};
+do
+	#Extract sample names from filepaths
+	filename=$(basename "${samples}")
+	samples="${filename%.*}"
+	
+	#Extract input directories from filepaths
+	inputDirectory=$(dirname "${bam}")
+	
+	#INPUT
+	#Location of input files
+	input=$inputDirectory/$samples.bam
+
+	#OUTPUT
+	#Location of output files
+	output=$inputDirectory/$samples.Reads-Per-Region.txt
+
+	samtools idxstats $samples.bam | cut -f 1,3 > $output
+done
