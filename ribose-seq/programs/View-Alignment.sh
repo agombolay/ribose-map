c04519d0b4a79e1401b5db70dfe3ee234c63aea6
Alli Gombolay <agombolay3@highmemvannberg.localdomain>
2016-08-08 15:16:20 -0400

    updates

--- ribose-seq/programs/View-Alignment.sh
+++ ribose-seq/programs/View-Alignment.sh
@@ -11,11 +11,11 @@ program=$0
 
 #Usage statement of the program
 function usage () {
-	echo "Usage: $program [-i] '/path/to/file1.bam etc.' [-c] 'chromosome' [-p] 'nucleotide position' [-r] '/path/to/reference.fa' [-h]
-	-i Input (i.e., /projects/home/agombolay3/data/repository/Ribose-seq-Project/ribose-seq/results/hg38/FS56/Alignment/FS56.bam)
+	echo "Usage: $program [-i] 'BAM' [-c] 'Chromosome' [-p] 'Position' [-r] Reference [-h]
+	-i Input ('/path/to/file1.bam etc.')
 	-c Chromosome of interest (i.e., chr1)
 	-p Nucleotide position of interest (i.e., 95)
-	-r Reference genome sequence file in FASTA format"
+	-r Reference FASTA ('/path/to/reference.fa' etc.)"
 }
 
 #Use getopts function to create the command-line options ([-i], [-c], [-p], [-r], and [-h])
