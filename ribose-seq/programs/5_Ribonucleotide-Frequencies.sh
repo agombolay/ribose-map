56086470645b809cb3571cab50be68e953d86220
Alli Gombolay <agombolay3@gmail.com>
2016-09-11 22:43:22 -0400

    Update 5_Ribonucleotide-Frequencies.sh

--- ribose-seq/programs/5_Ribonucleotide-Frequencies.sh
+++ ribose-seq/programs/5_Ribonucleotide-Frequencies.sh
@@ -56,10 +56,10 @@ fi
 #Print only ribonucleotides (3' end of read (end for + strand and start for - strand)) to output file
 
 #Print ribonucleotides for positive strands (located at end of sequence)
-awk '$2 == "+" {print substr($0,length($0)-2)}' $list > Ribonucleotide_List.$subset.txt
+#awk '$2 == "+" {print substr($0,length($0)-2)}' $list > Ribonucleotide_List.$subset.txt
 
 #Print ribonucleotides for negative strands (located at beginning of sequence)
-awk -v "OFS=\t" '$2 == "-" {print substr($0,0,1), $2}' $list >> Ribonucleotide_List.$subset.txt
+#awk -v "OFS=\t" '$2 == "-" {print substr($0,0,1), $2}' $list >> Ribonucleotide_List.$subset.txt
 
 #Calculate count of "A" ribonucleotides
 A_ribonucleotide_count=$(awk '$1 == "A" && $2 == "+" || $1 == "T" && $2 == "-" {print $1, $2}' Ribonucleotide_List.$subset.txt | wc -l)
