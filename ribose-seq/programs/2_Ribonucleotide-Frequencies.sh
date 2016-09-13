21f7e72a1d7c6918a3a9b61066b32dddf87843c1
Alli Gombolay <agombolay3@gmail.com>
2016-09-13 00:20:09 -0400

    Update 2_Ribonucleotide-Frequencies.sh

--- ribose-seq/programs/2_Ribonucleotide-Frequencies.sh
+++ ribose-seq/programs/2_Ribonucleotide-Frequencies.sh
@@ -197,10 +197,13 @@ C_normalized_ribonucleotide_frequency=$(bc <<< "scale = 4; `expr $C_ribonucleoti
 G_normalized_ribonucleotide_frequency=$(bc <<< "scale = 4; `expr $G_ribonucleotide_frequency/$G_background_frequency`")
 U_normalized_ribonucleotide_frequency=$(bc <<< "scale = 4; `expr $U_ribonucleotide_frequency/$T_background_frequency`")
 
-echo $A_normalized_ribonucleotide_frequency" >> $output1/$sample.$reference.$subset.Ribonucleotide-Frequencies.txt
-echo $C_normalized_ribonucleotide_frequency" >> $output1/$sample.$reference.$subset.Ribonucleotide-Frequencies.txt
-echo $G_normalized_ribonucleotide_frequency" >> $output1/$sample.$reference.$subset.Ribonucleotide-Frequencies.txt
-echo $U_normalized_ribonucleotide_frequency" >> $output1/$sample.$reference.$subset.Ribonucleotide-Frequencies.txt
+#echo $A_normalized_ribonucleotide_frequency" >> $output1/$sample.$reference.$subset.Ribonucleotide-Frequencies.txt
+#echo $C_normalized_ribonucleotide_frequency" >> $output1/$sample.$reference.$subset.Ribonucleotide-Frequencies.txt
+#echo $G_normalized_ribonucleotide_frequency" >> $output1/$sample.$reference.$subset.Ribonucleotide-Frequencies.txt
+#echo $U_normalized_ribonucleotide_frequency" >> $output1/$sample.$reference.$subset.Ribonucleotide-Frequencies.txt
+
+echo "$A_normalized_ribonucleotide_frequency $C_normalized_ribonucleotide_frequency $G_normalized_ribonucleotide_frequency \
+$U_normalized_ribonucleotide_frequency" | column  > $output1/$sample.$reference.$subset.Ribonucleotide-Frequencies.txt
 
 rm temporary.txt
 
