311176ffaf04305b11ee55ec112e344239ef0c0a
Alli Gombolay <agombolay3@gmail.com>
2016-10-08 16:34:42 -0400

    Update 2_Nucleotide-Frequencies.sh

--- ribose-seq/programs/2_Nucleotide-Frequencies.sh
+++ ribose-seq/programs/2_Nucleotide-Frequencies.sh
@@ -125,10 +125,10 @@ for sample in ${sample[@]}; do
 	G_backgroundCount=$(grep -v '>' $referenceFasta1 | grep -o 'G' - | wc -l)
 	T_backgroundCount=$(grep -v '>' $referenceFasta1 | grep -o 'T' - | wc -l)
 
-	echo $A_backgroundCount"
-	echo $C_backgroundCount"
-	echo $G_backgroundCount"
-	echo $T_backgroundCount"
+	echo $A_backgroundCount
+	echo $C_backgroundCount
+	echo $G_backgroundCount
+	echo $T_backgroundCount
 	
 	#Calculate total number of dNTPs
 	total_backgroundCount=$(($A_backgroundCount+$C_backgroundCount+$G_backgroundCount+$T_backgroundCount))
