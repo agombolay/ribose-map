a41de03e0d1a7e023be5eea8a201e2b4a82adc1a
Alli Gombolay <agombolay3@gmail.com>
2016-11-10 11:13:22 -0500

    Update poisson.sh

--- ribose-seq/programs/poisson.sh
+++ ribose-seq/programs/poisson.sh
@@ -34,10 +34,10 @@ if [ "$1" == "-h" ]; then
 fi
 
 #Separate genome into 2.5 kb windows
-bedtools makewindows -g /projects/home/agombolay3/data/repository/Ribose-seq-Project/ribose-seq/reference/sacCer2.bed -w 2500 > sacCer2.windows.bed
+bedtools makewindows -g /projects/home/agombolay3/data/repository/Ribose-seq-Project/ribose-seq/reference/$reference.bed -w 2500 > $reference.windows.bed
 
 #Sort BED file of rNMP coordinates (sort first and second columns of file)
-sort -k1,1 -k2,2n FS15.trimmed.v1.rNMP-coordinates.0-based.nuclear.txt > FS15.sorted.bed
+sort -k1,1 -k2,2n $sample.rNMP-coordinates.0-based.$subset.bed > $sample.rNMP-coordinates.0-based.$subset.sorted.bed
 
 #Determine intersect regions of the two BED files and then bin data
-bedtools intersect -a sacCer2.windows.bed -b FS15.sorted.bed -c -sorted -nonamecheck > binned.data.txt
+bedtools intersect -a $reference.windows.bed -b $sample.rNMP-coordinates.0-based.$subset.sorted.bed -c -sorted -nonamecheck > $sample.binned.data.bed
