7a69dd27cb3f91bfa17b622a258d28fc46c0e1fb
Alli Gombolay <agombolay3@gmail.com>
2016-11-12 22:00:43 -0500

    Update 2_rNMP-Coordinates.sh

--- ribose-seq/programs/2_rNMP-Coordinates.sh
+++ ribose-seq/programs/2_rNMP-Coordinates.sh
@@ -58,9 +58,10 @@ for sample in ${sample[@]}; do
 	coverage=$output/$sample.rNMP-coverage.txt
 	readInformation=$output/$sample.read-information.bed
 	
-	riboCoordinates1=$output/$sample.rNMP-coordinates.genome.bed
-	riboCoordinates2=$output/$sample.rNMP-coordinates.$subset.bed
-	
+	#riboCoordinates1=$output/$sample.rNMP-coordinates.genome.bed
+	#riboCoordinates2=$output/$sample.rNMP-coordinates.$subset.bed
+	riboCoordinates1=$output/$sample.rNMP-coordinates.bed
+
 #############################################################################################################################
 	#STEP 1: Covert BAM alignment file to FASTA format
 
@@ -92,20 +93,20 @@ for sample in ${sample[@]}; do
 	awk -v "OFS=\t" '$5 == "-" {print $1, $2, ($2 + 1), " ", " ", $5}' $readInformation > negative-reads.txt
 	
 	#Combine reads on both strands (+ and -) into one file
-	cat positive-reads.txt negative-reads.txt > $riboCoordinates1
+	#cat positive-reads.txt negative-reads.txt > $riboCoordinates1
 	
 	#Select only rNMP coordinates located in nuclear DNA
 	if [ $subset == "nuclear" ]; then
-		#cat positive-reads.txt negative-reads.txt | grep -v 'chrM' - > $riboCoordinates1
-		grep -v 'chrM' $riboCoordinates1 > $riboCoordinates2
+		cat positive-reads.txt negative-reads.txt | grep -v 'chrM' - > $riboCoordinates1
+		#grep -v 'chrM' $riboCoordinates1 > $riboCoordinates2
 	#Select only rNMP coordinates located in mitochondrial DNA
 	elif [ $subset == "chrM" ]; then
-		#cat positive-reads.txt negative-reads.txt | grep 'chrM' - > $riboCoordinates1
-		grep 'chrM' $riboCoordinates1 > $riboCoordinates2
+		cat positive-reads.txt negative-reads.txt | grep 'chrM' - > $riboCoordinates1
+		#grep 'chrM' $riboCoordinates1 > $riboCoordinates2
 	#Select all rNMP coordinates located in genomic DNA
 	else
-		#cat positive-reads.txt negative-reads.txt > $riboCoordinates1
-		cat $riboCoordinates1 > $riboCoordinates2
+		cat positive-reads.txt negative-reads.txt > $riboCoordinates1
+		#cat $riboCoordinates1 > $riboCoordinates2
 	fi
 	
 	#Remove intermediate files
