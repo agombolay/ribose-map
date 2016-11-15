a5e6cd842fe29dc371592469cf363e04a3d88863
Alli Gombolay <agombolay3@gmail.com>
2016-11-15 16:42:52 -0500

    Update 3_Nucleotide-Frequencies.sh

--- ribose-seq/programs/3_Nucleotide-Frequencies.sh
+++ ribose-seq/programs/3_Nucleotide-Frequencies.sh
@@ -93,7 +93,7 @@ for sample in ${sample[@]}; do
 	
 	if [ $reference == "sacCer2" ] && [ $subset == "nuclear" ] ; then
 		#Select only nuclear DNA and output to new file
-		samtools faidx $referenceFasta1 2micron chrI chrII chrIII chrIV chrV 
+		samtools faidx $referenceFasta1 2micron chrI chrII chrIII chrIV chrV \
 		chrVI chrVII chrVIII chrIX chrX chrXI chrXII chrXIII chrXIV chrXV chrXVI > $referenceFasta2
 	
 	elif [ $reference == "sacCer2" ] && [ $subset == "chrM" ] ; then
