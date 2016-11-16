8b493445626e84aaaeede1fad105747808ec4344
Alli Gombolay <agombolay3@gmail.com>
2016-11-15 21:57:17 -0500

    Update 3_Nucleotide-Frequencies.sh

--- ribose-seq/programs/3_Nucleotide-Frequencies.sh
+++ ribose-seq/programs/3_Nucleotide-Frequencies.sh
@@ -95,9 +95,17 @@ for sample in ${sample[@]}; do
 		#Select only nuclear DNA and output to new file
 		samtools faidx $referenceFasta1 2micron chrI chrII chrIII chrIV chrV chrVI chrVII \
 		chrVIII chrIX chrX chrXI chrXII chrXIII chrXIV chrXV chrXVI > $referenceFasta2
-	elif [ $reference == "sacCer2" ] && [ $subset == "chrM" ] ; then
+	elif [ $reference == "mm9" ] && [ $subset == "nuclear" ] ; then
+		samtools faidx $referenceFasta1 chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 \
+		chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chrX chrY > $referenceFasta2
+	elif [ $reference == "hg38" ] && [ $subset == "nuclear" ] ; then
+		samtools faidx $referenceFasta1 chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 \
+		chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX chrY > $referenceFasta2
+	elif [ $subset == "chrM" ] ; then
 		#Select only mitochondrial DNA and output to new file
 		samtools faidx $referenceFasta1 chrM > $referenceFasta2
+	elif [ $subset == "genome" ] ; then
+		samtools faidx $referenceFasta1 > $referenceFasta2
 	fi
 	
 	#Calculate counts of each dNTP
