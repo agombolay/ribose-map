128400f497756e7a71aa13f3a30a6bc091f7ef15
Alli Gombolay <agombolay3@gmail.com>
2016-11-20 20:01:54 -0500

    Update 4_Poisson.sh

--- ribose-seq/programs/4_Poisson.sh
+++ ribose-seq/programs/4_Poisson.sh
@@ -67,7 +67,5 @@ for i in {1..10}; do
 	(( total+=$(awk '$4 >= ('$i')' FS15.trimmed.v1.binned.data.bed | awk '{sum+=$4} END{print sum}') ))
 	count=$(awk '$4 >= ('$i')' FS15.trimmed.v1.binned.data.bed | awk '{sum+=$4} END{print sum}')
 	proportion=$(echo "scale = 12; ($count/$total)" | bc | awk '{printf "%.12f\n", $0}')
-	#echo $proportion
+	echo $proportion
 done
-echo "$total"
-echo $proportion
