2996655729f4294e22b5a827879987b9c1e21717
Alli Gombolay <agombolay3@gmail.com>
2016-11-20 21:49:32 -0500

    Update 4_Poisson.sh

--- ribose-seq/programs/4_Poisson.sh
+++ ribose-seq/programs/4_Poisson.sh
@@ -76,6 +76,6 @@ for i in {1..9}; do
 	proportions+=($(echo "scale = 12; ($counts1/$total)" | bc | awk '{printf "%.12f\n", $0}'))
 	
 done
-#( IFS=$'\n'; echo "${proportions[*]}" )
+( IFS=$'\n'; echo "${proportions[*]}" )
 ( IFS=$'\n'; echo "${counts1[*]}" )
 echo $total
