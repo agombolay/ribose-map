ed684d46f2f6894a0984af63fb41a3113e7a65de
Alli Gombolay <agombolay3@gmail.com>
2016-12-04 01:35:14 -0500

    Update 4_Poisson.sh

--- ribose-seq/programs/4_Poisson.sh
+++ ribose-seq/programs/4_Poisson.sh
@@ -70,10 +70,11 @@ positions2=$(echo "($total-$(wc -l $coverage | awk '{print $1}' -))" | bc)
 #Print observed count data to fit to Poisson distribution
 ( IFS=$'\n'; echo -e "$positions2\n${positions1[*]}" ) > $counts1
 
-array1=$(seq 1 13)
-array2=${positions1[*]}
+array1=$(seq 1 5)
+array2=$(seq 1 3)
+
 echo ${array2[*]}
 for i in "${!array1[@]}"; do
-        #echo "${array1[i]} ${array2[*]}"
+        echo "${array1[i]} ${array2[*]}"
         echo "${array1[$i]}*${array2[$i]}" | bc | awk '{printf "%.12f\n", $0}'
 done
