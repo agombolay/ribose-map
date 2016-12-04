f3fbf6853b57ff0f3e92a61589948f3aee264c04
Alli Gombolay <agombolay3@gmail.com>
2016-12-04 17:51:18 -0500

    Update 4_Poisson_v1.sh

--- ribose-seq/programs/4_Poisson_v1.sh
+++ ribose-seq/programs/4_Poisson_v1.sh
@@ -79,7 +79,7 @@ array2=(${positions1[*]})
 sum=0
 for i in "${!array1[@]}"; do
         echo -e "${array1[i]}\t${array2[i]}"
-        (( sum +=$(echo "${array1[$i]}*${array2[$i]}" | bc | awk '{printf "%.0f\n", $0}') ))
+        (( sum +=$(echo "${array1[$i]}*${array2[$i]}" | bc) ))
 done
 echo "Total rNMPs:" $sum
 
