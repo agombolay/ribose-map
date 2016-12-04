749ba52504341ecfc0dff191364bbf4ea8c56511
Alli Gombolay <agombolay3@gmail.com>
2016-12-03 22:28:08 -0500

    Update 4_Poisson.sh

--- ribose-seq/programs/4_Poisson.sh
+++ ribose-seq/programs/4_Poisson.sh
@@ -62,13 +62,15 @@ positions2=$(wc -l output | awk '{print $1}' -)
 zero=$(echo "($positions1-$positions2)" | bc | awk '{printf "%.0f\n", $0}')
 
 #Count how many positions have X number of rNMPs
-#positions3()
+positions3()
 for i in $(seq 1 $maximum); do
 	positions3+=($(awk '$4 == ('$i')' output | wc -l))
 done
 
 #echo $zero
 echo ${positions3[*]}
+#( IFS=$'\n'; echo "${counts1[*]}" )
+#( IFS=$'\n'; echo "${proportions[*]}" )
 
 #############################################################################################################################
 
