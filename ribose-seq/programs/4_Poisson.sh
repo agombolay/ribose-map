885eb6a5e50c8b3e6aa8ffdf9ea5daf893c341e7
Alli Gombolay <agombolay3@gmail.com>
2016-12-03 22:23:37 -0500

    Update 4_Poisson.sh

--- ribose-seq/programs/4_Poisson.sh
+++ ribose-seq/programs/4_Poisson.sh
@@ -65,6 +65,7 @@ zero=$(echo "($positions1-$positions2)" | bc | awk '{printf "%.0f\n", $0}')
 positions3()
 for i in $(seq 1 $maximum); do
 	positions3+=($(awk '$4 == ('$i')' output | wc -l))
+	echo $positions3
 done
 
 echo $zero
