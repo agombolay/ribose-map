d8c1097ff8d9e7ff60079c278163adc2b4dd31bc
Alli Gombolay <agombolay3@gmail.com>
2016-12-04 00:40:35 -0500

    Update 4_Poisson.sh

--- ribose-seq/programs/4_Poisson.sh
+++ ribose-seq/programs/4_Poisson.sh
@@ -72,5 +72,5 @@ positions2=$(echo "($total-$(wc -l $coverage | awk '{print $1}' -))" | bc)
 echo ${positions1[0:1]}
 
 array=$(seq 1 2)
-lambda=$(echo "scale = 12; ${positions1[0..1]}*$array" | bc | awk '{printf "%.12f\n", $0}')
+lambda=$(echo "scale = 12; ${positions1[@]:0:1}*$array" | bc | awk '{printf "%.12f\n", $0}')
 echo $lambda
