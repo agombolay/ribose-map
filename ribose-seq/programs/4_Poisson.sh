3f3702bb6d9b86e0b1ff0367b1e531af90f7508b
Alli Gombolay <agombolay3@gmail.com>
2016-12-03 22:07:17 -0500

    Update 4_Poisson.sh

--- ribose-seq/programs/4_Poisson.sh
+++ ribose-seq/programs/4_Poisson.sh
@@ -56,7 +56,7 @@ fi
 maximum=$(sort -nk 4 output | tail -1 - | awk '{print $4}' -)
 
 positions2=$(wc -l output)
-zero=$($positions1-$positions2)
+zero=$(echo "scale = 12; ($positions1-$positions2)" | bc | awk '{printf "%.12f\n", $0}')
 echo $zero
 
 #Count how many positions have X number of rNMPs
