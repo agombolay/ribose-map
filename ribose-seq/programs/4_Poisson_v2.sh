4ae4fed2f775f21ada850a3798b88045313d8f41
Alli Gombolay <agombolay3@gmail.com>
2017-01-14 23:34:09 -0500

    Update 4_Poisson_v2.sh

--- ribose-seq/programs/4_Poisson_v2.sh
+++ ribose-seq/programs/4_Poisson_v2.sh
@@ -146,15 +146,16 @@ total=$(awk '{ SUM += $2} END { print SUM }' $counts1)
 
 #Proportions of windows (P(X=x))
 for i in ${windows[*]}; do
-	values+=($(echo "scale = 12; ($i/$total)" | bc | awk '{printf "%.12f\n", $0}'))
+	values1+=($(echo "scale = 12; ($i/$total)" | bc | awk '{printf "%.12f\n", $0}'))
 done
 paste <(echo "$(seq 0 $maximum)") <(cat <( IFS=$'\n'; echo "${values[*]}" )) > $proportions1
-#( IFS=$'\n'; echo "${values[*]}" )
 
 #Proportions of windows (P(X>=x))
 for i in $(awk '{print $1}' $proportions1); do
-	echo "scale = 12; (1-$i)" | bc | awk '{printf "%.12f\n", $0}' >> $proportions2
+	values2+=($(echo "scale = 12; (1-$i)" | bc | awk '{printf "%.12f\n", $0}'))
 done
+paste <(echo "$(seq 0 $maximum)") <(cat <( IFS=$'\n'; echo "${values[*]}" )) > $proportions2
+
 
 #variable=0
 #proportions=()
