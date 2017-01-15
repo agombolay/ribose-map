707995046ef62d6c6f2ac248069f824e3073ebcc
Alli Gombolay <agombolay3@gmail.com>
2017-01-14 21:38:06 -0500

    Update 4_Poisson_v2.sh

--- ribose-seq/programs/4_Poisson_v2.sh
+++ ribose-seq/programs/4_Poisson_v2.sh
@@ -130,8 +130,7 @@ done
 paste <(echo "$(seq 0 $maximum)") <(cat <( IFS=$'\n';echo "${windows[*]}" )) >> data1.txt
 
 for i in $(wc -l < data1.txt); do
-echo $i
-	#head -${i} data1.txt | awk '{ SUM += $2} END { print SUM }'
+	head -${i} data1.txt | tail -${i} | awk '{ SUM += $2} END { print SUM }'
 done
 
 #variable=0
