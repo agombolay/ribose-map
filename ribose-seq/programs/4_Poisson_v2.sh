5e57b1df7d75b2c9fb758e45b933c124a415c7b2
Alli Gombolay <agombolay3@gmail.com>
2017-01-14 23:32:06 -0500

    Update 4_Poisson_v2.sh

--- ribose-seq/programs/4_Poisson_v2.sh
+++ ribose-seq/programs/4_Poisson_v2.sh
@@ -148,7 +148,7 @@ total=$(awk '{ SUM += $2} END { print SUM }' $counts1)
 for i in ${windows[*]}; do
 	values+=($(echo "scale = 12; ($i/$total)" | bc | awk '{printf "%.12f\n", $0}'))
 done
-paste <(echo "$(seq 0 $maximum)") <(cat <( IFS=$'\n'; echo "${values[*]}" ))
+paste <(echo "$(seq 0 $maximum)") <(cat <( IFS=$'\n'; echo "${values[*]}" )) >> $proportions1
 #( IFS=$'\n'; echo "${values[*]}" )
 
 #Proportions of windows (P(X>=x))
