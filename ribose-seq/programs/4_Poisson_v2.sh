82b1896de576f70740f4e654d8b344a9aa44bc75
Alli Gombolay <agombolay3@gmail.com>
2017-01-15 01:57:51 -0500

    Update 4_Poisson_v2.sh

--- ribose-seq/programs/4_Poisson_v2.sh
+++ ribose-seq/programs/4_Poisson_v2.sh
@@ -93,8 +93,9 @@ for i in ${windows[*]}; do
 done
 
 sum=0
+echo ${values1[1]}
 #Proportions of windows (P(X>=x))
-for i in `seq 1 3`; do
+for i in `seq ${values1[1]} ${values1[1]}`; do
 	sum=`expr $sum + $i`
         echo $sum
 	#values2+=($(echo "scale = 12; (1-$i)" | bc | awk '{printf "%.12f\n", $0}'))
