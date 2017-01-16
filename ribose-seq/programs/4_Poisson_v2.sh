2674600ae8623d1ded3d3f21cf7480d2a3d98c9d
Alli Gombolay <agombolay3@gmail.com>
2017-01-16 16:37:45 -0500

    Update 4_Poisson_v2.sh

--- ribose-seq/programs/4_Poisson_v2.sh
+++ ribose-seq/programs/4_Poisson_v2.sh
@@ -42,15 +42,14 @@ referenceBed=$directory/ribose-seq/reference/$reference.bed
 sorted=$directory/ribose-seq/results/$reference/$sample/Coordinates/$subset/$sample.rNMP-coordinates.sorted.bed
 
 #Output directories
-output1=$directory/ribose-seq/reference/; output2=$directory/ribose-seq/results/$reference/$sample/Poisson
+output1=$directory/ribose-seq/reference; output2=$directory/ribose-seq/results/$reference/$sample/Poisson
 
 #Create directories
 mkdir -p $output1 $output2
 
 #Output files
 binnedData=$output2/$sample.binned.data.bed; referenceWindows=$output1/$reference.windows.bed
-counts1=$output2/$sample.Poisson-PDF.counts.txt; counts2=$output2/$sample.Poisson-CDF.counts.txt
-proportions1=$output2/$sample.Poisson-PDF.proportions.txt; proportions2=$output2/$sample.Poisson-CDF.proportions.txt
+counts=$output2/$sample.PDF-windows.txt; proportions=$output2/$sample.PDF-proportions.txt
 
 #Separate reference genome into 2.5 kb windows
 bedtools makewindows -g $referenceBed -w 2500 > $referenceWindows
@@ -58,11 +57,9 @@ bedtools makewindows -g $referenceBed -w 2500 > $referenceWindows
 #Select only data of interest
 if [ $subset == "nuclear" ]; then
 	#Determine regions of BED files that intersect and count number of overlaps (nuclear)
-	#bedtools intersect -a $referenceWindows -b $sorted -c -sorted -nonamecheck | grep -v 'chrM' - | \
+	bedtools intersect -a $referenceWindows -b $sorted -c -sorted -nonamecheck | grep -v 'chrM' - | \
 	#Remove rows where window size is < 2.5 kb
-	#awk '{ $5 = $3 - $2 } 1' - | awk '($5 == 2500 )' - > $binnedData
-	
-	bedtools intersect -a $referenceWindows -b $sorted -c -sorted -nonamecheck | grep -v 'chrM' - > $binnedData
+	awk '{ $5 = $3 - $2 } 1' - | awk '($5 == 2500 )' - > $binnedData
 elif [ $subset == "chrM" ]; then
 	#Determine regions of BED files that intersect and count number of overlaps (chrM)
 	bedtools intersect -a $referenceWindows -b $sorted -c -sorted -nonamecheck | grep 'chrM' - | \
@@ -73,41 +70,10 @@ fi
 #Maximum value of genome coverage in BED file
 maximum=$(sort -nk 4 $binnedData | tail -1 - | awk '{print $4}' -)
 
-#Number of positions with X number of rNMPs
+#Determine number of windows with 0...maximum rNMPs
 for i in $(seq 0 $maximum); do
 	windows+=($(awk '$4 == ('$i')' $binnedData | wc -l))
 done
 
-#Probability Mass Function Counts
-#Print number of windows in genomic region with exactly 0...X rNMPs
-paste <(echo "$(seq 0 $maximum)") <(cat <( IFS=$'\n'; echo "${windows[*]}" )) > $counts1
-
-#Cumulative Distribution Counts
-#Print number of windows in genomic region with greater than or equal to 0...X rNMPs
-for i in $(seq $(wc -l < $counts1) -1 1); do
-	head -$(wc -l < $counts1) $counts1 | tail -${i} | awk '{ SUM += $2} END { print SUM }' >> $counts2
-done
-
-#Total number of windows
-total=$(awk '{ SUM += $2} END { print SUM }' $counts1)
-
-#Proportions of windows (P(X=x))
-for i in ${windows[*]}; do
-	values1+=($(echo "scale = 12; ($i/$total)" | bc | awk '{printf "%.12f\n", $0}'))
-done
-
-sum=0
-#Proportions of windows (P(X>=x))
-for i in ${values1[@]}; do
-	sum=$(echo $sum + $i | bc)
-        #echo $sum
-	#values2+=($(echo "scale = 12; (1-$i)" | bc | awk '{printf "%.12f\n", $0}'))
-	echo "scale = 12; (1-$sum)" | bc | awk '{printf "%.12f\n", $0}'
-done
-
-#Print proportions of windows (probability mass function and cumulative distribution)
-paste <(echo "$(seq 0 $maximum)") <(cat <( IFS=$'\n'; echo "${values1[*]}" )) > $proportions1
-paste <(echo "$(seq 0 $maximum)") <(cat <( IFS=$'\n'; echo -e "1.0\n${values2[*]}" )) > $proportions2
-
-#Calculate lambda value (mean) for Poisson Distribution
-echo "Lambda:" $(echo "scale = 12; $(wc -l < $sorted)/$total" | bc | awk '{printf "%.5f\n", $0}')
+#Print number of windows with 0...maximum rNMPs and save to file (input into R)
+paste <(echo "$(seq 0 $maximum)") <(cat <( IFS=$'\n'; echo "${windows[*]}" )) > $counts
