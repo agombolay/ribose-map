7c2a2348998cd68a9ea0cfae878aad6786a39bc8
Alli Gombolay <agombolay3@gmail.com>
2017-03-14 12:23:20 -0400

    Update commands4.sh

--- ribose-seq/programs/Examples/commands4.sh
+++ ribose-seq/programs/Examples/commands4.sh
@@ -1,5 +1,5 @@
 #Count number of reads with correct barcode
 samtools view -h sample.bam -o sample.sam
-grep 'UMI_....barcode....' sample.sam > sample-filtered.sam
+grep -E 'UMI_....barcode....|@' sample.sam > sample-filtered.sam
 samtools view sample-filtered.sam -b -S | samtools sort -o sample-filtered.bam
 samtools index sample-filtered.bam
