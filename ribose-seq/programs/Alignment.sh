76d927eb7b63d0eeda3db89a80cf5362df310aad
Alli Gombolay <agombolay3@gmail.com>
2017-03-13 21:23:30 -0400

    Update Alignment.sh

--- ribose-seq/programs/Alignment.sh
+++ ribose-seq/programs/Alignment.sh
@@ -10,7 +10,7 @@
 function usage () {
 	echo "Usage: Alignment.sh [-i] 'Sample(s)' [-p] 'Path' [-b] 'Index' [-d] 'Directory' [-h]
 		-i Sample name(s) (FS1, FS2, FS3 etc.)
-		-p '/projects/home/agombolay3/data/bin/Trimmomatic-0.36'
+		-p /projects/home/agombolay3/data/bin/Trimmomatic-0.36
 		-b Basename of Bowtie2 index (sacCer2, ecoli, mm9, hg38, etc.)
 		-d Directory (/projects/home/agombolay3/data/repository/Ribose-seq-Project)"
 }
