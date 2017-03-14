c70f0f386536fd0ff3ab46e1426710c5012af153
Alli Gombolay <agombolay3@gmail.com>
2017-03-14 12:36:22 -0400

    Update commands4.sh

--- ribose-seq/programs/Examples/commands4.sh
+++ ribose-seq/programs/Examples/commands4.sh
@@ -3,3 +3,9 @@ samtools view -h sample.bam -o sample.sam
 grep -e 'UMI_....barcode....' -e '@HG' -e 'SQ' -e 'PG' sample.sam > sample-filtered.sam
 samtools view sample-filtered.sam -b -S | samtools sort -o sample-filtered.bam
 samtools index sample-filtered.bam
+
+Results of these commands should match!
+```
+samtools view -c -b sample-filtered.bam
+samtools bam2fq sample.bam | grep 'UMI_....TGA....' - | wc -l
+```
