9f608e47369f4fd22847e1f4bae20913650e0933
Alli Gombolay <agombolay3@gmail.com>
2017-03-13 22:06:31 -0400

    Update 4_Plot-Poisson-Distribution.sh

--- ribose-seq/programs/Examples/4_Plot-Poisson-Distribution.sh
+++ ribose-seq/programs/Examples/4_Plot-Poisson-Distribution.sh
@@ -1,1 +1,1 @@
-Rscript PoissonLabMeeting.R "/Users/alligombolay/desktop/FS15.Poisson-PDF.counts.txt" "Plot.pdf"
+Rscript Hotspots.R "/Users/alligombolay/desktop/FS15.Poisson-PDF.counts.txt" "Plot.pdf"
