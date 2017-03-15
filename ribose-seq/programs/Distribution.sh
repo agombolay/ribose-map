471db9a2a7dde0f33ddfd490aaad3f25b2641c52
Alli Gombolay <agombolay3@gmail.com>
2017-03-15 12:28:53 -0400

    Update Distribution.sh

--- ribose-seq/programs/Distribution.sh
+++ ribose-seq/programs/Distribution.sh
@@ -60,9 +60,14 @@ if [ $subset == "nuclear" ]; then
 	{print $1,$2,$3,$4}' - | sort -k4 -n - > temporary1
 
 elif [ $subset == "chrM" ]; then
+	#grep 'chrM' $referenceWindows > temporary2
+	#bedtools intersect -a temporary2 -b $coordinates -c -sorted -nonamecheck \
+	#| grep 'chrM' - | awk '{ $5 = $3 - $2 } 1' - | awk -v OFS='\t' '($5 == 2500 ) \
+	#{print $1,$2,$3,$4}' - | sort -k4 -n - > temporary1
+	
 	grep 'chrM' $referenceWindows > temporary2
 	bedtools intersect -a temporary2 -b $coordinates -c -sorted -nonamecheck \
-	| grep 'chrM' - | awk '{ $5 = $3 - $2 } 1' - | awk -v OFS='\t' '($5 == 2500 ) \
+	| grep 'chrM' - | awk '{ $5 = $3 - $2 } 1' - | awk -v OFS='\t' \
 	{print $1,$2,$3,$4}' - | sort -k4 -n - > temporary1
 fi
 
