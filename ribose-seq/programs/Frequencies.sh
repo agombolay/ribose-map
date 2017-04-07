5d2ab207e98cda558b540923e7ea3025c9a16ab9
Alli Gombolay <agombolay3@gmail.com>
2017-04-07 15:30:17 -0400

    Update Frequencies.sh

--- ribose-seq/programs/Frequencies.sh
+++ ribose-seq/programs/Frequencies.sh
@@ -90,34 +90,37 @@ for sample in ${sample[@]}; do
 	#Index reference FASTA file
 	samtools faidx $referenceFasta1
 	
-	#Subset nuclear DNA
-	if [ $reference == "pombe" ] && [ $subset == "nuclear" ] ; then
-		#Select only nuclear DNA and output to new file
-		samtools faidx $referenceFasta1 I II III > $referenceFasta2
-	elif [ $reference == "sacCer2" ] && [ $subset == "nuclear" ] ; then
-		samtools faidx $referenceFasta1 2micron chrI chrII chrIII chrIV chrV chrVI chrVII \
-		chrVIII chrIX chrX chrXI chrXII chrXIII chrXIV chrXV chrXVI > $referenceFasta2
-	elif [ $reference == "mm9" ] && [ $subset == "nuclear" ] ; then
-		samtools faidx $referenceFasta1 chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 \
-		chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chrX chrY > $referenceFasta2
-	elif [ $reference == "hg38" ] && [ $subset == "nuclear" ] ; then
-		samtools faidx $referenceFasta1 chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 \
-		chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX chrY > $referenceFasta2
-	
-	#Subset mito DNA
-	elif [ $reference == "pombe" ] && [ $subset == "mito" ] ; then
-		#Select only mitochondrial DNA and output to new file
-		samtools faidx $referenceFasta1 MT > $referenceFasta2
-	elif [ $reference != "pombe" ] && [ $subset == "mito" ] ; then
-		#Select only mitochondrial DNA and output to new file
-		samtools faidx $referenceFasta1 chrM > $referenceFasta2
-	
-	#All genomic DNA
-	elif [ $subset == "genome" ] ; then
-		cp $referenceFasta1 $referenceFasta2
-		samtools faidx $referenceFasta2
-	fi
+	if [ -f "$file" ]
+	then
+		exit
+	else
 
+		#Subset nuclear DNA
+		if [ $reference == "pombe" ] && [ $subset == "nuclear" ] ; then
+			samtools faidx $referenceFasta1 I II III > $referenceFasta2
+		elif [ $reference == "sacCer2" ] && [ $subset == "nuclear" ] ; then
+			samtools faidx $referenceFasta1 2micron chrI chrII chrIII chrIV chrV chrVI chrVII \
+			chrVIII chrIX chrX chrXI chrXII chrXIII chrXIV chrXV chrXVI > $referenceFasta2
+		elif [ $reference == "mm9" ] && [ $subset == "nuclear" ] ; then
+			samtools faidx $referenceFasta1 chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 \
+			chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chrX chrY > $referenceFasta2
+		elif [ $reference == "hg38" ] && [ $subset == "nuclear" ] ; then
+			samtools faidx $referenceFasta1 chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 \
+			chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX chrY > $referenceFasta2
+	
+		#Subset mito DNA
+		elif [ $reference == "pombe" ] && [ $subset == "mito" ] ; then
+			samtools faidx $referenceFasta1 MT > $referenceFasta2
+		elif [ $reference != "pombe" ] && [ $subset == "mito" ] ; then
+			samtools faidx $referenceFasta1 chrM > $referenceFasta2
+	
+		#All genomic DNA
+		elif [ $subset == "genome" ] ; then
+			cp $referenceFasta1 $referenceFasta2
+			samtools faidx $referenceFasta2
+		fi
+	fi
+	
 	#Index reference FASTA file
 	samtools faidx $referenceFasta2
 	
