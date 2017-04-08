e7001d1b1315a4d6f596e097939a4cfbef8db1fa
Alli Gombolay <agombolay3@gmail.com>
2017-04-07 20:19:26 -0400

    Update Frequencies.sh

--- ribose-seq/programs/Frequencies.sh
+++ ribose-seq/programs/Frequencies.sh
@@ -48,20 +48,12 @@ for sample in ${sample[@]}; do
 
 	#Output directories
 	output1=$directory/ribose-seq/results/Background-Frequencies
-	
-	#output2=$directory/ribose-seq/results/$reference/$sample/Frequencies/rNMPs/$subset
-	#output3=$directory/ribose-seq/results/$reference/$sample/Frequencies/dNTPs/$subset/Results
-	
-	output4=$directory/ribose-seq/results/$reference/$sample/Frequencies/dNTPs/$subset/Columns/upstream
-	output5=$directory/ribose-seq/results/$reference/$sample/Frequencies/dNTPs/$subset/Columns/downstream
-	
-	#output6=$directory/ribose-seq/results/$reference/$sample/Frequencies/dNTPs/$subset/Columns/upstream/sequences
-	#output7=$directory/ribose-seq/results/$reference/$sample/Frequencies/dNTPs/$subset/Columns/downstream/sequences
-	
-	output8=$directory/ribose-seq/results/$reference/$sample/Frequencies/Datasets/$subset
+	output2=$directory/ribose-seq/results/$reference/$sample/Frequencies/Datasets/$subset
+	output3=$directory/ribose-seq/results/$reference/$sample/Frequencies/dNTPs/$subset/Columns/upstream
+	output4=$directory/ribose-seq/results/$reference/$sample/Frequencies/dNTPs/$subset/Columns/downstream
 
 	#Create directories
-	mkdir -p $output{1,4,5,8}
+	mkdir -p $output{1..4}
 
 	#Remove older versions files
 	rm -f ./*.txt
@@ -69,22 +61,8 @@ for sample in ${sample[@]}; do
 	
 	#Output files
 	background=$output1/Background-Frequencies.$reference.$subset.txt
-	#riboSequences=$output2/$sample.rNMP-Sequences.$reference.$subset.txt
-	
-	#upstreamSequences=$output3/$sample.upstream-sequences.$reference.$subset.fa
-	#upstreamIntervals=$output3/$sample.upstream-intervals.$reference.$subset.bed
-	
-	#downstreamSequences=$output3/$sample.downstream-sequences.$reference.$subset.fa
-	#downstreamIntervals=$output3/$sample.downstream-intervals.$reference.$subset.bed
-	
-	#sequences1=$output6/$sample.upstream-sequences.$reference.$subset.txt
-	#sequences2=$output7/$sample.downstream-sequences.$reference.$subset.txt
-	
-	#columns1=$output6/$sample.upstream-sequences.$reference.$subset.tab
-	#columns2=$output7/$sample.downstream-sequences.$reference.$subset.tab
-
-	dataset=$output8/$sample.nucleotide-frequencies-dataset.$reference.$subset.txt
-	zoomed=$output8/$sample.nucleotide-frequencies-zoomed.$reference.$subset.txt
+	zoomed=$output2/$sample.nucleotide-frequencies-zoomed.$reference.$subset.txt
+	dataset=$output2/$sample.nucleotide-frequencies-dataset.$reference.$subset.txt
 		
 #############################################################################################################################
 	#STEP 1: Calculate background dNTP frequencies of reference genome
@@ -283,3 +261,7 @@ for sample in ${sample[@]}; do
 
 done
 done
+
+#Remove temp files
+rm -f ./*.txt
+rm -f ./*.tab
