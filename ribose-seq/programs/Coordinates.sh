64c9906e36364fdff13bb29f91fa69c9dfb5c6cf
Alli Gombolay <agombolay3@gmail.com>
2017-04-10 16:10:17 -0400

    Update Coordinates.sh

--- ribose-seq/programs/Coordinates.sh
+++ ribose-seq/programs/Coordinates.sh
@@ -72,15 +72,16 @@ for sample in ${sample[@]}; do
 		#Obtain coordinates of rNMPs located on negative strand of DNA
 		negativeReads=$(awk -v "OFS=\t" '$5 == "-" {print $1, $2, ($2 + 1), " ", " ", $5}' $reads)
 	
-		if [ $subset == "mitochondria" ]; then
-			#Combine +/- mitochondria DNA coordinates and sort coordinates
+		#Combine +/- coordinates into one file for processing
+		cat <(echo "$positiveReads") <(echo "$negativeReads") > temp3
+		
+		#Subset and/or sort coordinates
+		if [ $subset == "genome" ]; then
+			sort -k1,1 -k2,2n temp3 > $coordinates
+		elif [ $subset == "mitochondria" ]; then
 			grep -E '(chrM|MT)' temp3 | sort -k1,1 -k2,2n - > $coordinates
 		elif [ $subset == "nucleus" ]; then
-			#Combine +/- nuclear DNA coordinates and sort coordinates
 			grep -v -E '(chrM|MT*|AB*|chrEBV|chrUN*|*random)' temp3 | sort -k1,1 -k2,2n - > $coordinates
-		elif [ $subset == "genome" ]; then
-			#Combine +/- genomic DNA coordinates and sort coordinates
-			cat <(echo "$positiveReads") <(echo "$negativeReads") > temp3; sort -k1,1 -k2,2n temp3 > $coordinates
 		fi
 	done
 done
