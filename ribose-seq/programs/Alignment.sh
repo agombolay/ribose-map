7d7fcc248b73e11e3d5982aecc0fc0366d2bfd47
Alli Gombolay <agombolay3@gmail.com>
2017-05-10 16:54:53 -0400

    Update Alignment.sh

--- ribose-seq/programs/Alignment.sh
+++ ribose-seq/programs/Alignment.sh
@@ -69,8 +69,8 @@ for sample in ${sample[@]}; do
 	#Trim UMI from 5' ends of reads (append UMI to read name for subsequent de-duplication step)
 	umi_tools extract -I $output/$sample-QCtrimmed.fastq -p $UMI -L $output/log.file -S $extracted
 	
-	#Reverse complement reads (rNMP = RC of 5' base)
-	cat $extracted | seqtk seq -r - > reverseComplement.fastq
+	#Reverse complement reads (rNMP = reverse complement of 5' base)
+	cat $extracted | seqtk seq -r - > $output/reverseComplement.fastq
 		
 	#Align reads to reference using Bowtie2 and output statistics
 	#bowtie -m 1 $index $reverseComplement -S $tempSAM 2> $statistics
