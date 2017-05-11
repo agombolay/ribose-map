4d40ff7016ad8c44a9bc5c1abb9d3374f0979d6d
Alli Gombolay <agombolay3@gmail.com>
2017-05-11 19:23:33 -0400

    Update Coordinates.sh

--- ribose-seq/programs/Coordinates.sh
+++ ribose-seq/programs/Coordinates.sh
@@ -8,9 +8,9 @@
 #Usage statement
 function usage () {
 	echo "Usage: Coordinates.sh [-i] 'Sample(s)' [-r] 'Reference' [-d] 'Directory' [-h]
-	-i Sample name(s) (FS1, FS2, FS3 etc.)
-	-r Reference genome (sacCer2, pombe, ecoli, mm9, hg38, etc.)
-	-d Directory (/projects/home/agombolay3/data/repository/Ribose-seq-Project)"
+	-i Input sample(s) (e.g., FS1, FS2, FS3)
+	-r Reference genome (e.g., sacCer2, pombe, ecoli, mm9, hg38)
+	-d Directory (e.g., /projects/home/agombolay3/data/repository/Ribose-seq-Project)"
 }
 
 #Command-line options
