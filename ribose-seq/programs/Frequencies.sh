b03edcff48e8cecee702250c8b917743e5c3c8bc
Alli Gombolay <agombolay3@gmail.com>
2017-05-14 18:21:05 -0400

    Update Frequencies.sh

--- ribose-seq/programs/Frequencies.sh
+++ ribose-seq/programs/Frequencies.sh
@@ -160,7 +160,7 @@ for sample in ${sample[@]}; do
 	bedtools flank -i $coordinates -s -g $referenceBED -l 0 -r 100 > downstreamIntervals.txt
 	
 	#Obtain sequences of upstream/downstream coordinates
-	#bedtools getfasta -s -fi $referenceFasta1 -bed upstreamIntervals.txt -fo upstreamSequences.txt
+	bedtools getfasta -s -fi $referenceFasta1 -bed upstreamIntervals.txt -fo upstreamSequences.txt
 	#bedtools getfasta -s -fi $referenceFasta1 -bed downstreamIntervals.txt -fo downstreamSequences.txt
 
 #############################################################################################################################
