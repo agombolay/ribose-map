b7d946f7a1f26e5a753e49487d2edfe1e2faa2ec
Alli Gombolay <agombolay3@gmail.com>
2017-05-21 19:30:06 -0400

    Update Frequencies.R

--- ribose-seq/programs/Frequencies.R
+++ ribose-seq/programs/Frequencies.R
@@ -6,22 +6,21 @@
 #This program plots rNMP and flanking nucleotide frequencies (100 bp upstream and downstream)
 #rNMP frequencies = position 0, upstream frequencies = -100 -> -1, and downstream = +1 -> +100
 
-##########################################################################################
+#############################################################################################################################
 #Load libraries
 library(optparse)
 library(ggplot2)
 
 #Command line options
 option_list <- list(make_option(c("-i", "--input"), help="Input sample name"),
-                    make_option(c("-t", "--title"), help="Title will be same for all plots"),
-                    make_option(c("-r", "--reference"), help="Reference genome (e.g., sacCer2, pombe, ecoli, mm9, hg38)"),
-                    make_option(c("-d", "--directory"), help="Local user directory (e.g., /projects/home/agombolay3/data/repository)"))
+make_option(c("-t", "--title"), help="Title will be same for all plots; blank = no title"),
+make_option(c("-r", "--reference"), help="Reference genome (e.g., sacCer2, pombe, ecoli, mm9, hg38)"),
+make_option(c("-d", "--directory"), help="Local user directory (e.g., /projects/home/agombolay3/data/repository)"))
 
 #Get command line options, if -h encountered print help
 opt <- parse_args(OptionParser(option_list=option_list))
 
-##############################################################################################################
-
+#############################################################################################################################
 for(i in c("all", "mito", "nucleus")) {
 
     #Specify output directory and file
@@ -33,7 +32,7 @@ for(i in c("all", "mito", "nucleus")) {
 	
                 for(j in c("regular", "zoomed")) {
 
-##############################################################################################################
+#############################################################################################################################
                     #Specify datasets to be used for each round of loop
                     if (j == "regular") {data = read.table(file, sep="\t", header=TRUE)}
                     if (j == "zoomed") {data = read.table(file, sep="\t", header=TRUE)[86:116,]}
@@ -41,7 +40,7 @@ for(i in c("all", "mito", "nucleus")) {
                     #Define variables to store nucleotide positions and frequency values
                     position <- data$X; A1 <- data$A; C1 <- data$C; G1 <- data$G; T1 <- data$U.T
 
-##############################################################################################################
+#############################################################################################################################
                     #Plot frequencies
                     myplot <- ggplot(data=data, aes(x=position)) +
     
@@ -68,7 +67,7 @@ for(i in c("all", "mito", "nucleus")) {
                     #Specify color values for each of the four different nucleotides
                     scale_colour_manual(values=c("#CC79A7", "#56B4E9", "#E69F00", "#009E73"), name="Nucleotide")
 
-##############################################################################################################
+#############################################################################################################################
                     #Specify output path and save plot as PNG file
                     ggsave(filename=file.path(output, paste(opt$input, "-", j, ".", i, ".png", sep="")), plot=myplot)
                 }
