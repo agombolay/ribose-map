31d3f5c2f012f9e1f5f05317a5d6042b4cd8b3b1
Alli Gombolay <agombolay3@gmail.com>
2017-05-21 19:27:55 -0400

    Update Frequencies.R

--- ribose-seq/programs/Frequencies.R
+++ ribose-seq/programs/Frequencies.R
@@ -6,98 +6,71 @@
 #This program plots rNMP and flanking nucleotide frequencies (100 bp upstream and downstream)
 #rNMP frequencies = position 0, upstream frequencies = -100 -> -1, and downstream = +1 -> +100
 
+##########################################################################################
 #Load libraries
 library(optparse)
 library(ggplot2)
 
 #Command line options
-option_list <- list(
-    make_option(c("-i", "--input")),
-    make_option(c("-t", "--title")),
-    make_option(c("-s", "--sample"))
-)
+option_list <- list(make_option(c("-i", "--input"), help="Input sample name"),
+                    make_option(c("-t", "--title"), help="Title will be same for all plots"),
+                    make_option(c("-r", "--reference"), help="Reference genome (e.g., sacCer2, pombe, ecoli, mm9, hg38)"),
+                    make_option(c("-d", "--directory"), help="Local user directory (e.g., /projects/home/agombolay3/data/repository)"))
 
 #Get command line options, if -h encountered print help
 opt <- parse_args(OptionParser(option_list=option_list))
 
-#Plot regular dataset
-if (file.exists(opt$input)) {
-    #Define data file (tab delimited) to read
-    dataset <- read.table(opt$input, sep="\t", header=TRUE)
-    
-    #Define position values
-    position <- dataset$X
-    
-    #Define frequency values
-    frequencyA <- dataset$A; frequencyC <- dataset$C
-    frequencyG <- dataset$G; frequencyT <- dataset$U.T
+##############################################################################################################
 
-    #Plot frequencies
-    myplot <- ggplot(data=dataset, aes(x=position)) +
-        
-        #Plot data as scatterplot   
-        geom_line(aes(y = frequencyA, colour = "A")) + geom_point(aes(y = frequencyA, colour = "A")) +
-        geom_line(aes(y = frequencyC, colour = "C")) + geom_point(aes(y = frequencyC, colour = "C")) +
-        geom_line(aes(y = frequencyG, colour = "G")) + geom_point(aes(y = frequencyG, colour = "G")) +
-        geom_line(aes(y = frequencyT, colour = "U/T")) + geom_point(aes(y = frequencyT, colour = "U/T")) +
-    
-        #Add axes titles
-        xlab("Position") + ylab("Frequency") +
+for(i in c("all", "mito", "nucleus")) {
 
-        #Remove and replace default background plot theme
-        theme_bw() + theme(panel.border=element_blank(), panel.grid.major=element_blank(),
-        panel.grid.minor=element_blank(), axis.line=element_line(colour="black")) +
-    
-        #Specify color values for each of the four nucleotides
-        scale_colour_manual(values=c("#CC79A7", "#56B4E9", "#E69F00", "#009E73"), name="Nucleotide") +
-    
-        #Label axes, add title, define y-axis limits, and specify font size
-        theme(text = element_text(size=14)) + ggtitle(opt$title) + theme(plot.title = element_text(hjust = 0.5)) +
+    #Specify output directory and file
+    output <- file.path(opt$directory, "Ribose-Map", "Results", opt$reference, opt$input, "Frequencies")
+    file <- file.path(output, paste(opt$input, ".", "frequencies", ".", opt$reference, ".", i, ".txt", sep=""))
 
-        #Specify increased distance between axes and axes lables so easier to read
-        theme(axis.title.y=element_text(margin=margin(0,20,0,0))) + theme(axis.title.x=element_text(margin=margin(20,0,0,0)))
+        #Plot regular dataset
+            if (file.exists(file)) {
+	
+                for(j in c("regular", "zoomed")) {
 
-    #Save plot as PDF file
-    ggsave(filename=paste(opt$sample, "-Regular", ".png", sep=""), plot=myplot)
-}
+##############################################################################################################
+                    #Specify datasets to be used for each round of loop
+                    if (j == "regular") {data = read.table(file, sep="\t", header=TRUE)}
+                    if (j == "zoomed") {data = read.table(file, sep="\t", header=TRUE)[86:116,]}
+    
+                    #Define variables to store nucleotide positions and frequency values
+                    position <- data$X; A1 <- data$A; C1 <- data$C; G1 <- data$G; T1 <- data$U.T
 
-#Plot zoomed dataset
-if (file.exists(opt$input)) {
-    #Define data file (tab delimited) to read
-    zoomed <- dataset[86:116,]
+##############################################################################################################
+                    #Plot frequencies
+                    myplot <- ggplot(data=data, aes(x=position)) +
     
-    #Define position values
-    position <- zoomed$X
+                    #Plot data as scatterplot
+                    geom_line(aes(y = A1, colour = "A")) + geom_point(aes(y = A1, colour = "A")) +
+                    geom_line(aes(y = C1, colour = "C")) + geom_point(aes(y = C1, colour = "C")) +
+                    geom_line(aes(y = G1, colour = "G")) + geom_point(aes(y = G1, colour = "G")) +
+                    geom_line(aes(y = T1, colour = "U/T")) + geom_point(aes(y = T1, colour = "U/T")) +
     
-    #Define frequency values
-    frequencyA <- zoomed$A; frequencyC <- zoomed$C
-    frequencyG <- zoomed$G; frequencyT <- zoomed$U.T
+                    #Add axes titles and plot title
+                    xlab("Position") + ylab("Frequency") + ggtitle(opt$title) +
     
-    #Plot frequencies
-    myplot <- ggplot(data=zoomed, aes(x=position)) +
+                    #Increase distance between axes and lables
+                    theme(axis.title.y=element_text(margin=margin(0,20,0,0))) +
+                    theme(axis.title.x=element_text(margin=margin(20,0,0,0))) +
     
-    #Plot data as scatterplot
-    geom_line(aes(y = frequencyA, colour = "A")) + geom_point(aes(y = frequencyA, colour = "A")) +
-    geom_line(aes(y = frequencyC, colour = "C")) + geom_point(aes(y = frequencyC, colour = "C")) +
-    geom_line(aes(y = frequencyG, colour = "G")) + geom_point(aes(y = frequencyG, colour = "G")) +
-    geom_line(aes(y = frequencyT, colour = "U/T")) + geom_point(aes(y = frequencyT, colour = "U/T")) +
+                    #Remove and replace default background plot theme
+                    theme_bw() + theme(panel.border=element_blank(), panel.grid.major=element_blank(),
+                    panel.grid.minor=element_blank(), axis.line=element_line(colour="black")) +
     
-    #Add axes titles
-    xlab("Position") + ylab("Frequency") +
-    
-    #Remove and replace default background plot theme
-    theme_bw() + theme(panel.border=element_blank(), panel.grid.major=element_blank(),
-    panel.grid.minor=element_blank(), axis.line=element_line(colour="black")) +
-    
-    #Specify color values for each of the four nucleotides
-    scale_colour_manual(values=c("#CC79A7", "#56B4E9", "#E69F00", "#009E73"), name="Nucleotide") +
-    
-    #Label axes, add title, define y-axis limits, and specify font size
-    theme(text = element_text(size=14)) + ggtitle(opt$title) + theme(plot.title = element_text(hjust = 0.5)) +
-    
-    #Specify increased distance between axes and axes lables so easier to read
-    theme(axis.title.y=element_text(margin=margin(0,20,0,0))) + theme(axis.title.x=element_text(margin=margin(20,0,0,0)))
-    
-    #Save plot as PDF file
-    ggsave(filename=paste(opt$sample, "-Zoomed", ".png", sep=""), plot=myplot)
+                    #Specify font size for plot text and center title of plot
+                    theme(text = element_text(size=14)) + theme(plot.title = element_text(hjust = 0.5)) +
+
+                    #Specify color values for each of the four different nucleotides
+                    scale_colour_manual(values=c("#CC79A7", "#56B4E9", "#E69F00", "#009E73"), name="Nucleotide")
+
+##############################################################################################################
+                    #Specify output path and save plot as PNG file
+                    ggsave(filename=file.path(output, paste(opt$input, "-", j, ".", i, ".png", sep="")), plot=myplot)
+                }
+            }
 }
