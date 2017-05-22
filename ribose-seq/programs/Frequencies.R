67c6de861877055b07be54957cd8abdc5039d280
Alli Gombolay <agombolay3@gmail.com>
2017-05-21 23:01:58 -0400

    Update Frequencies.R

--- ribose-seq/programs/Frequencies.R
+++ ribose-seq/programs/Frequencies.R
@@ -12,7 +12,7 @@ library(optparse)
 library(ggplot2)
 
 #Command line options
-option_list <- list(make_option(c("-s", "--sample"), help="Sample name"),
+option_list <- list(make_option(c("-s", "--sample"), help="Sample name(s)"),
 make_option(c("-t", "--title"), help="Title will be same for all plots; blank = no title"),
 make_option(c("-r", "--reference"), help="Reference genome (e.g., sacCer2, pombe, ecoli, mm9, hg38)"),
 make_option(c("-d", "--directory"), help="Local user directory (e.g., /projects/home/agombolay3/data/repository)"))
@@ -21,21 +21,22 @@ make_option(c("-d", "--directory"), help="Local user directory (e.g., /projects/
 opt <- parse_args(OptionParser(option_list=option_list))
 
 #############################################################################################################################
-for(i in c("all", "mito", "nucleus")) {
-
+for(i in opt$samples) {
+for(j in c("all", "mito", "nucleus")) {
+print(i)
     #Specify output directory and file
     output <- file.path(opt$directory, "Ribose-Map", "Results", opt$reference, opt$sample, "Frequencies")
-    file <- file.path(output, paste(opt$sample, "-", "Frequencies", ".", opt$reference, ".", i, ".txt", sep=""))
+    file <- file.path(output, paste(opt$sample, "-", "Frequencies", ".", opt$reference, ".", j, ".txt", sep=""))
 
         #Plot only if files exist
             if (file.exists(file)) {
 		#Plot regular and zoomed datasets
-                for(j in c("regular", "zoomed")) {
+                for(k in c("regular", "zoomed")) {
 
 #############################################################################################################################
                     #Specify datasets to be used for each round of loop
-                    if (j == "regular") {data = read.table(file, sep="\t", header=TRUE)}
-                    if (j == "zoomed") {data = read.table(file, sep="\t", header=TRUE)[86:116,]}
+                    if (k == "regular") {data = read.table(file, sep="\t", header=TRUE)}
+                    if (k == "zoomed") {data = read.table(file, sep="\t", header=TRUE)[86:116,]}
     
                     #Define variables to store nucleotide positions and frequency values
                     position <- data$X; A1 <- data$A; C1 <- data$C; G1 <- data$G; T1 <- data$U.T
@@ -69,7 +70,7 @@ for(i in c("all", "mito", "nucleus")) {
 
 #############################################################################################################################
                     #Specify output path and save plot as PNG file
-                    ggsave(filename=file.path(output, paste(opt$sample, "-", j, ".", i, ".png", sep="")), plot=myplot)
+                    ggsave(filename=file.path(output, paste(opt$sample, "-", k, ".", j, ".png", sep="")), plot=myplot)
                 }
             }
 }
