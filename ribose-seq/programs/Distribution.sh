6a4463f57a747dbee585b504945ddc3acb12a107
Alli Gombolay <agombolay3@gmail.com>
2017-05-21 20:11:28 -0400

    Update Distribution.sh

--- ribose-seq/programs/Distribution.sh
+++ ribose-seq/programs/Distribution.sh
@@ -7,17 +7,19 @@
 
 #Usage statement
 function usage () {
-	echo "Usage: Distribution.sh [-i] 'Sample(s)' [-r] 'Reference' [-d] 'Directory' [-h]
+	echo "Usage: Distribution.sh [-s] 'Size' [-i] 'Sample(s)' [-r] 'Reference' [-d] 'Directory' [-h]
+	-s Size of genomic windows
 	-i Input sample(s) (e.g., FS1, FS2, FS3)
 	-r Reference genome (e.g., sacCer2, pombe, ecoli, mm9, hg38)
 	-d Directory (e.g., /projects/home/agombolay3/data/repository/Ribose-seq-Project)"
 }
 
 #Command-line options
-while getopts "i:r:d:h" opt; do
+while getopts "s:i:r:d:h" opt; do
     case $opt in
         #Allow multiple input arguments
-        i ) sample=($OPTARG) ;;
+        s ) size=($OPTARG) ;;
+	i ) sample=($OPTARG) ;;
 	#Allow only one input argument
 	r ) reference=$OPTARG ;;
 	d ) directory=$OPTARG ;;
@@ -33,39 +35,34 @@ fi
 
 #############################################################################################################################
 #Input files
-referenceBed=$directory/ribose-seq/reference/$reference.bed
-coordinates=$directory/ribose-seq/results/$reference/$sample/Coordinates/genome/$sample-Coordinates.genome.bed
+bed=$directory/Ribose-Map/Reference/$reference.bed
+coordinates=$directory/Ribose-Map/Results/$reference/$sample/Coordinates/$subset/$sample-Coordinates.$subset.bed
 
-#Output directories
-output1=$directory/ribose-seq/reference; output2=$directory/ribose-seq/results/$reference/$sample/Distribution/
+#Output directories and files
+output=$directory/Ribose-Map/Results/$reference/$sample/Distribution/; counts=$output/$sample-ObservedCounts.txt
 
-#Create directories
-mkdir -p $output1 $output2
+#Create directory
+mkdir -p $output
 
-#Output files
-counts=$output2/$sample.observed-counts.txt
-referenceWindows=$output1/$reference.windows.bed
-
-#Separate chromosomes of reference into 2.5 kb windows
-bedtools makewindows -g $referenceBed -w 2500 > $referenceWindows
+#Divide chromosomes of reference into windows
+bedtools makewindows -g $bed -w $size > windows.bed
 
 #Determine regions of BED files that intersect and count number of intersections
-#Remove rows where window size is < 2.5 kb and sort based on # of rNMPs in windows
-bedtools intersect -a $referenceWindows -b $coordinates -c -sorted -nonamecheck \
-| awk '{ $5 = $3 - $2 } 1' - | awk -v OFS='\t' '($5 == 2500 ) {print $1,$2,$3,$4}' - \
-| sort -k4 -n - > temporary
+#Remove rows where window size is < size and sort based on # of rNMPs in windows
+bedtools intersect -a windows.bed -b $coordinates -c -sorted -nonamecheck | awk '{ $5 = $3 - $2 } 1' - | \
+awk -v OFS='\t' '($5 == $size ) {print $1,$2,$3,$4}' - | sort -k4 -n - > temporary.txt
 
 #Maximum number of rNMPs in binned data
-max=$(tail -1 temporary | awk '{print $4}' -)
+max=$(tail -1 temporary.txt | awk '{print $4}' -)
 
 #Determine number of windows with 0...maximum rNMPs
 for i in $(seq 0 $max); do
-	windows+=($(awk '$4 == ('$i')' temporary | wc -l))
+	windows+=($(awk '$4 == ('$i')' temporary.txt | wc -l))
 done
 
 #Add column names and # of windows with 0...maximum rNMPs
 echo -e "rNMPs\tWindows" > $counts && paste <(echo "$(seq 0 $max)") \
 <(cat <( IFS=$'\n'; echo "${windows[*]}" )) >> $counts
 
-#Remove temporary file
-rm temporary
+#Remove temp file
+rm temporary.txt
