0a6c8eea12dbb11a01547bf581ba20f9f165165a
Alli Gombolay <agombolay3@gmail.com>
2017-05-23 22:45:21 -0400

    Update Distribution.R

--- ribose-seq/programs/Distribution.R
+++ ribose-seq/programs/Distribution.R
@@ -7,111 +7,126 @@
 #If the observed or expected counts are < 5, then categories will be collapsed.
 
 #Load ggplot2
-library("ggplot2")
+library(optparse)
+library(ggplot2)
 
-#Argument 1 = dataset file
-dataset <- commandArgs(trailingOnly = TRUE)[1]
-
-#Argument 2 = PDF filename
-filename <- commandArgs(trailingOnly = TRUE)[2]
-
-#############################################################################################################################
-#OBSERVED DATA
-
-#Assign rNMP counts to variable
-counts <- read.csv(file = dataset, sep = "\t")[ ,c('rNMPs')]
-
-#Assign # of windows to variable
-observed.windows <- read.csv(file = dataset, sep = "\t")[ ,c('Windows')]
-
-#Sum # of windows in observed dataset
-observed.total <- sum(observed.windows)
-
-#Calculate proportions (# of windows/total)
-observed.frequencies <- observed.windows/observed.total
-
-#############################################################################################################################
-#EXPECTED DATA
-
-#Calculate Poisson distribution lambda value
-lambda <- sum(observed.windows*counts/observed.total)
-
-#Calculate frequencies based on lambda value
-expected.frequencies <- c(dpois(counts, lambda))
-
-#Calculate expected # of windows based on frequencies
-expected.windows <- c(dpois(counts, lambda))*observed.total
-
-#Determine if data need to be collapsed into few categories
-
-#Rows with observed windows >=5
-idx <- which(expected.windows >= 5)
-
-#Row at which to start collapsing data
-row.start <- length(expected.windows[idx])
-
-#Number of rows of data in dataset
-row.end <- length(readLines(dataset))-1
-
-#Sum # of observed windows that need to be collapsed
-collapsed.observed.sum <- sum(observed.windows[c(row.start:row.end)])
-
-#Select rows of observed data that do not need to be collapsed
-observed.data <- read.csv(file=dataset, sep = "\t")[ 1:row.start-1,]
-
-#Append collapsed data to last row of dataset to create final observed dataset
-observed.data.final <- rbind(observed.data, c(row.start-1, collapsed.observed.sum))
-observed.windows.final <- observed.data.final[ ,c('Windows')]
-
-#Sum # of expected windows that need to be collapsed
-collapsed.expected.sum <- sum(expected.windows[c(row.start:row.end)])
-
-#Select rows of expected data that do not need to be collapsed and afterward
-#Append collapsed data to last row of dataset to create final expected dataset
-expected.windows.final <- c(expected.windows[c(0:(row.start-1))],collapsed.expected.sum)
-
-#############################################################################################################################
-#CHI SQUARE TEST
-
-#Calculate Chi Square Statistic from observed and expected number of windows in dataset
-chi.square <- sum(((observed.windows.final - expected.windows.final)^2)/expected.windows.final)
-
-#Calculate test statistic
-#df=#categories-#estimated parameters-1
-test.statistic <- qchisq(.95, df=row.start-1-1)
-
-#Determine associated p-value
-if (chi.square > test.statistic) {
-  print("p-value < 0.05")
-}
-
-#############################################################################################################################
-#Create dataframe
-dataset <- data.frame(
-  type = factor(c(replicate(row.start, "Poisson"), replicate(row.start, "Observed"))),
-  counts = factor(c(seq(0,row.start-1,1),seq(0,row.start-1,1)), levels=seq(0,row.start-1,1)),
-  windows = c(expected.windows.final,observed.windows.final)
+#Command line options
+option_list <- list(
+make_option(c("-s", "--sample"), help="Sample name(s) (e.g., FS1, FS2, FS3)"),
+make_option(c("-t", "--title"), help="Title will be same for all plots; blank space = no titles"),
+make_option(c("-r", "--reference"), help="Reference genome (e.g., sacCer2, pombe, ecoli, mm9, hg38)"),
+make_option(c("-d", "--directory"), help="Local user directory (e.g., /projects/home/agombolay3/data/repository)")
 )
 
-#Plot distributions
-myplot <- ggplot(data=dataset, aes(x=counts, y=windows, fill=type)) +
+#Get command line options, if -h encountered print help
+opt <- parse_args(OptionParser(option_list=option_list))
 
-  #Plot data as barchart
-  geom_bar(stat="identity", position=position_dodge(width=0.8), width=0.6) +
-  
-  #Specify color values for distributions
-  scale_fill_manual(values=c("#000000","#999999")) +
+for(i in opt$sample) {
+    for(j in c("all", "mito", "nucleus")) {
+        
+    #Specify output directory and file
+    output <- file.path(opt$directory, "Ribose-Map", "Results", opt$reference, opt$sample, "Distribution")
+    file <- file.path(output, paste(opt$sample, "-", "Counts", ".", opt$reference, ".", j, ".txt", sep=""))
+    
+    #Plot only if files exist
+    if (file.exists(file)) {
+#############################################################################################################################
+    #OBSERVED DATA
 
-  #Label axes, add title, and specify font size
-  xlab("rNMP Count per 2.5 kb Window")+ylab("Number of Windows") +
-  guides(fill=guide_legend(title=""))+theme(text=element_text(size=14)) +
+    #Assign rNMP counts to variable
+    number.rNMPs <- read.csv(file=file, sep="\t")[ ,c('rNMPs')]
 
-  #Remove and replace default background plot theme
-  theme_bw()+theme(panel.border=element_blank(),panel.grid.major=element_blank(),
-  panel.grid.minor=element_blank(),axis.line=element_line(colour="black")) +
+    #Assign # of windows to variable
+    observed.windows <- read.csv(file=file, sep="\t")[ ,c('Windows')]
 
-  #Customize x axis labels and expand width of y axis
-  scale_x_discrete(labels=seq(0,row.start-1,1))+scale_y_continuous(expand=c(0.015,0))
+#############################################################################################################################
+    #EXPECTED DATA
 
-#Save plot as PDF file
-ggsave(filename=filename, plot=myplot)
+    #Calculate Poisson distribution lambda value
+    lambda <- sum(observed.windows*number.rNMPs)/sum(observed.windows)
+
+    #Calculate expected # of windows based on frequencies
+    expected.windows <- c(dpois(number.rNMPs, lambda))*sum(observed.windows)
+
+#############################################################################################################################
+    #COLLAPSE DATA IF EXPECTED COUNTS >=5
+
+    #Rows with observed windows >=5
+    idx <- which(expected.windows >= 5)
+
+    #Print error message if idx equals zero
+    if ((identical(idx, integer(0)))=="TRUE") {
+        message("Plot cannot be created for", " ", i, " ", "(", j, ")")
+    } else {
+
+    #Row at which to start collapsing data
+    row.start <- length(expected.windows[idx])
+
+    #Number of rows of data in dataset
+    row.end <- length(readLines(file))-1
+
+    #Sum # of observed windows that need to be collapsed
+    collapsed.observed.sum <- sum(observed.windows[c(row.start:row.end)])
+
+    #Append collapsed data to last row of dataset to create re-binned expected dataset
+    observed.windows.final <- c(observed.windows[1:row.start-1],collapsed.observed.sum)
+
+    #Sum # of expected windows that need to be collapsed
+    collapsed.expected.sum <- sum(expected.windows[c(row.start:row.end)])
+
+    #Append collapsed data to last row of dataset to create re-binned expected dataset
+    expected.windows.final <- c(expected.windows[1:row.start-1],collapsed.expected.sum)
+
+#############################################################################################################################
+    #CHI SQUARE GOODNESS OF FIT TEST FOR POISSON DISTRIBUTION
+
+    #Calculate Chi Square Statistic from observed/expected number of windows in re-binned data
+    chi.square <- sum(((observed.windows.final - expected.windows.final)^2)/expected.windows.final)
+
+    #Calculate critical value at 95% confidence
+    test.statistic <- qchisq(.95, df=row.start-1-1)
+
+    #Determine if p-value is < 0.05
+    if (chi.square > test.statistic) {
+        message("p-value for", " ", i, " ", "(", j, ")", " ", "< 0.05")
+    } else {
+        message("p-value for", " ", i, " ", "(", j, ")", " ", ">= 0.05")
+    }
+
+#############################################################################################################################
+    #PLOT OBSERVED VS. EXPECTED DISTRIBUTION OF RNMPS ACROSS GENOME
+
+    #Create dataframe
+    data <- data.frame(
+        type = factor(c(replicate(row.start, "Poisson"), replicate(row.start, "Observed"))),
+        rNMPs = factor(c(seq(0,row.start-1,1),seq(0,row.start-1,1)), levels=seq(0,row.start-1,1)),
+        windows = c(expected.windows.final,observed.windows.final)
+        )
+
+    #Plot distributions
+    myplot <- ggplot(data=data, aes(x=rNMPs, y=windows, fill=type)) +
+
+    #Plot data as barcharts
+    geom_bar(stat="identity",position=position_dodge(width=0.8), width=0.6) +
+
+    #Remove and replace default background plot theme
+    theme_bw() + theme(panel.border=element_blank(), panel.grid.major=element_blank(),
+    panel.grid.minor=element_blank(), axis.line=element_line(colour="black"))+
+    
+    #Add axes titles and plot title (specified by user)
+    xlab("rNMPs per Window") + ylab("Number of Windows") + ggtitle(opt$title) +
+    
+    #Specify font size and center title (if any) of plot on page
+    theme(text=element_text(size=14)) + theme(plot.title=element_text(hjust=0.5)) +
+
+    #Specify colors of bars, remove legend title, and decrease space between bars and x-axis
+    scale_fill_manual(values=c("#000000", "#999999"), name="") + scale_y_continuous(expand=c(0.015,0))
+
+    #Save plot as PDF file
+    ggsave(filename=file.path(output, paste(opt$sample, "-", "Distribution", ".", j, ".png", sep="")), plot=myplot)
+
+}
+}
+}
+message("Plotting of", " ", i , " ", "is complete")
+}
