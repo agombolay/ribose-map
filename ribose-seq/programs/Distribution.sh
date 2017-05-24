26c4e8d70c2adb273354ea510549d3e23f7edbaf
Alli Gombolay <agombolay3@gmail.com>
2017-05-23 20:29:12 -0400

    Update Distribution.sh

--- ribose-seq/programs/Distribution.sh
+++ ribose-seq/programs/Distribution.sh
@@ -43,7 +43,8 @@ for sample in ${sample[@]}; do
 	coordinates=$directory/Ribose-Map/Results/$reference/$sample/Coordinates/$sample-Coordinates.$subset.bed
 
 	#Output directories and files
-	output=$directory/Ribose-Map/Results/$reference/$sample/Distribution; dataset=$output/$sample-Counts.$subset.txt
+	output=$directory/Ribose-Map/Results/$reference/$sample/Distribution;
+	dataset=$output/$sample-Counts.$reference.$subset.txt
 
 	#Create directory and remove old file
 	mkdir -p $output; rm -f $output/$dataset
