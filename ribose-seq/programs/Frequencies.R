82bf368fc9b1a9a4bd9108e32d56a196ad81183d
Alli Gombolay <agombolay3@gmail.com>
2017-05-23 22:04:09 -0400

    Update Frequencies.R

--- ribose-seq/programs/Frequencies.R
+++ ribose-seq/programs/Frequencies.R
@@ -14,7 +14,7 @@ library(ggplot2)
 #Command line options
 option_list <- list(
 make_option(c("-s", "--sample"), help="Sample name(s) (e.g., FS1, FS2, FS3)"),
-make_option(c("-t", "--title"), help="Title will be same for all plots; blank space = no titles"),
+make_option(c("-t", "--title"), help="Title will be same for all plots; blank space=no titles"),
 make_option(c("-r", "--reference"), help="Reference genome (e.g., sacCer2, pombe, ecoli, mm9, hg38)"),
 make_option(c("-d", "--directory"), help="Local user directory (e.g., /projects/home/agombolay3/data/repository)")
 )
