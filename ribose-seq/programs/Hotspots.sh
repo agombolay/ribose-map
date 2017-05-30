86d9d42d6e23d4fa9c4d79e56d3fc801a119ccb0
Alli Gombolay <agombolay3@gmail.com>
2017-05-30 16:19:47 -0400

    Update Hotspots.sh

--- ribose-seq/programs/Hotspots.sh
+++ ribose-seq/programs/Hotspots.sh
@@ -9,23 +9,17 @@
 function usage () {
 	echo "Usage: Alignment.sh [-s] 'Sample(s)' [-u] 'UMI' [-m] 'Min' [-p] 'Path' [-i] 'Index' [-d] 'Directory' [-h]
 		-s Sample name(s) (e.g., FS1, FS2, FS3)
-		-u Length of UMI (e.g., NNNNNNNN or NNNNNNNNNNN)
-		-m Minimum length of read to retain after trimming (e.g., 50)
-		-p Path (e.g., /projects/home/agombolay3/data/bin/Trimmomatic-0.36)
-		-i Basename of Bowtie2 index (e.g., sacCer2, pombe, ecoli, mm9, or hg38)
+		-r Basename of Bowtie2 index (e.g., sacCer2, pombe, ecoli, mm9, or hg38)
 		-d Local user directory (e.g., /projects/home/agombolay3/data/repository)"
 }
 
 #Command-line options
-while getopts "s:u:m:p:i:d:h" opt; do
+while getopts "s:r:d:h" opt; do
     case "$opt" in
         #Allow multiple input arguments
         s ) sample=($OPTARG) ;;
 	#Allow only one input argument
-	u ) UMI=$OPTARG ;;
-	m ) MIN=$OPTARG ;;
-	p ) path=$OPTARG ;;
-	i ) index=$OPTARG ;;
+	r ) reference=$OPTARG ;;
 	d ) directory=$OPTARG ;;
         #Print usage statement
         h ) usage ;;
