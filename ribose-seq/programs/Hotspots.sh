06b6b6a95650d9d6aaec31af219b44c0d152de21
Alli Gombolay <agombolay3@gmail.com>
2017-05-30 15:57:43 -0400

    Update Hotspots.sh

--- ribose-seq/programs/Hotspots.sh
+++ ribose-seq/programs/Hotspots.sh
@@ -1,1 +1,42 @@
+#!/usr/bin/env bash
+
+#© 2016 Alli Gombolay
+#Author: Alli Lauren Gombolay
+#E-mail: alli.gombolay@gatech.edu
+#This program aligns trimmed reads to reference genome using Bowtie2 and de-duplicates reads based on UMI's
+#Note: FASTQ files must be located in the users's Sequencing folder (/LocalDirectory/Ribose-Map/Sequencing)
+
+#Usage statement
+function usage () {
+	echo "Usage: Alignment.sh [-s] 'Sample(s)' [-u] 'UMI' [-m] 'Min' [-p] 'Path' [-i] 'Index' [-d] 'Directory' [-h]
+		-s Sample name(s) (e.g., FS1, FS2, FS3)
+		-u Length of UMI (e.g., NNNNNNNN or NNNNNNNNNNN)
+		-m Minimum length of read to retain after trimming (e.g., 50)
+		-p Path (e.g., /projects/home/agombolay3/data/bin/Trimmomatic-0.36)
+		-i Basename of Bowtie2 index (e.g., sacCer2, pombe, ecoli, mm9, or hg38)
+		-d Local user directory (e.g., /projects/home/agombolay3/data/repository)"
+}
+
+#Command-line options
+while getopts "s:u:m:p:i:d:h" opt; do
+    case "$opt" in
+        #Allow multiple input arguments
+        s ) sample=($OPTARG) ;;
+	#Allow only one input argument
+	u ) UMI=$OPTARG ;;
+	m ) MIN=$OPTARG ;;
+	p ) path=$OPTARG ;;
+	i ) index=$OPTARG ;;
+	d ) directory=$OPTARG ;;
+        #Print usage statement
+        h ) usage ;;
+    esac
+done
+
+#Exit program if [-h]
+if [ "$1" == "-h" ]; then
+        exit
+fi
+
+#############################################################################################################################
 bedtools genomecov
