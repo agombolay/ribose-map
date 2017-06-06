7c100e2f5658511ce150f5babeb34605515de985
Alli Gombolay <agombolay3@gmail.com>
2017-06-06 15:10:12 -0400

    Update Coordinates.sh

--- ribose-seq/programs/Coordinates.sh
+++ ribose-seq/programs/Coordinates.sh
@@ -33,7 +33,7 @@ fi
 
 #Determine coordinates
 for sample in ${sample[@]}; do
-	for subset in "all" "mito" "nucleus"; do
+	for subset in "mito" "nucleus"; do
 	
 #############################################################################################################################
 		#Create directory
