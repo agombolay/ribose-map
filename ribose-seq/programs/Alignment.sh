c07d812bc4594528be533e1ae809159e48f51916
Alli Gombolay <agombolay3@gmail.com>
2017-06-20 11:54:10 -0400

    Update Alignment.sh

--- ribose-seq/programs/Alignment.sh
+++ ribose-seq/programs/Alignment.sh
@@ -87,7 +87,7 @@ if [[ $UMI == "N"* ]]; then
 	umi_tools extract -I temp1.fq -p $UMI --3prime -v 0 -S Read1.fq
 fi
 --read2-in, --read2-out
---quality-filter-threshold
+--quality-filter-threshold=10
 #############################################################################################################################
 #STEP 4: Align reads to reference genome and save Bowtie statistics to file
 #Single End Reads
