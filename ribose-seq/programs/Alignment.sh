8b0d9d6cca55ed81a18a5c3009ee903ae3837c88
Alli Gombolay <agombolay3@gmail.com>
2017-06-20 11:55:10 -0400

    Update Alignment.sh

--- ribose-seq/programs/Alignment.sh
+++ ribose-seq/programs/Alignment.sh
@@ -123,8 +123,8 @@ grep -e '_$barcode' -e '@HG' -e '@SQ' -e '@PG' dedup.sam > filtered.sam
 samtools view filtered.sam -b -S | samtools sort -o $output; samtools index $output
 
 #############################################################################################################################
-#Remove temporary files
-rm -f Paired*.fq Unpaired*.fq temp1.fq Read*.fq mapped.* dedup.sam dedup.bam filtered.sam
-
 #Notify user that alignment step is complete for which samples
 echo "Alignment of $sample to $index reference genome is complete"
+
+#Remove temporary files
+rm -f Paired*.fq Unpaired*.fq temp1.fq Read*.fq mapped.* dedup.* filtered.sam
