0d6ef279a250b159d63f856aeff5dcae2325ee21
Alli Gombolay <agombolay3@gmail.com>
2017-06-24 21:57:43 -0400

    Delete wrapper.sh

--- wrapper.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-#Author: Alli Gombolay
-#This program automatically runs input FASTQ.GZ files through the Ribose-seq analysis pipeline
-#Adapted from Jay Hesselberth's code located at https://github.com/hesselberthlab/modmap/tree/snake
-
-if [ "$1" == "-h" ]; then
-        exit;
-fi
