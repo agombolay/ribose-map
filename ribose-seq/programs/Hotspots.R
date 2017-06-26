8680e85eaa176047026b5d2bdead0a1b02686415
Alli Gombolay <agombolay3@gmail.com>
2017-06-25 22:50:30 -0400

    Update Hotspots.R

--- ribose-seq/programs/Hotspots.R
+++ ribose-seq/programs/Hotspots.R
@@ -22,41 +22,43 @@ make_option(c("-d", "--directory"), help="Local user directory (e.g., /projects/
 opt <- parse_args(OptionParser(option_list=option_list))
 
 for(i in opt$sample) {
-    for(j in c("mito" "nucleus")) {
         
         #Specify output directory and file
         output <- file.path(opt$directory, "Ribose-Map", "Results", opt$reference, opt$sample, "Hotspots")
-        file <- file.path(output, paste(opt$sample, "-", "Coverage", ".", j, ".txt", sep=""))
-
-        #Plot only if files exist
-        if (file.exists(file)) {
+	files <- list.files(path=output, pattern=".bed", full.names=TRUE, recursive=FALSE)
+        
+	for(file in files){
+		
+		#Plot only if files exist
+        	if (file.exists(file)) {
             
-            #Specify dataset
-            data=read.table(file, sep="\t", header=FALSE)
+        		#Specify dataset
+            		data=read.table(file, sep="\t", header=FALSE)
 
 #############################################################################################################################
-        #Plot coverage
-        myplot <- ggplot(data, aes(x=1:length(data[,3]), y=data[,3])) +
+        		#Plot coverage
+        		myplot <- ggplot(data, aes(x=1:length(data[,3]), y=data[,3])) +
 
-        #Add axes titles and plot title
-        xlab("Position in genome") + ylab("Frequency of rNMPs") + ggtitle("") +
+        		#Add axes titles and plot title
+        		xlab("Position in genome") + ylab("Frequency of rNMPs") + ggtitle("") +
 
-        #Replace default background plot theme
-        theme_bw() + theme(panel.border=element_blank(), panel.grid.major=element_blank(),
-        panel.grid.minor=element_blank(), axis.line=element_line(colour="black")) +
+        		#Replace default background plot theme
+        		theme_bw() + theme(panel.border=element_blank(), panel.grid.major=element_blank(),
+        		panel.grid.minor=element_blank(), axis.line=element_line(colour="black")) +
 
-        #Plot data as scatterchart with connecting lines
-        geom_point(shape=1, colour="blue4") + geom_line(aes(y=data[,3]), colour="blue4") +
+        		#Plot data as scatterchart with connecting lines
+        		geom_point(shape=1, colour="blue4") + geom_line(aes(y=data[,3]), colour="blue4") +
 				
-        #Decrease space between scatterplot and x-axis/y-axis
-        scale_y_continuous(expand=c(0.015,0)) + scale_x_continuous(expand=c(0.015,0)) +
+        		#Decrease space between scatterplot and x-axis/y-axis
+        		scale_y_continuous(expand=c(0.015,0)) + scale_x_continuous(expand=c(0.015,0)) +
 
-        #Specify font size and center title (if any) of plot on page
-        theme(text=element_text(size=20)) + theme(plot.title=element_text(hjust=0.5))
+        		#Specify font size and center title (if any) of plot on page
+        		theme(text=element_text(size=20)) + theme(plot.title=element_text(hjust=0.5))
 
 #############################################################################################################################
 #Save plot as PNG file
 ggsave(filename=file.path(output, paste(file_path_sans_ext(basename(file)), ".png", sep="")), plot=myplot, width=20, dpi=300)
+
 }
 }
 }
