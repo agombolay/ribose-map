af050f5ab1c6bbe173a3cb9a3bdbe84b9f223db1
Alli Gombolay <agombolay3@gmail.com>
2017-07-04 20:06:14 -0400

    Update Frequencies.sh

--- ribose-seq/programs/Frequencies.sh
+++ ribose-seq/programs/Frequencies.sh
@@ -43,6 +43,8 @@ for sample in ${sample[@]}; do
 	BED=$directory/Ribose-Map/References/$reference.bed; FASTA=$directory/Ribose-Map/References/$reference.fa
 	coordinates=$directory/Ribose-Map/Results/$reference/$sample/Coordinates/$sample-Coordinates.$subset.bed
 	
+	if [ -f $coordinates ]; then
+	
 	#Output file
 	dataset=$directory/Ribose-Map/Results/$reference/$sample/Frequencies/$sample-Frequencies.$subset.txt
 	
@@ -168,6 +170,8 @@ for sample in ${sample[@]}; do
 	
 	#Remove temp files
 	rm -f ./*Upstream.* ./*Downstream.* ./RiboBases.txt ./temp.fa*
+	
+	fi
 	
 	done
 done
