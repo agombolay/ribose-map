1b3da3f5a5e19b010c9d5d8a9be58c0fa529f4be
Alli Gombolay <agombolay3@gmail.com>
2017-07-12 13:48:12 -0400

    Update Distribution.sh

--- ribose-seq/programs/Distribution.sh
+++ ribose-seq/programs/Distribution.sh
@@ -37,7 +37,7 @@ for sample in ${sample[@]}; do
 #############################################################################################################################
 	#STEP 1: Count number of positions containing 0...max # of rNMPs
 	
-	for subset in "nucleus"; do
+	for subset in "mito"; do
 		
 		#Create directory
 		mkdir -p $directory/Ribose-Map/Results/$reference/$sample/Distribution
