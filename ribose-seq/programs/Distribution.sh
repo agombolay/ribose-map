4a83155ba68c51a5f14cdb6829054101a95d563d
Alli Gombolay <agombolay3@gmail.com>
2017-07-13 20:47:23 -0400

    Update Distribution.sh

--- ribose-seq/programs/Distribution.sh
+++ ribose-seq/programs/Distribution.sh
@@ -79,7 +79,7 @@ for sample in ${sample[@]}; do
 				paste <(echo "$(seq 0 $max)") <(cat temp3.txt) >> $counts
 
 				#Print completion status
-				echo "Counts for $sample ($chr) have been determined"
+				echo "Counts for $sample ($subset) have been determined"
 	
 				#Remove temp files
 				rm -f temp3.txt
