c77b564d4572ee280b183b96f14bbf313786209f
Alli Gombolay <agombolay3@gmail.com>
2017-08-24 21:18:38 -0400

    Update Alignment.sh

--- ribose-seq/programs/Alignment.sh
+++ ribose-seq/programs/Alignment.sh
@@ -67,10 +67,10 @@ if [[ $type == "SE" ]]; then
 	cat $Fastq1 | seqtk seq -r - > $output/Reverse.fq
 	
 	#Extract UMI from 3' ends of reads (append UMI to read name)
-	umi_tools extract -I $output/Reverse.fq -p $UMI --3prime -v 0 -S $output/Read1-UMI.fq
+	umi_tools extract -I $output/Reverse.fq -p $UMI --3prime -v 0 -S $output/Extract.fq
 	
 	#Trim FASTQ files based on quality and adapter content
-	java -jar $path/trimmomatic-0.36.jar SE -phred33 $output/Read1-UMI.fq $output/Read1.fq \
+	java -jar $path/trimmomatic-0.36.jar SE -phred33 $output/Extract.fq $output/Read1.fq \
 	ILLUMINACLIP:$path/adapters/TruSeq3-SE.fa:2:30:10 LEADING:10 MINLEN:$min
 		
 		if [[ -n $UMI ]] && [[ -z $barcode ]]; then
