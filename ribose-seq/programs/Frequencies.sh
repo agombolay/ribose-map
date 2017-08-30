db7d7e4094dbc805826b490168512b7e7c04f6d7
Alli Gombolay <agombolay3@gmail.com>
2017-08-29 20:56:06 -0400

    Update Frequencies.sh

--- ribose-seq/programs/Frequencies.sh
+++ ribose-seq/programs/Frequencies.sh
@@ -38,7 +38,7 @@ for sample in ${sample[@]}; do
 	#Input files
 	reads=$directory/Ribose-Map/Results/$reference/$sample/Coordinates/$sample-ReadInformation.$subset.txt
 	BED=$directory/Ribose-Map/References/$reference.bed; FASTA=$directory/Ribose-Map/References/$reference.fa
-	coordinates=$directory/Ribose-Map/Results/$reference/$sample/Coordinates/$sample-Coordinates.$subset.bed
+	coordinates=$directory/Ribose-Map/Results/$reference/$sample/Coordinates/$sample-Coordinates.bed
 	
 	#Output directory
 	output=$directory/Ribose-Map/Results/$reference/$sample/Frequencies
@@ -60,16 +60,18 @@ for sample in ${sample[@]}; do
 	
 	#Subset FASTA file based on region
 	if [ $subset == "mito" ]; then
-		chromosomes=$(awk '{print $1}' $BED | grep -E '(chrM|MT)')
-		samtools faidx $FASTA $chromosomes > temp.fa; samtools faidx temp.fa
+		chr=$(awk '{print $1}' $BED | grep -E '(chrM|MT)')
+		samtools faidx $FASTA $chr > $output/temp.fa; samtools faidx $output/temp.fa
 	elif [ $subset == "nucleus" ]; then
-		chromosomes=$(awk '{print $1}' $BED | grep -vE '(chrM|MT)')
-		samtools faidx $FASTA $chromosomes > temp.fa; samtools faidx temp.fa
+		chr=$(awk '{print $1}' $BED | grep -vE '(chrM|MT)')
+		samtools faidx $FASTA $chr > $output/temp.fa; samtools faidx $output/temp.fa
 	fi
 
 	#Calculate counts of each nucleotide
-	A_BkgCount=$(grep -v '>' temp.fa | grep -o 'A' - | wc -l); C_BkgCount=$(grep -v '>' temp.fa | grep -o 'C' - | wc -l)
-	G_BkgCount=$(grep -v '>' temp.fa | grep -o 'G' - | wc -l); T_BkgCount=$(grep -v '>' temp.fa | grep -o 'T' - | wc -l)
+	A_BkgCount=$(grep -v '>' $output/temp.fa | grep -o 'A' - | wc -l)
+	C_BkgCount=$(grep -v '>' $output/temp.fa | grep -o 'C' - | wc -l)
+	G_BkgCount=$(grep -v '>' $output/temp.fa | grep -o 'G' - | wc -l)
+	T_BkgCount=$(grep -v '>' $output/temp.fa | grep -o 'T' - | wc -l)
 	
 	#Calculate total number of nucleotides
 	total_Bkg=$(($A_BkgCount+$C_BkgCount+$G_BkgCount+$T_BkgCount))
@@ -91,11 +93,14 @@ for sample in ${sample[@]}; do
 	fi
 	
 	#Extract rNMP bases
-	bedtools getfasta -s -fi temp.fa -bed $output/$sample-Coordinates.$subset.bed | grep -v '>' - > RiboBases.txt
+	bedtools getfasta -s -fi $output/temp.fa -bed $output/$sample-Coordinates.$subset.bed \
+	| grep -v '>' - > $output/RiboBases.txt
 	
 	#Calculate counts of rNMPs
-	A_RiboCount=$(awk '$1 == "A"' RiboBases.txt | wc -l); C_RiboCount=$(awk '$1 == "C"' RiboBases.txt | wc -l)
-	G_RiboCount=$(awk '$1 == "G"' RiboBases.txt | wc -l); U_RiboCount=$(awk '$1 == "T"' RiboBases.txt | wc -l)
+	A_RiboCount=$(awk '$1 == "A"' $output/RiboBases.txt | wc -l)
+	C_RiboCount=$(awk '$1 == "C"' $output/RiboBases.txt | wc -l)
+	G_RiboCount=$(awk '$1 == "G"' $output/RiboBases.txt | wc -l)
+	U_RiboCount=$(awk '$1 == "T"' $output/RiboBases.txt | wc -l)
 	
 	#Calculate total number of rNMPs
 	RiboCount=$(($A_RiboCount+$C_RiboCount+$G_RiboCount+$U_RiboCount))
@@ -112,34 +117,38 @@ for sample in ${sample[@]}; do
 #############################################################################################################################
 	#STEP 3: Obtain coordinates/sequences of dNMPs +/- 100 bp from rNMPs
 
-	#Obtain coordinates of sequences upstream/downstream from rNMPs
-	bedtools flank -i $coordinates -s -g $BED -l 100 -r 0 | awk '$2 != "0" && $3 != "0"' - > Upstream.bed
-	bedtools flank -i $coordinates -s -g $BED -l 0 -r 100 | awk '$2 != "0" && $3 != "0"' - > Downstream.bed
+	#Obtain coordinates of sequences up/downstream from rNMPs and remove coordinates that are 0,0
+	bedtools flank -i $coordinates -s -g $BED -l 100 -r 0 | awk '$2 != "0" && $3 != "0"' - > $output/Up.bed
+	bedtools flank -i $coordinates -s -g $BED -l 0 -r 100 | awk '$2 != "0" && $3 != "0"' - > $output/Down.bed
 	
 	#Obtain sequences of the sequences upstream/downstream from rNMPs
-	bedtools getfasta -s -fi temp.fa -bed Upstream.bed -fo Upstream.fa
-	bedtools getfasta -s -fi temp.fa -bed Downstream.bed -fo Downstream.fa
+	bedtools getfasta -s -fi temp.fa -bed $output/Upstream.bed -fo $output/Up.fa
+	bedtools getfasta -s -fi temp.fa -bed $output/Downstream.bed -fo $output/Down.fa
 	
 #############################################################################################################################
 	#STEP 4: Insert tabs between sequences of dNMPs +/- 100 bp from rNMPs
 
-	#Extract sequences from FASTA files (reverse order of upstream) and insert tabs between each base
-	grep -v '>' Upstream.fa | rev > Upstream.txt; cat Upstream.txt | sed 's/.../& /2g;s/./& /g' > Upstream.tab
-	grep -v '>' Downstream.fa > Downstream.txt; cat Downstream.txt | sed 's/.../& /2g;s/./& /g' > Downstream.tab
+	#Extract sequences (reverse order of upstream)
+	grep -v '>' $output/Upstream.fa | rev > $output/Up.txt
+	grep -v '>' $output/Downstream.fa > $output/Down.txt
+	
+	#Insert tabs between each base for easier parsing
+	cat $output/Upstream.txt | sed 's/.../& /2g;s/./& /g' > $output/Up.tab
+	cat $output/Downstream.txt | sed 's/.../& /2g;s/./& /g' > $output/Down.tab
 
-	#Save lists of dNMPs at each of the 100 upstream/downstream positions in separate files for later
+	#Save lists of dNMPs at each of the +/-100 positions in separate files
 	for i in {1..100}; do
-		awk -v field=$i '{ print $field }' Upstream.tab > $sample.Column.$i.Upstream.$reference.$subset.txt
-		awk -v field=$i '{ print $field }' Downstream.tab > $sample.Column.$i.Downstream.$reference.$subset.txt
+		awk -v field=$i '{ print $field }' $output/Up.tab > $output/$sample.Up.$i.txt
+		awk -v field=$i '{ print $field }' $output/Down.tab > $output/$sample.Down.$i.txt
 	done
 	
 #############################################################################################################################
 	#STEP 5: Calculate frequencies of dNMPs +/- 100 base pairs from rNMPs
 
 	#Calculate frequencies at each position
-	for direction in "Upstream" "Downstream"; do
+	for direction in "Up" "Down"; do
 		
-		for file in `ls -v ./$sample.Column.*.$direction.$reference.$subset.txt`; do
+		for file in `ls -v $output/$sample.$direction.$i.txt`; do
 		
 		#Calculate count of each dNMP
 		A_FlankCount=$(grep -o 'A' $file | wc -l); C_FlankCount=$(grep -o 'C' $file | wc -l)
@@ -155,13 +164,13 @@ for sample in ${sample[@]}; do
 		T_FlankFreq=$(echo "scale=12; ($T_FlankCount/$FlankCount)/$T_BkgFreq" | bc | awk '{printf "%.12f\n", $0}')
 		
 		#Save normalized dNMPs frequencies to TXT file
-		echo $A_FlankFreq >> A_$direction.txt; echo $C_FlankFreq >> C_$direction.txt
-		echo $G_FlankFreq >> G_$direction.txt; echo $T_FlankFreq >> T_$direction.txt
+		echo $A_FlankFreq >> $output/A_$direction.txt; echo $C_FlankFreq >> $output/C_$direction.txt
+		echo $G_FlankFreq >> $output/G_$direction.txt; echo $T_FlankFreq >> $output/T_$direction.txt
 		
-		if [ $direction == "Upstream" ]; then
-			Up=$(paste A_Upstream.txt C_Upstream.txt G_Upstream.txt T_Upstream.txt | tac -)
-		elif [ $direction == "Downstream" ]; then
-			Down=$(paste A_Downstream.txt C_Downstream.txt G_Downstream.txt T_Downstream.txt)
+		if [ $direction == "Up" ]; then
+			Up=$(paste $output/A_Up.txt $output/C_Up.txt $output/G_Up.txt $output/T_Up.txt | tac -)
+		elif [ $direction == "Down" ]; then
+			Down=$(paste $output/A_Down.txt $output/C_Down.txt $output/G_Down.txt $output/T_Down.txt)
 		fi
 				
 		done
@@ -171,29 +180,31 @@ for sample in ${sample[@]}; do
 	#STEP 6: Create and save dataset file containing nucleotide frequencies
 
 	#Add nucleotides to header line
-	echo -e "\tA\tC\tG\tU/T" > $dataset
+	echo -e "\tA\tC\tG\tU/T" > $output/$sample-Frequencies.$subset.txt
 
-	#Add positions and frequencies of nucleotides in correct order to create dataset of frequencies
-	paste <(echo "$(seq -100 1 100)") <(cat <(echo "$Up") <(echo "$Ribo") <(echo "$Down")) >> $dataset
-	
-	#Print completion status
-	echo "Calculation of frequencies for $sample ($subset) is complete"
-	
-	#Remove temp files
-	rm -f ./*Upstream.* ./*Downstream.* ./RiboBases.txt ./temp.fa*
+	#Add positions and frequencies of nucleotides in correct order to create dataset
+	paste <(echo "$(seq -100 1 100)") <(cat <(echo "$Up") <(echo "$Ribo") <(echo "$Down")) \
+	>> $output/$sample-Frequencies.$subset.txt
 
 #############################################################################################################################
 	#STEP 7: Create and save file containing background nucleotide frequencies
 		
 	#Add nucleotides to header line
-	echo -e "A\tC\tG\tT" > $background
+	echo -e "A\tC\tG\tT" > $output/$sample-BackgroundFrequencies.txt
 	
 	#Add frequencies of nucleotides in reference genome
-	paste <(echo -e "$A_BkgFreq\t$C_BkgFreq\t$G_BkgFreq\t$T_BkgFreq") >> $background
+	paste <(echo -e "$A_BkgFreq\t$C_BkgFreq\t$G_BkgFreq\t$T_BkgFreq") >> $output/$sample-BackgroundFrequencies.txt
 	
 	#Add total number of nucleotides in reference genome
-	echo -e "Total number of bases in reference genome: $((total_Bkg*2))" >> $background
+	echo -e "Total number of bases in reference genome: $((total_Bkg*2))" >> $output/$sample-BackgroundFrequencies.txt
 
+#############################################################################################################################
+	#Print completion status
+	echo "Calculation of frequencies for $sample ($subset) is complete"
+	
+	#Remove temp files
+	rm -f $output/*Upstream.* $output/*Downstream.* $output/RiboBases.txt $output/temp.fa*
+	
 	fi
 	
 	done
