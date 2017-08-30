e93d0d41bb10026924b8828fb0f2afea57199f22
Alli Gombolay <agombolay3@gmail.com>
2017-08-30 11:58:02 -0400

    Update Random.sh

--- ribose-seq/programs/Random.sh
+++ ribose-seq/programs/Random.sh
@@ -1,1 +1,10 @@
 #Random
+
+data=c()
+for (i in 1:cells) {
+	data=sample(1:positions, ribos, replace=TRUE)
+	vector <- append(data,sample(1:positions, ribos, replace=FALSE))
+}
+
+hx=hist(vector,breaks=seq(1,positions,l=positions+1),plot=FALSE)
+plot(hx$counts)
