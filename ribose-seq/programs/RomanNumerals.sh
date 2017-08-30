a02ac2412becf19fe7d2ce3b597352dce89e5b08
Alli Gombolay <agombolay3@gmail.com>
2017-08-29 21:51:27 -0400

    Update RomanNumerals.sh

--- ribose-seq/programs/RomanNumerals.sh
+++ ribose-seq/programs/RomanNumerals.sh
@@ -1,25 +1,25 @@
 #Output directory
 output=$directory/Ribose-Map/Results/$reference/$sample/Coordinates
 
-sed 's/chrI/chr1/'      $output/temp3.txt > $output/temp4.txt
-sed 's/chrII/chr2/'     $output/temp3.txt > $output/temp4.txt
-sed 's/chrIII/chr3/'    $output/temp3.txt > $output/temp4.txt
-sed 's/chrIV/chr4/'     $output/temp3.txt > $output/temp4.txt
-sed 's/chrV/chr5/'      $output/temp3.txt > $output/temp4.txt
-sed 's/chrVI/chr6/'     $output/temp3.txt > $output/temp4.txt
-sed 's/chrVII/chr7/'    $output/temp3.txt > $output/temp4.txt
-sed 's/chrVIII/chr8/'   $output/temp3.txt > $output/temp4.txt
-sed 's/chrIX/chr9/'     $output/temp3.txt > $output/temp4.txt
-sed 's/chrX/chr10/'     $output/temp3.txt > $output/temp4.txt
-sed 's/chrXI/chr11/'    $output/temp3.txt > $output/temp4.txt
-sed 's/chrXII/chr12/'   $output/temp3.txt > $output/temp4.txt
-sed 's/chrXIII/chr13/'  $output/temp3.txt > $output/temp4.txt
-sed 's/chrXIV/chr14/'   $output/temp3.txt > $output/temp4.txt
-sed 's/chrXV/chr15/'    $output/temp3.txt > $output/temp4.txt
-sed 's/chrXVI/chr16/'   $output/temp3.txt > $output/temp4.txt
-sed 's/chrXVII/chr17/'  $output/temp3.txt > $output/temp4.txt
-sed 's/chrXVIII/chr18/' $output/temp3.txt > $output/temp4.txt
-sed 's/chrXIX/chr19/'   $output/temp3.txt > $output/temp4.txt
-sed 's/chrXX/chr20/'    $output/temp3.txt > $output/temp4.txt
-sed 's/chrXXI/chr21/'   $output/temp3.txt > $output/temp4.txt
-sed 's/chrXXII/chr22/' $output/temp3.txt > $output/temp4.txt
+sed 's/chr1/chrI/'      $output/temp3.txt > $output/temp4.txt
+sed 's/chr2/chrII/'     $output/temp3.txt > $output/temp4.txt
+sed 's/chr3/chrIII/'    $output/temp3.txt > $output/temp4.txt
+sed 's/chr4/chrIV/'     $output/temp3.txt > $output/temp4.txt
+sed 's/chr5/chrV/'      $output/temp3.txt > $output/temp4.txt
+sed 's/chr6/chrVI/'     $output/temp3.txt > $output/temp4.txt
+sed 's/chr7/chrVII/'    $output/temp3.txt > $output/temp4.txt
+sed 's/chr8/chrVIII/'   $output/temp3.txt > $output/temp4.txt
+sed 's/chr9/chrIX/'     $output/temp3.txt > $output/temp4.txt
+sed 's/chr10/chrX/'     $output/temp3.txt > $output/temp4.txt
+sed 's/chr11/chrXI/'    $output/temp3.txt > $output/temp4.txt
+sed 's/chr12/chrXII/'   $output/temp3.txt > $output/temp4.txt
+sed 's/chr13/chrXIII/'  $output/temp3.txt > $output/temp4.txt
+sed 's/chr14/chrXIV/'   $output/temp3.txt > $output/temp4.txt
+sed 's/chr15/chrXV/'    $output/temp3.txt > $output/temp4.txt
+sed 's/chr16/chrXVI/'   $output/temp3.txt > $output/temp4.txt
+sed 's/chr17/chrXVII/'  $output/temp3.txt > $output/temp4.txt
+sed 's/chr18/chrXVIII/' $output/temp3.txt > $output/temp4.txt
+sed 's/chr19/chrXIX/'   $output/temp3.txt > $output/temp4.txt
+sed 's/chr20/chrXX/'    $output/temp3.txt > $output/temp4.txt
+sed 's/chr21/chrXXI/'   $output/temp3.txt > $output/temp4.txt
+sed 's/chr22/chrXXII/'  $output/temp3.txt > $output/temp4.txt
