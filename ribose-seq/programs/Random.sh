474045addebe4d25f0ba5ec99987d7087d8a7e30
Alli Gombolay <agombolay3@gmail.com>
2017-08-30 12:05:29 -0400

    Update Random.sh

--- ribose-seq/programs/Random.sh
+++ ribose-seq/programs/Random.sh
@@ -11,7 +11,7 @@ library(optparse)
 #Command line options
 option_list <- list(
 make_option(c("-c", "--cells"), help="Estimated number of cells"),
-make_option(c("-c", "--rNMPs"), help="Number of observed ribonucleotides"),
+make_option(c("-c", "--ribos"), help="Number of observed ribonucleotides"),
 make_option(c("-s", "--sample"), help="Sample name(s) (e.g., FS1, FS2, FS3)"),
 make_option(c("-r", "--reference"), help="Reference genome (e.g., sacCer2, pombe, ecoli, mm9, hg38)"),
 make_option(c("-d", "--directory"), help="Local user directory (e.g., /projects/home/agombolay3/data/repository)")
@@ -21,10 +21,10 @@ make_option(c("-d", "--directory"), help="Local user directory (e.g., /projects/
 opt <- parse_args(OptionParser(option_list=option_list))
 
 data=c()
-for (i in 1:cells) {
-	data=sample(1:positions, ribos, replace=TRUE)
-	vector <- append(data,sample(1:positions, ribos, replace=FALSE))
+for (i in 1:opt$cells) {
+	data=sample(1:opt$positions, opt$ribos, replace=TRUE)
+	vector <- append(data,sample(1:opt$positions, opt$ribos, replace=FALSE))
 }
 
-hx=hist(vector,breaks=seq(1,positions,l=positions+1),plot=FALSE)
+hx=hist(vector,breaks=seq(1,opt$positions,l=opt$positions+1),plot=FALSE)
 plot(hx$counts)
