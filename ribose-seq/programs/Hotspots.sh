8a5162fe0dff7a6388c9f35e727976ca384f9bda
Alli Gombolay <agombolay3@gmail.com>
2017-09-06 21:12:37 -0400

    Update Hotspots.sh

--- ribose-seq/programs/Hotspots.sh
+++ ribose-seq/programs/Hotspots.sh
@@ -63,17 +63,17 @@ for sample in ${sample[@]}; do
 		color=0,128,0 visibility=full" > $output/$sample-Reverse.bedgraph
 		
 		#Rearrange file so format is same as bedgraph format (forward)
-		awk -v "OFS=\t" '$5 == "+" {print $2, $3, $4, $1}' $output/temp1.txt >> $output/$sample-Forward.bedgraph
+		awk -v "OFS=\t" '$5 == "+" {print $2,$3,$4,$1}' $output/temp1.txt >> $output/$sample-Forward.bedgraph
 
 		#Rearrange file so format is same as bedgraph format (reverse)
-		awk -v "OFS=\t" '$5 == "-" {print $2, $3, $4, $1}' $output/temp1.txt >> $output/$sample-Reverse.bedgraph
+		awk -v "OFS=\t" '$5 == "-" {print $2,$3,$4,$1}' $output/temp1.txt >> $output/$sample-Reverse.bedgraph
 
 #############################################################################################################################
 		#Remove old files
 		rm -f $output/$sample-Coverage.bed $output/$sample-Hotspots.$chr.bed
 		
 		#Create file containing coverage of both strands
-		awk -v "OFS=\t" '{print $2, $3, $4, $1}' temp1.txt > $output/$sample-Coverage.bed
+		awk -v "OFS=\t" '{print $2,$3,$4,$1}' $output/temp1.txt > $output/$sample-Coverage.bed
 		
 		#Save coverage of rNMPs for each chromosome to separate files for plotting
 		for chr in $( awk '{print $1}' $directory/Ribose-Map/References/$reference.bed ); do
