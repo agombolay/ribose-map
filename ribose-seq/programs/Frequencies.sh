508a6adfec12604094eb21888aa93eaab7202484
Alli Gombolay <agombolay3@gmail.com>
2017-09-11 12:40:21 -0400

    Update Frequencies.sh

--- ribose-seq/programs/Frequencies.sh
+++ ribose-seq/programs/Frequencies.sh
@@ -30,10 +30,10 @@ done
 if [ "$1" == "-h" ]; then
         exit
 fi
-echo 'hello'
+
 for sample in ${sample[@]}; do
 	for subset in "mito" "nucleus"; do
-	
+echo 'hello'	
 #############################################################################################################################
 	#Input files
 	BED=$directory/References/$reference.bed; FASTA=$directory/References/$reference.fa
