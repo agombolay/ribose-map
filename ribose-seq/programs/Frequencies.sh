7931f8d02235431c645519477c03cba21315def2
Alli Gombolay <agombolay3@gmail.com>
2017-09-11 16:25:08 -0400

    Update Frequencies.sh

--- ribose-seq/programs/Frequencies.sh
+++ ribose-seq/programs/Frequencies.sh
@@ -195,23 +195,29 @@ for sample in ${sample[@]}; do
 	
 #############################################################################################################################
 			#STEP 6: Create and save dataset file containing nucleotide frequencies
-
 			
 			#Add nucleotides to header line
 			echo -e "\tA\tC\tG\tU/T" > $output/$sample-Frequencies.$subset.txt
 			
+			#Add nucleotides to header line
+			echo -e "\t\tA\tC\tG\tT" > $directory/References/$reference-Freqs.txt
+			
 			#Add positions and frequencies of nucleotides in correct order to create dataset
-			paste <(echo "$(seq -100 1 100)") <(cat <(echo "$Up") <(echo "$Ribo") <(echo "$Down")) \
+			#paste <(echo "$(seq -100 1 100)") <(cat <(echo "$Up") <(echo "$Ribo") <(echo "$Down")) \
+			#>> $output/$sample-Frequencies.$subset.txt
+			
+			paste <(echo "$(seq -100 1 100)") <(echo "$Up") <(echo "$Ribo") <(echo "$Down") \
 			>> $output/$sample-Frequencies.$subset.txt
+			
+			#Add frequencies of nucleotides in reference genome
+			paste <(echo -e "Mito:\nNucleus:") <(echo -e \t"$Bkg") >> $directory/References/$reference-Freqs.txt
 	
 #############################################################################################################################
 			#STEP 7: Create and save file containing background nucleotide frequencies
 		
-			#Add nucleotides to header line
-			echo -e "\t\tA\tC\tG\tT" > $directory/References/$reference-Freqs.txt
+			
 	
-			#Add frequencies of nucleotides in reference genome
-			paste <(echo -e "Mito:\nNucleus:\t$Bkg") >> $directory/References/$reference-Freqs.txt
+		
 			
 #############################################################################################################################
 			#Print completion status
