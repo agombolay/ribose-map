db13f66d3ddd2f3f87c0d809a842ba8e90fc1e97
Alli Gombolay <agombolay3@gmail.com>
2017-09-11 19:39:44 -0400

    Update Coverage.sh

--- ribose-seq/programs/Coverage.sh
+++ ribose-seq/programs/Coverage.sh
@@ -1,1 +1,45 @@
-#Coverage
+#!/usr/bin/env bash
+
+#© 2016 Alli Gombolay
+#Author: Alli Lauren Gombolay
+#E-mail: alli.gombolay@gatech.edu
+#This program calculates the coveage at each rNMP position
+
+#Usage statement
+function usage () {
+	echo "Usage: Coverage.sh [options]
+		-s Sample name(s) (e.g., FS1, FS2, FS3)
+		-r Reference genome (e.g., sacCer2, pombe, ecoli, mm9, hg38)
+		-d Local user directory (e.g., /projects/home/agombolay3/data/repository/Ribose-Map)"
+}
+
+#Command-line options
+while getopts "s:r:d:h" opt; do
+    case "$opt" in
+        #Allow multiple input arguments
+        s ) sample=($OPTARG) ;;
+	#Allow only one input argument
+	r ) reference=$OPTARG ;;
+	d ) directory=$OPTARG ;;
+        #Print usage statement
+        h ) usage ;;
+    esac
+done
+
+#Exit program if [-h]
+if [ "$1" == "-h" ]; then
+        exit
+fi
+
+#Determine coordinates
+for sample in ${sample[@]}; do
+
+#############################################################################################################################
+#Input file
+coordinates=$directory/Results/$reference/$sample/Coordinates/$sample-Coordinates.bed
+	
+#Output directory
+output=$directory/Results/$reference/$sample/Hotspots
+	
+#Create directory
+mkdir -p $output
