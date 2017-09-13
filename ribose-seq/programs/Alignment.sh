14aee038d8ee5568bababe6634c99f29d604e8a7
Alli Gombolay <agombolay3@gmail.com>
2017-09-13 16:27:44 -0400

    Update Alignment.sh

--- ribose-seq/programs/Alignment.sh
+++ ribose-seq/programs/Alignment.sh
@@ -170,7 +170,7 @@ for sample in ${sample[@]}; do
 	echo "Trimming, alignment, and de-duplication of $sample is complete"
 
 	#Remove temporary files
-	rm -f $output/$sample_trimmed.fq $output/$sample*_val_*.fq $output/Reverse.fq $output/Read*.fq \
+	rm -f $output/${sample}_trimmed.fq $output/$sample*_val_*.fq $output/Reverse.fq $output/Read*.fq \
 	$output/mapped.sam $output/sorted.bam* $output/unmapped.bam $output/deduped.* $output/filtered.sam \
 	$output/Paired1.fq $output/Unpaired1.fq $output/Paired2.fq $output/Unpaired2.fq
 
