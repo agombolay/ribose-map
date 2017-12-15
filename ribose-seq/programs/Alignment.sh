b2a617cc2571e550c4ebfdff9136858823c478e9
Alli Gombolay <agombolay3@gmail.com>
2017-12-15 12:29:19 -0500

    Update Alignment.sh

--- ribose-seq/programs/Alignment.sh
+++ ribose-seq/programs/Alignment.sh
@@ -95,16 +95,16 @@ samtools index $output/$sample.bam
 			
 #############################################################################################################################
 #Calculate percentage of reads that remain after de-duplication
-#x=$(echo "$(samtools view -c $output/deduped.bam)/$(samtools view -c $output/sorted.bam)")
+x=$(echo "$(samtools view -c $output/deduped.bam)/$(samtools view -c $output/sorted.bam)")
 		
 #Calculate percentage of reads that contain correct barcode sequence
-#y=$(echo "$(samtools view -c $output/$sample.bam)/$(samtools view -c $output/deduped.bam)")
+y=$(echo "$(samtools view -c $output/$sample.bam)/$(samtools view -c $output/deduped.bam)")
 		
 #Save info about percentage of reads that remain after de-duplication
-#echo -e "Percentage: $(echo "$x*100" | bc -l | xargs printf "%.*f\n" 2)%" > $output/Unique.log
+echo -e "Percentage: $(echo "$x*100" | bc -l | xargs printf "%.*f\n" 2)%" > $output/Unique.log
 		
 #Save info about percentage of reads that contain correct barcode sequence
-#echo -e "Percentage: $(echo "$y*100" | bc -l | xargs printf "%.*f\n" 2)%" > $output/Barcode.log
+echo -e "Percentage: $(echo "$y*100" | bc -l | xargs printf "%.*f\n" 2)%" > $output/Barcode.log
 		
 #############################################################################################################################
 #Notify user alignment step is complete for input sample
