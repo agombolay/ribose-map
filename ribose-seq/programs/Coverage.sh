70a2003a0002e2cad120b0d4a98d7476a4e71380
Alli Gombolay <agombolay3@gmail.com>
2018-02-06 12:25:01 -0500

    Update Coverage.sh

--- ribose-seq/programs/Coverage.sh
+++ ribose-seq/programs/Coverage.sh
@@ -33,42 +33,38 @@ if [ "$1" == "-h" ]; then
         exit
 fi
 
-#Determine coordinates
-for sample in ${sample[@]}; do
-
 #############################################################################################################################
-	#Input files
-	reference=$directory/References/$reference.bed
-	coordinates=$directory/Results/$reference/$sample/Coordinates/$sample-Coordinates.bed
+#Input files
+reference=$directory/References/$reference.bed
+coordinates=$directory/Results/$reference/$sample/Coordinates/$sample-Coordinates.bed
 	
-	#Create output directory and remove old directory if present
-	output=$directory/Results/$reference/$sample/Coverage; mkdir -p $output; rm -rf $output
+#Create output directory and remove old directory if present
+output=$directory/Results/$reference/$sample/Coverage; mkdir -p $output; rm -rf $output
 
 #############################################################################################################################
-	if [[ -s $coordinates ]]; then
+if [[ -s $coordinates ]]; then
 		
-		#Save coverage of rNMPs per chromosome to separate files
-		for chr in $( awk '{print $1}' $bed ); do
-			uniq -c $coordinates | grep -w "$chr" - > $output/$sample-Coverage.$chr.bed
-		done
+	#Save coverage of rNMPs per chromosome to separate files
+	for chr in $( awk '{print $1}' $bed ); do
+		uniq -c $coordinates | grep -w "$chr" - > $output/$sample-Coverage.$chr.bed
+	done
 		
-		#Add trackline for forward strand to input into UCSC genome browser
-		echo "track type=bedGraph name="$sample-ForwardStrand" description="$sample-ForwardStrand" \
-		color=0,128,0 visibility=full" > $output/$sample-Forward.bg
+	#Add trackline for forward strand to input into UCSC genome browser
+	echo "track type=bedGraph name="$sample-ForwardStrand" description="$sample-ForwardStrand" \
+	color=0,128,0 visibility=full" > $output/$sample-Forward.bg
 		
-		#Add trackline for reverse strand to input into UCSC genome browser
-		echo "track type=bedGraph name="$sample-ReverseStrand" description="$sample-ReverseStrand" \
-		color=0,0,255 visibility=full" > $output/$sample-Reverse.bg
+	#Add trackline for reverse strand to input into UCSC genome browser
+	echo "track type=bedGraph name="$sample-ReverseStrand" description="$sample-ReverseStrand" \
+	color=0,0,255 visibility=full" > $output/$sample-Reverse.bg
 		
-		#Rearrange file so format is same as bedgraph format (forward)
-		awk -v "OFS=\t" '$5 == "+" {print $2,$3,$4,$1}' $output/temp1.txt >> $output/$sample-Forward.bg
+	#Rearrange file so format is same as bedgraph format (forward)
+	awk -v "OFS=\t" '$5 == "+" {print $2,$3,$4,$1}' $output/temp1.txt >> $output/$sample-Forward.bg
 
-		#Rearrange file so format is same as bedgraph format (reverse)
-		awk -v "OFS=\t" '$5 == "-" {print $2,$3,$4,$1}' $output/temp1.txt >> $output/$sample-Reverse.bg
+	#Rearrange file so format is same as bedgraph format (reverse)
+	awk -v "OFS=\t" '$5 == "-" {print $2,$3,$4,$1}' $output/temp1.txt >> $output/$sample-Reverse.bg
 
 #############################################################################################################################
-		#Print completion status for program
-		echo "Status: Program complete for $sample"
+	#Print completion status for program
+	echo "Status: Program complete for $sample"
 	
-	fi
-done
+fi
