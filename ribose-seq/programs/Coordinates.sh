276d56eeec3f67cea65306f862062caeb6dfc7e1
Alli Gombolay <agombolay3@gmail.com>
2018-02-06 15:46:53 -0500

    Update Coordinates.sh

--- ribose-seq/programs/Coordinates.sh
+++ ribose-seq/programs/Coordinates.sh
@@ -8,7 +8,7 @@
 #Usage statement
 function usage () {
 	echo "Usage: Coordinates.sh [options]
-	-s Sample name(s) (e.g., FS1, FS2, FS3)
+	-s Name of sequenced library
 	-r Reference genome/Basename of Bowtie2 index
 	-t rNMP sequencing technique used for library prep
 	-d Ribose-Map repository"
