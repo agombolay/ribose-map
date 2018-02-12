96c9b5d81bf652fe24dde1d5d18fb764c16cadac
Alli Gombolay <agombolay3@gmail.com>
2018-02-12 13:32:29 -0500

    Update pre-process.sh

--- ribose-seq/programs/pre-process.sh
+++ ribose-seq/programs/pre-process.sh
@@ -4,6 +4,32 @@
 #Author: Alli Lauren Gombolay
 #E-mail: alli.gombolay@gatech.edu
 
+#Usage statement
+function usage () {
+	echo "Usage: pre-process.sh [options]
+		Required:
+		-d Ribose-Map repository
+		-s Name of sequenced library
+		-f Input Read 1 FASTQ filename
+		-r Input Read 2 FASTQ filename"
+}
+
+while getopts "f:r:s:d:h" opt; do
+    	case "$opt" in
+		f ) read1=$OPTARG ;;
+		r ) read2=$OPTARG ;;
+		s ) sample=$OPTARG ;;
+		d ) directory=$OPTARG ;;
+    #Print usage statement
+    h ) usage ;;
+    esac
+done
+
+#Exit program if [-h]
+if [ "$1" == "-h" ]; then
+        exit
+fi
+
 #Single-end reads
 trim_galore $fastq1 --fastqc --length $minimum -a $adapter -o $output
 
