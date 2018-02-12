a69097680e7389896a56211158d411f1c49ac1b2
Alli Gombolay <agombolay3@gmail.com>
2018-02-11 22:52:35 -0500

    Update Coverage.R

--- ribose-seq/programs/Coverage.R
+++ ribose-seq/programs/Coverage.R
@@ -25,32 +25,29 @@ path <- file.path(opt$directory, "Results", opt$sample, "Coverage")
 input <- list.files(path=path, pattern=".bed", full.names=T, recursive=F)
         
 for(file in input){
-		
-	#Plot if file exists
-        if (file.exists(file)) {
             
-        	#Specify dataset
-		data=read.table(file, sep="\t", header=FALSE)
+        #Specify dataset
+	data=read.table(file, sep="\t", header=FALSE)
 			
-		#Transform values on negative strand
-		values <- ifelse(data$V6=='-',data$V3*-1,data$V3)
+	#Transform values on negative strand
+	values <- ifelse(data$V6=='-',data$V3*-1,data$V3)
 
 #############################################################################################################################
-		#Create plot
-		myplot <- ggplot(data, aes(x=data[,2], y=values))+
+	#Create plot
+	myplot <- ggplot(data, aes(x=data[,2], y=values))+
 		
-		#Create barchart and add titles for axes
-        	geom_bar(stat="identity", fill="black") + xlab("Chromosome Position") + ylab("rNMP Frequency")+ 
+	#Create barchart and add titles for axes
+        geom_bar(stat="identity", fill="black") + xlab("Chromosome Position") + ylab("rNMP Frequency")+ 
 		
-		#Remove and replace default background theme of plot
-		theme(panel.grid=element_blank(), panel.background=element_blank(), axis.line=element_line(colour="black"))+
+	#Remove and replace default background theme of plot
+	theme(panel.grid=element_blank(), panel.background=element_blank(), axis.line=element_line(colour="black"))+
 		
-		#Decrease spacing between plot and axes and increase font
-		theme(text=element_text(size=15)) + scale_y_discrete(expand=c(0.015,0)) + scale_x_discrete(expand=c(0.015,0))
+	#Decrease spacing between plot and axes and increase font
+	theme(text=element_text(size=15)) + scale_y_discrete(expand=c(0.015,0)) + scale_x_discrete(expand=c(0.015,0))
 
 #############################################################################################################################
-		#Save plot as PNG file
-		ggsave(filename=file.path(path, paste(file_path_sans_ext(basename(file)), ".png", sep="")), plot=myplot, width=20)
+	#Save plot as PNG file
+	ggsave(filename=file.path(path, paste(file_path_sans_ext(basename(file)), ".png", sep="")), plot=myplot, width=20)
 
 }
 }
