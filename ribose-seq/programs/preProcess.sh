4469d3100291e448edaf49c9a945e80957891d2c
Alli Gombolay <agombolay3@gmail.com>
2018-02-12 16:07:08 -0500

    Update preProcess.sh

--- ribose-seq/programs/preProcess.sh
+++ ribose-seq/programs/preProcess.sh
@@ -4,6 +4,15 @@
 #Author: Alli Lauren Gombolay
 #E-mail: alli.gombolay@gatech.edu
 
+#############################################################################################################################
+#Input files
+fastq1=$directory/fastqs/$read1
+fastq2=$directory/fastqs/$read2
+
+#Remove old files
+rm -f $output/*_trimmed{1,2}.fq
+
+#############################################################################################################################
 umi_tools extract -v 0 -I $fastq1 -p $UMI -S $output/extract1.fq
 
 umi_tools extract -v 0 -I $fastq1 -p $UMI -S $output/extract1.fq --read2-in=$fastq2 --read2-out=$output/extract2.fq
