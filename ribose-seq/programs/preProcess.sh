fc1736f94eeb03296b42e319131a893e4f2aecfd
Alli Gombolay <agombolay3@gmail.com>
2018-02-12 16:30:18 -0500

    Update preProcess.sh

--- ribose-seq/programs/preProcess.sh
+++ ribose-seq/programs/preProcess.sh
@@ -11,12 +11,14 @@ if [[ $fastq1 ]] && [[ ! $fastq2 ]]; then
   umi_tools extract -v 0 -I $fastq1 -p $UMI -S $output/UMI1.fq
   
 elif [[ $fastq1 ]] && [[ $fastq2 ]]; then
-  umi_tools extract -v 0 -I $fastq1 -p $UMI -S $output/UMI1.fq --read2-in=$fastq2 --read2-out=$output/UMI2.fq
+  umi_tools extract -v 0 -I $fastq1 -p $UMI -S $output/UMI1.fq \
+  --read2-in=$fastq2 --read2-out=$output/UMI2.fq
 fi
 
 if [[ $barcode ]]; then
   #Filter reads by barcode and remove barcode from reads
-  grep -B 1 -A 2 ^$bc $output/UMI1.fq | sed '/^--$/d' | awk 'NR%2==0 {sub(/^.{'${#bc}'}/,"")} {print}' > $output/bc.fq
+  grep -B 1 -A 2 ^$bc $output/UMI1.fq | sed '/^--$/d' \
+  | awk 'NR%2==0 {sub(/^.{'${#bc}'}/,"")} {print}' > $output/bc.fq
   
   #Calculate % of reads that contain correct barcode sequence
   x=$(echo $((`wc -l < $output/bc.fq` / 4))/$((`wc -l < $output/UMI1.fq` / 4)))
