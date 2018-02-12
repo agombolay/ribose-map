509ef53228be0667dc2bf6e25d05282b48d44aca
Alli Gombolay <agombolay3@gmail.com>
2018-02-12 15:19:20 -0500

    Update alignment.sh

--- ribose-seq/programs/alignment.sh
+++ ribose-seq/programs/alignment.sh
@@ -57,19 +57,78 @@ output=$directory/results/$sample/alignment
 mkdir -p $output; rm -f $output/*.{bam,bai,log}
 
 #############################################################################################################################
+if [[ ! $read2 ]]; then
+	
+	if [[ ! $umi ]]; then
+		bowtie2 -x $idx -U $fastq1 -S $output/map.sam 2> $output/align.log
+		
+		samtools view -bS -F260 $output/map.sam | samtools sort - -o $output/$sample.bam
+		samtools index $output/$sample.bam	
 
-#############################################################################################################################
-#Calculate % of reads that contain correct barcode sequence
-x=$(echo $((`wc -l < $output/filtered.fq` / 4))/$((`wc -l < $output/UMI.fq` / 4)))
+	elif [[ $umi ]]; then
+		umi_tools extract -v 0 -I $fastq1 -p $UMI -S $output/extract.fq
+		
+		if [[ ! $barcode ]]; then
+			bowtie2 -x $idx -U $output/extract.fq -S $output/map.sam 2> $output/align.log
 
-#Calculate % of reads that remain after de-duplication step
-y=$(echo "$(samtools view -c $output/$sample.bam)/$(samtools view -c $output/sorted.bam)")
-
-#Save info about %of reads that contain correct barcode sequence
-echo -e "Percentage of reads with barcode: $(echo "$x*100" | bc -l | xargs printf "%.*f\n" 2)%" > $output/barcode.log
-
-#Save info about % of reads that remain after de-duplication step
-echo -e "Percentage of reads that are unique: $(echo "$y*100" | bc -l | xargs printf "%.*f\n" 2)%" > $output/unique.log
+			samtools view -bS -F260 $output/map.sam | samtools sort - -o $output/sort.bam
+			samtools index $output/sorted.bam
+	
+			umi_tools dedup -v 0 -I $output/sort.bam | samtools sort - -o $output/$sample.bam
+			samtools index $output/$sample.bam
+		
+		elif [[ $barcode ]]; then
+			grep -B 1 -A 2 ^$barcode $output/extract.fq | sed '/^--$/d' \
+			| awk 'NR%2 == 0 {sub(/^.{'${#barcode}'}/,"")} {print}' > $output/filter.fq
+		
+			bowtie2 -x $idx -U $output/filter.fq -S $output/map.sam 2> $output/align.log
+			
+			samtools view -bS -F260 $output/map.sam | samtools sort - -o $output/sort.bam
+			samtools index $output/sort.bam
+	
+			umi_tools dedup -v 0 -I $output/sort.bam | samtools sort - -o $output/$sample.bam
+			samtools index $output/$sample.bam
+		fi
+	fi
+	
+elif [[ $read2 ]]; then
+	
+	if [[ ! $umi ]]; then
+		bowtie2 -x $idx -1 $fastq1 -2 $fastq2 -S $output/map.sam 2> $output/align.log
+		
+		samtools view -bS -f67 -F260 $output/map.sam | samtools sort - -o $output/$sample.bam
+		samtools index $output/$sample.bam
+	
+	elif [[ $umi ]]; then
+	
+		umi_tools extract -v 0 -I $fastq1 -p $UMI -S $output/extract1.fq --read2-in=$fastq2 \
+		--read2-out=$output/extract2.fq
+	
+		if [[ ! $barcode ]]; then
+			bowtie2 -x $idx -1 $output/extract1.fq -2 $output/extract2.fq -S $output/map.sam \
+			2> $output/align.log
+		
+			samtools view -bS -f67 -F260 $output/map.sam | samtools sort - -o $output/sort.bam
+			samtools index $output/sort.bam
+	
+			umi_tools dedup -v 0 --paired -I $output/sort.bam | samtools sort - -o $output/$sample.bam
+			samtools index $output/$sample.bam
+	
+		elif [[ $barcode ]]; then
+			grep -B 1 -A 2 ^$barcode $output/extract.fq | sed '/^--$/d' \
+			| awk 'NR%2 == 0 {sub(/^.{'${#barcode}'}/,"")} {print}' > $output/filter.fq
+		
+			bowtie2 -x $idx -1 $output/filter.fq -2 $output/extract2.fq  -S $output/map.sam \
+			2> $output/align.log
+		
+			samtools view -bS -f67 -F260 $output/map.sam | samtools sort - -o $output/sort.bam
+			samtools index $output/sort.bam
+	
+			umi_tools dedup -v 0 --paired -I $output/sort.bam | samtools sort - -o $output/$sample.bam
+			samtools index $output/$sample.bam
+		fi
+	fi
+fi
 		
 #############################################################################################################################
 #Print completion status
