e7bfa9add066eca9d3fabb00d1399fb11e2c142e
Alli Gombolay <agombolay3@gmail.com>
2018-02-12 11:32:27 -0500

    Update Frequencies.sh

--- ribose-seq/programs/Frequencies.sh
+++ ribose-seq/programs/Frequencies.sh
@@ -36,7 +36,8 @@ fi
 output=$directory/results/$sample/frequencies
 	
 #Input reference and coordinates files
-reference=$directory/references/$reference.fa
+reference1=$directory/references/$reference.fa
+reference2=$directory/references/$reference.fai
 bed=$directory/results/$sample/coordinates/$sample.bed
 	
 #Create directory and remove old files
@@ -48,7 +49,7 @@ for subset in "mito" "nucleus"; do
 	#STEP 1: Calculate frequencies of reference genome
 	
 	#Create BED file for reference genome
-	cut -f 1,2 $reference > $output/$reference.bed
+	cut -f 1,2 $reference2 > $output/$reference.bed
 
 	#Subset FASTA file based on region
 	if [ $subset == "mito" ]; then
