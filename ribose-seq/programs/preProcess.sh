56e6d4777f902f7ff1a85b44efd00c760037f68a
Alli Gombolay <agombolay3@gmail.com>
2018-02-12 16:05:27 -0500

    Update preProcess.sh

--- ribose-seq/programs/preProcess.sh
+++ ribose-seq/programs/preProcess.sh
@@ -4,7 +4,8 @@
 #Author: Alli Lauren Gombolay
 #E-mail: alli.gombolay@gatech.edu
 
-umi_tools extract -v 0 -I $fastq1 -p $UMI -S $output/extract.fq
+umi_tools extract -v 0 -I $fastq1 -p $UMI -S $output/extract1.fq
 
-grep -B 1 -A 2 ^$barcode $output/extract.fq | sed '/^--$/d' \
-| awk 'NR%2 == 0 {sub(/^.{'${#barcode}'}/,"")} {print}' > $output/filter.fq
+umi_tools extract -v 0 -I $fastq1 -p $UMI -S $output/extract1.fq --read2-in=$fastq2 --read2-out=$output/extract2.fq
+
+grep -B 1 -A 2 ^$bc $output/extract1.fq | sed '/^--$/d' | awk 'NR%2 == 0 {sub(/^.{'${#bc}'}/,"")} {print}' > $output/filter.fq
