f1e4682fd2d6392445f0926a56533c92308ecffc
Alli Gombolay <agombolay3@gmail.com>
2018-02-13 12:00:02 -0500

    Update qualityControl.sh

--- ribose-seq/programs/qualityControl.sh
+++ ribose-seq/programs/qualityControl.sh
@@ -4,41 +4,18 @@
 #Author: Alli Lauren Gombolay
 #E-mail: alli.gombolay@gatech.edu
 
-#Usage statement
-#function usage () {
-#	echo "Usage: pre-process.sh [options]
-#		Required:
-#		-d Ribose-Map directory
-#		-s Name of sequenced library
-#		-f Input Read 1 FASTQ filename
-#		-r Input Read 2 FASTQ filename"
-#}
-
-#while getopts "f:r:s:d:h" opt; do
-#	case "$opt" in
-#		h ) usage ;;
-#		f ) read1=$OPTARG ;;
-#		r ) read2=$OPTARG ;;
-#		s ) sample=$OPTARG ;;
-#		d ) directory=$OPTARG ;;
-#	esac
-#done
-
 . /data2/users/agombolay3/Ribose-Map/config.txt
 
 #############################################################################################################################
 #Input files
-fastq1=$directory/fastqs/$read1
-fastq2=$directory/fastqs/$read2
-
-#Remove old files
-rm -f $directory/fastqs/*_trimmed{1,2}.fq
+#fastq1=$directory/fastqs/$read1
+#fastq2=$directory/fastqs/$read2
 
 #############################################################################################################################
 if [[ ! $read2 ]]; then
 	#Single-end reads
-	fastqc $fastq1 -o $directory/fastqs
-	cutadapt $fastq1 -m 50 -a 'AGTTGCGACACGGATCTCTCA' -o $directory/fastqs/${sample}_trimmed.fq
+	fastqc $fastq1 -o $(dirname $read1_fastq)
+	#cutadapt $fastq1 -m 50 -a 'AGTTGCGACACGGATCTCTCA' -o $directory/fastqs/${sample}_trimmed1.fq
 
 elif [[ $read2 ]]; then
 	#Paired-end reads
