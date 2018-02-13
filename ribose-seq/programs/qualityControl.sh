5bb55f2af6e80b00894f50f004ade39603225e38
Alli Gombolay <agombolay3@gmail.com>
2018-02-13 15:35:54 -0500

    Update qualityControl.sh

--- ribose-seq/programs/qualityControl.sh
+++ ribose-seq/programs/qualityControl.sh
@@ -4,10 +4,31 @@
 #Author: Alli Lauren Gombolay
 #E-mail: alli.gombolay@gatech.edu
 
+#Usage statement
+function usage () {
+	echo "Usage: qualityControl.sh [options]
+		-d Ribose-Map directory
+		-s Name of sequenced library
+		-a Sequencing adapter to trim"
+}
+
+#Command-line options
+while getopts "s:d:a:h" opt; do
+    case "$opt" in
+        s ) sample=$OPTARG ;;
+	r ) reference=$OPTARG ;;
+	d ) directory=$OPTARG ;;
+	h ) usage ;;
+    esac
+done
+
+#Exit program if [-h]
+if [ "$1" == "-h" ]; then
+        exit
+fi
+
 #############################################################################################################################
 adapter='AGTTGCGACACGGATCTCTCA'
-
-. /data2/users/agombolay3/Ribose-Map/config.txt
 
 output=$directory/results/$sample/alignment
 mkdir -p $output
