cf2b2121e0857e5d8f7c0f152ae129770a70b368
Alli Gombolay <agombolay3@gmail.com>
2018-02-13 12:13:30 -0500

    Update qualityControl.sh

--- ribose-seq/programs/qualityControl.sh
+++ ribose-seq/programs/qualityControl.sh
@@ -8,14 +8,14 @@
 . /data2/users/agombolay3/Ribose-Map/config.txt
 output=$directory/$name/pre-processing; mkdir -p $output
 
+#Single-end reads
 if [[ ! $read2 ]]; then
-	#Single-end reads
 	fastqc $read1_fastq -o $directory/$name/pre-processing
 	
 	cutadapt $read1_fastq -m 50 -a $adapter -o $output/${name}_trimmed1.fq
 
+#Paired-end reads
 elif [[ $read2 ]]; then
-	#Paired-end reads
 	fastqc $read1_fastq $read2_fastq -o $directory/$name/pre-processing
 	
 	cutadapt $read1_fastq $read2_fastq -m 50 -a $adapter -o $output/${name}_trimmed1.fq \
