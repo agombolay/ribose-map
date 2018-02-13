ddcfd040ad2f85672841c981a7230263e247f399
Alli Gombolay <agombolay3@gmail.com>
2018-02-13 12:17:36 -0500

    Update qualityControl.sh

--- ribose-seq/programs/qualityControl.sh
+++ ribose-seq/programs/qualityControl.sh
@@ -12,14 +12,14 @@ mkdir -p $output
 
 #Single-end reads
 if [[ ! $read2 ]]; then
-	fastqc $read1_fastq -o $directory/$name/pre-processing
+	fastqc $read1_fastq -o $directory/$sample/pre-processing
 	
-	cutadapt $read1_fastq -m 50 -a $adapter -o $output/${name}_trimmed1.fq
+	cutadapt $read1_fastq -m 50 -a $adapter -o $output/${sample}_trimmed1.fq
 
 #Paired-end reads
 elif [[ $read2 ]]; then
-	fastqc $read1_fastq $read2_fastq -o $directory/$name/pre-processing
+	fastqc $read1_fastq $read2_fastq -o $directory/$sample/pre-processing
 	
-	cutadapt $read1_fastq $read2_fastq -m 50 -a $adapter -o $output/${name}_trimmed1.fq \
-	-p $output/${name}_trimmed1.fq
+	cutadapt $read1_fastq $read2_fastq -m 50 -a $adapter -o $output/${sample}_trimmed1.fq \
+	-p $output/${sample}_trimmed1.fq
 fi
