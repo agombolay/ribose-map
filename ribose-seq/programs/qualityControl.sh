6ed25b9697b24578b3e88da0f42017797778b07c
Alli Gombolay <agombolay3@gmail.com>
2018-02-13 15:51:57 -0500

    Update qualityControl.sh

--- ribose-seq/programs/qualityControl.sh
+++ ribose-seq/programs/qualityControl.sh
@@ -28,34 +28,25 @@ if [ "$1" == "-h" ]; then
 fi
 
 #############################################################################################################################
-adapter='AGTTGCGACACGGATCTCTCA'
-
 output=$directory/results/$sample/alignment
 mkdir -p $output
 
+adapter='AGTTGCGACACGGATCTCTCA'
+#############################################################################################################################
+
+if [[ $instrument ]]: then
+	nextseq=''
+elif [[ $instrument ]]: then
+	nextseq='--nextseq-trim=20'
+
 #Single-end reads
-if [[ ! $nextseq ]]: then
+if [[ ! $read2 ]]; then
+	fastqc $read1_fastq -o $output
+	cutadapt $nextseq -a $adapter -m 50 $read1_fastq -o $output/trimmed.fq
 
-	if [[ ! $read2 ]]; then
-		fastqc $read1_fastq -o $output
-		cutadapt -a $adapter -m 50 $read1_fastq -o $output/trimmed.fq
+#Paired-end reads
+elif [[ $read2 ]]; then
+	fastqc $read1 $read2 -o $output
+	cutadapt $nextseq -a $adapter -m 50 -p $read1 $read2 -o $output/trimmed1.fq -p $output/trimmed2.fq
 
-	#Paired-end reads
-	elif [[ $read2 ]]; then
-		fastqc $read1 $read2 -o $output
-		cutadapt -a $adapter -m 50 -p $read1 $read2 -o $output/trimmed1.fq -p $output/trimmed2.fq
-
-	fi
-	
-elif [[ ! $nextseq ]]: then
-
-	if [[ ! $read2 ]]; then
-		fastqc $read1_fastq -o $output
-		cutadapt --nextseq-trim=20 -a $adapter -m 50 $read1_fastq -o $output/trimmed.fq
-
-	#Paired-end reads
-	elif [[ $read2 ]]; then
-		fastqc $read1 $read2 -o $output
-		cutadapt --nextseq-trim=20 -a $adapter -m 50 -p $read1 $read2 -o $output/trimmed1.fq -p $output/trimmed2.fq
-	fi
 fi
