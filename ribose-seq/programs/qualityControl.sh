b0f4b4d137f27df32d98515c862e8793ff80edd7
Alli Gombolay <agombolay3@gmail.com>
2018-02-13 12:33:13 -0500

    Update qualityControl.sh

--- ribose-seq/programs/qualityControl.sh
+++ ribose-seq/programs/qualityControl.sh
@@ -13,15 +13,9 @@ mkdir -p $output
 #Single-end reads
 if [[ ! $read2 ]]; then
 	trim_galore --fastqc -a 'AGTTGCGACACGGATCTCTCA' --length 50 $read1_fastq -o $output
-	
-	#fastqc $read1_fastq -o $output
-	#cutadapt $read1_fastq -m 50 -a 'AGTTGCGACACGGATCTCTCA' -o $output/${sample}_trimmed.fq
 
 #Paired-end reads
 elif [[ $read2 ]]; then
 	trim_galore --fastqc -a 'AGTTGCGACACGGATCTCTCA' --length 50 --paired $read1_fastq $read2_fastq -o $output
-	
-	#fastqc $read1_fastq $read2_fastq -o $output 
-	#cutadapt $read1_fastq $read2_fastq -m 50 -a 'AGTTGCGACACGGATCTCTCA' -o $output/${sample}_trimmed1.fq
-	#-p $output/${sample}_trimmed2.fq
+
 fi
