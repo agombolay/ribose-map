b1cdf97edb96c72a1ef89d203590fea05b5b178b
Alli Gombolay <agombolay3@gmail.com>
2018-02-13 12:02:58 -0500

    Update qualityControl.sh

--- ribose-seq/programs/qualityControl.sh
+++ ribose-seq/programs/qualityControl.sh
@@ -14,7 +14,7 @@
 #############################################################################################################################
 if [[ ! $read2 ]]; then
 	#Single-end reads
-	fastqc $fastq1 -o $(dirname $read1_fastq)
+	fastqc $fastq1 -o $directory/$sample/alignment
 	#cutadapt $fastq1 -m 50 -a 'AGTTGCGACACGGATCTCTCA' -o $directory/fastqs/${sample}_trimmed1.fq
 
 elif [[ $read2 ]]; then
