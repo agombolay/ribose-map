f2ac802c02bad649f3360244626b6153bf3e6f82
Alli Gombolay <agombolay3@gmail.com>
2018-02-13 11:43:36 -0500

    Update qualityControl.sh

--- ribose-seq/programs/qualityControl.sh
+++ ribose-seq/programs/qualityControl.sh
@@ -5,24 +5,26 @@
 #E-mail: alli.gombolay@gatech.edu
 
 #Usage statement
-function usage () {
-	echo "Usage: pre-process.sh [options]
-		Required:
-		-d Ribose-Map directory
-		-s Name of sequenced library
-		-f Input Read 1 FASTQ filename
-		-r Input Read 2 FASTQ filename"
-}
+#function usage () {
+#	echo "Usage: pre-process.sh [options]
+#		Required:
+#		-d Ribose-Map directory
+#		-s Name of sequenced library
+#		-f Input Read 1 FASTQ filename
+#		-r Input Read 2 FASTQ filename"
+#}
 
-while getopts "f:r:s:d:h" opt; do
-	case "$opt" in
-		h ) usage ;;
-		f ) read1=$OPTARG ;;
-		r ) read2=$OPTARG ;;
-		s ) sample=$OPTARG ;;
-		d ) directory=$OPTARG ;;
-	esac
-done
+#while getopts "f:r:s:d:h" opt; do
+#	case "$opt" in
+#		h ) usage ;;
+#		f ) read1=$OPTARG ;;
+#		r ) read2=$OPTARG ;;
+#		s ) sample=$OPTARG ;;
+#		d ) directory=$OPTARG ;;
+#	esac
+#done
+
+. /data2/users/agombolay3/Ribose-Map/config.txt
 
 #############################################################################################################################
 #Input files
