3162ebf6fd83ef321f1ed1110c648e4bc56a936b
Alli Gombolay <agombolay3@gmail.com>
2018-02-12 23:39:29 -0500

    Update preProcess.sh

--- ribose-seq/programs/preProcess.sh
+++ ribose-seq/programs/preProcess.sh
@@ -7,21 +7,21 @@
 output=$directory/results/alignment
 
 if [[ $fastq1 ]] && [[ ! $fastq2 ]]; then
-  umi_tools extract -v 0 -I $fastq1 -p $UMI -S $output/umi_extracted.fq
+  umi_tools extract -v 0 -I $fastq1 -p $UMI -S $output/umi_extracted1.fq
   
 elif [[ $fastq1 ]] && [[ $fastq2 ]]; then
-  umi_tools extract -v 0 -I $fastq1 -p $UMI -S $output/umi_extracted.fq \
-  --read2-in=$fastq2 --read2-out=$output/UMI2.fq
+  umi_tools extract -v 0 -I $fastq1 -p $UMI -S $output/umi_extracted1.fq \
+  --read2-in=$fastq2 --read2-out=$output/umi_extracted2.fq
 fi
 
 if [[ $barcode ]]; then
   #Filter reads by barcode and remove barcode from reads
-  grep -B 1 -A 2 ^$barcode $output/umi_extracted.fq | sed '/^--$/d' \
+  grep -B 1 -A 2 ^$barcode $output/umi_extracted1.fq | sed '/^--$/d' \
   | awk 'NR % 2 == 0 {sub(/^.{'${#barcode}'}/,"")} {print}' > $output/barcode.fq
   
   #Calculate % of reads that contain correct barcode sequence
-  x=$(wc -l < $output/barcode.fq)/4; y=$(wc -l < $output/umi_extracted.fq)/4
-
+  x=$(echo $(bc -l <<< "$(wc -l < $output/umi_extracted1.fq)/4")/$(bc -l <<< "$(wc -l < $output/barcode.fq)/4"))
+  
   #Save info about % of reads that contain correct barcode sequence
-  echo -e "Reads with barcode: $(echo "$((x/y))*100" | bc -l)%" > $output/barcode.log
+  echo -e "Reads with the molecular barcode, $barcode: $(bc -l <<< "scale = 2; $x * 100")%" > $output/barcode.log
 fi
