3ebda19cbf5441da406a8214d620c07813480736
Alli Gombolay <agombolay3@gmail.com>
2018-02-13 14:41:55 -0500

    Update alignment.sh

--- ribose-seq/programs/alignment.sh
+++ ribose-seq/programs/alignment.sh
@@ -11,45 +11,6 @@
 #Note: Input FASTQ files must be located in Ribose-Map 'fastqs' directory (Ribose-Map/fastqs)
 #Note: Bowtie2 index files must be located in Ribose-Map 'indexes' directory (Ribose-Map/indexes)
 
-#Usage statement
-function usage () {
-	echo "Usage: Alignment.sh [options]
-		Required:
-		-d Ribose-Map directory
-		-s Name of sequenced library
-		-i Basename of Bowtie2 index
-		-f Input Read 1 FASTQ filename
-		-r Input Read 2 FASTQ filename
-		Optional:
-		-u UMI (e.g., NNNNNNNN or NNNNXXXNNNN)
-		-b Molecular barcode in UMI (e.g., TGA)"
-}
-
-while getopts "h:u:i:f:r:s:b:d" opt; do
-    	case "$opt" in
-		h ) usage ;;
-		u ) UMI=$OPTARG ;;
-		i ) index=$OPTARG ;;
-		f ) read1=$OPTARG ;;
-		r ) read2=$OPTARG ;;
-		s ) sample=$OPTARG ;;
-		b ) barcode=$OPTARG ;;
-		d ) directory=$OPTARG ;;
-    	esac
-done
-
-#############################################################################################################################
-#Input files
-#fastq1=$directory/fastqs/$read1
-#fastq2=$directory/fastqs/$read2
-#index=$directory/references/$idx
-
-#Create output directory
-#output=$directory/results/$sample/alignment
-
-#Create directory and remove old files
-#mkdir -p $output; rm -f $output/*.{bam,bai,log}
-
 . /data2/users/agombolay3/Ribose-Map/config.txt
 
 output=$directory/results/$sample/alignment
@@ -88,12 +49,6 @@ elif [[ $read2 ]]; then
 		samtools index $output/$sample.bam
 	fi
 fi
-
-#Calculate % of reads that remain after de-duplication step
-#y=$(echo "$(samtools view -c $output/$sample.bam)/$(samtools view -c $output/sorted.bam)")
-
-#Save info about % of reads that remain after de-duplication step
-#echo -e "Percentage of reads that are unique: $(echo "$y*100" | bc -l)%" > $output/unique.log
 
 #############################################################################################################################
 #Print completion status
