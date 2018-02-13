970cee79a601213d79fb8d0024790a592b67e845
Alli Gombolay <agombolay3@gmail.com>
2018-02-13 15:33:32 -0500

    Update qualityControl.sh

--- ribose-seq/programs/qualityControl.sh
+++ ribose-seq/programs/qualityControl.sh
@@ -5,17 +5,23 @@
 #E-mail: alli.gombolay@gatech.edu
 
 #############################################################################################################################
+adapter='AGTTGCGACACGGATCTCTCA'
+
 . /data2/users/agombolay3/Ribose-Map/config.txt
 
-output=$directory/results/$sample/pre-processing
+output=$directory/results/$sample/alignment
 mkdir -p $output
 
 #Single-end reads
 if [[ ! $read2 ]]; then
-	trim_galore --fastqc -a 'AGTTGCGACACGGATCTCTCA' --length 50 $read1_fastq -o $output
+	fastqc $read1_fastq -o $output
+	cutadapt-a $adapter -m 50 $read1_fastq -o $output/${sample}_trimmed.fq
 
 #Paired-end reads
 elif [[ $read2 ]]; then
-	trim_galore --fastqc -a 'AGTTGCGACACGGATCTCTCA' --length 50 --paired $read1_fastq $read2_fastq -o $output
+	fastqc $read1 $read2 -o $output
+	cutadapt -a $adapter -m --paired $read1 $read2 -o $output/${sample}_trimmed1.fq -p $output/${sample}_trimmed2.fq
 
 fi
+
+--nextseq-trim=20
