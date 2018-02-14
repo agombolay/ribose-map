1dcb2a0f7c4ceea05ee4352abaca4e2f5e0a6b41
Alli Gombolay <agombolay3@gmail.com>
2018-02-14 12:42:13 -0500

    Update qualityControl.sh

--- ribose-seq/programs/qualityControl.sh
+++ ribose-seq/programs/qualityControl.sh
@@ -4,38 +4,11 @@
 #Author: Alli Lauren Gombolay
 #E-mail: alli.gombolay@gatech.edu
 
-#Usage statement
-function usage () {
-	echo "Usage: qualityControl.sh [options]
-		-a Filepath of read 1
-		-b Filepath of read 2
-		-d Ribose-Map directory
-		-s Name of sequenced library
-		-n Sequencing instrument used"
-}
+. /data2/users/agombolay3/Ribose-Map/config.txt
 
-#Command-line options
-while getopts "a:b:s:d:i:h" opt; do
-    case "$opt" in
-	a ) read1=$OPTARG ;;
-	b ) read2=$OPTARG ;;
-	s ) sample=$OPTARG ;;
-	d ) directory=$OPTARG ;;
-	i ) instrument=$OPTARG ;;
-	h ) usage ;;
-    esac
-done
-
-#Exit program if [-h]
-if [ "$1" == "-h" ]; then
-        exit
-fi
-
-#############################################################################################################################
 output=$directory/results/$sample/alignment
 mkdir -p $output
 
-adapter='AGTTGCGACACGGATCTCTCA'
 #############################################################################################################################
 
 if [[ ! $instrument ]]; then
