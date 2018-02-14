97a5edd87fcd8d84a7929e92e6b1b7ace8c820aa
Alli Gombolay <agombolay3@gmail.com>
2018-02-14 13:39:54 -0500

    Update coverage.sh

--- ribose-seq/programs/coverage.sh
+++ ribose-seq/programs/coverage.sh
@@ -21,7 +21,8 @@ uniq -c $directory/results/$sample/coordinates/$sample.bed > $output/temp1.txt
 	
 #Save coverage of rNMPs per chromosome to separate files
 for chromosome in $( awk '{print $1}' $output/reference.bed ); do
-	grep -w "$chromosome" $output/temp1.txt > $output/$sample.$chromosome.bed
+	grep -w "$chromosome" $output/temp1.txt \
+	| awk -v "OFS=\t" '$5 == "+" {print $2,$3,$4,$1}'> $output/$sample.$chromosome.bed
 done
 		
 #Add trackline for forward strand to input into UCSC genome browser
