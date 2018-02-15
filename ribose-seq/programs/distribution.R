fbd4aba87ff92c6fc727606e1ebe959bca2c3d15
Alli Gombolay <agombolay3@gmail.com>
2018-02-14 22:55:59 -0500

    Update distribution.R

--- ribose-seq/programs/distribution.R
+++ ribose-seq/programs/distribution.R
@@ -36,7 +36,7 @@ for(file in input_files){
 	theme(panel.grid=element_blank(), panel.background=element_blank(), axis.line=element_line(colour="black")) +
 	
 	#Create barchart and add titles for axes
-        geom_bar(stat="identity", fill="black", color="black") + xlab("Chromosome Position") + ylab("rNMP Frequency") 
+        geom_bar(stat="identity", fill="black", color="black", width = 0.1) + xlab("Chromosome Position") + ylab("rNMP Frequency") 
 	
 	#Decrease spacing between plot and axes and increase font
 	#theme(text=element_text(size=15)) + scale_y_discrete(expand=c(0.015,0)) + scale_x_discrete(expand=c(0.015,0))
