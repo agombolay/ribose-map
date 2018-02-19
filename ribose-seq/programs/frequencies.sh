a5ec013e2a3d11d0e2299e2983ec8094a2ab308d
Alli Gombolay <agombolay3@gmail.com>
2018-02-18 23:12:34 -0500

    Update frequencies.sh

--- ribose-seq/programs/frequencies.sh
+++ ribose-seq/programs/frequencies.sh
@@ -11,12 +11,11 @@
 #Load config file
 . "$1"
 
-#Create output directory
-output=$directory/results/$sample/frequencies
-mkdir -p $output; rm -f $output/$sample-Frequencies.$subset.txt
-
-#Create config file for R script
+#Create R config file
 echo "sample <- '$sample'; directory <- '$directory'" > $directory/config-$sample.R
+
+#Create output directory
+output=$directory/results/$sample/frequencies mkdir -p $output; rm -f $output/*.{txt,bed}
 
 #############################################################################################################################
 for subset in "mito" "nucleus"; do
