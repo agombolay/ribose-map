f2776e8ba245e882f2a8784eb7757be4a8eb35c0
Alli Gombolay <agombolay3@gmail.com>
2018-02-18 21:43:53 -0500

    Update distribution.R

--- ribose-seq/programs/distribution.R
+++ ribose-seq/programs/distribution.R
@@ -8,13 +8,11 @@
 #2. Saves plots as png files to appropriate directory
 
 #############################################################################################################################
-#Load config file
-config <- commandArgs(TRUE)[1]
-#source(file.path(path,"config.R"))
-source(config)
+#Load library
+library(ggplot2)
 
-#Load R libraries
-library(tools); library(ggplot2); library(optparse)
+#Load config file
+source(commandArgs(TRUE)[1])
 
 #############################################################################################################################
 #Specify output directory and file
@@ -26,20 +24,14 @@ for(file in input_files){
         #Specify dataset
 	data=read.table(file, sep="\t", header=FALSE)
 			
-	#Transform values on negative strand
-	forward <- data[ which(data$V5=='+'), ]; reverse <- data[ which(data$V5=='-'), ]
-
+	labels <- c('+' = 'Forward', '-' = 'Reverse')
+	data$V5_new = factor(data$V5, levels=c('+','-'))
+	
 #############################################################################################################################
 	#Create plot
-	myplot <- ggplot() + geom_point(aes(x=forward$V3, y=forward$V4, colour="forward")) +
-	
-	geom_point(aes(x=reverse$V3, y=reverse$V4*-1, colour="reverse")) + theme(text=element_text(size=20))
-	
-	scale_colour_manual(values=c("#CC79A7", "#56B4E9"), name="") + theme(legend.key = element_blank()) +
-	
-        xlab("Chromosome Position") + ylab("rNMP Frequency")
-	
-	#Remove and replace default background theme of plot
+	myplot <- ggplot(data,aes(V3, V4, colour=V5_new)) + geom_point() + ylim(1,max(data$V4)) +
+	facet_wrap(~V5_new,ncol=1,labeller=labeller(V5_new = labels)) + theme(legend.position="none") +
+	scale_colour_manual(values=c("blue", "green3")) + xlab("Chromosome Position") + ylab("rNMP Frequency") +
 	theme(panel.grid=element_blank(), panel.background=element_blank(), axis.line=element_line(colour="black"))
 
 #############################################################################################################################
