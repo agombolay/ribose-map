2b46be8600e7193835df674eb1804d3eb10a957c
Alli Gombolay <agombolay3@gmail.com>
2018-02-19 11:39:57 -0500

    Update distribution.R

--- ribose-seq/programs/distribution.R
+++ ribose-seq/programs/distribution.R
@@ -48,7 +48,7 @@ for(file in input_files){
 
 #############################################################################################################################
 		#Save plot as PNG file
-		ggsave(filename=file.path(output, paste(file_path_sans_ext(basename(file)), ".png", sep="")), plot=myplot, width=20)
+		ggsave(filename=file.path(output, paste(file_path_sans_ext(basename(file)), ".png", sep="")), plot=myplot, width=15)
 
 }
 }
