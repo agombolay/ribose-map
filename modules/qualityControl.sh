e1202bc4ad381d7a2c6350d72e00d527dd7cebdc
Alli Gombolay <agombolay3@gmail.com>
2018-03-06 14:19:37 -0500

    Update qualityControl.sh

--- modules/qualityControl.sh
+++ modules/qualityControl.sh
@@ -22,13 +22,13 @@ fi
 #Single-end reads
 if [[ ! $raw2 ]]; then
 	fastqc "$2" -o $output
-	cutadapt $nextseq -a $adapter -m 50 "$2" -o $output/$sample-trimmed.fastq
+	cutadapt $nextseq -a $adapter -m 50 "$2" -q 10 -o $output/$sample-trimmed.fastq
 	fastqc $output/$sample-trimmed.fastq -o $output
 
 #Paired-end reads
 elif [[ $read2 ]]; then
 	fastqc "$2" "$3" -o $output
-	cutadapt $nextseq -a $adapter -m 50 "$2" "$3" -o $output/$sample-trimmed1.fastq -p $output/$sample-trimmed2.fastq
+	cutadapt $nextseq -a $adapter -m 50 "$2" "$3" -q 10 -o $output/$sample-trimmed1.fastq -p $output/$sample-trimmed2.fastq
 	fastqc $output/$sample-trimmed1.fastq $output/$sample-trimmed2.fastq -o $output
 
 fi
