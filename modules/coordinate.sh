1415ce7cb9317b6a8b22d43798f135829cdcc7d7
Alli Gombolay <agombolay3@gmail.com>
2018-04-08 17:33:48 -0400

    Update coordinate.sh

--- modules/coordinate.sh
+++ modules/coordinate.sh
@@ -17,12 +17,12 @@ output=$repository/results/$sample/coordinates; rm -rf $output; mkdir -p $output
 #############################################################################################################################
 #Remove unaligned reads
 if [[ ! $read2 ]]; then
-	samtools view -b -q 30 -F260 $repository/results/$sample/alignment/$sample.bam | samtools sort - -o $output/temp.bam
+	samtools view -b -q 20 -F260 $repository/results/$sample/alignment/$sample.bam | samtools sort - -o $output/temp.bam
 	samtools index $output/temp.bam
 
 #Keep only first read in pair
 elif [[ $read2 ]]; then
-	samtools view -b -q 30 -f67 -F260 $repository/results/$sample/alignment/$sample.bam | samtools sort - -o $output/temp.bam
+	samtools view -b -q 20 -f67 -F260 $repository/results/$sample/alignment/$sample.bam | samtools sort - -o $output/temp.bam
 	samtools index $output/temp.bam
 fi
 
