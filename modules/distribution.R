ca56ec1767786e341619c837040890d94ee9ce61
Alli Gombolay <agombolay3@gmail.com>
2018-05-22 13:40:50 -0400

    Update distribution.R

--- modules/distribution.R
+++ modules/distribution.R
@@ -50,7 +50,7 @@ for(file in input_files){
 		myplot <- ggplot(data, aes(V3, V4, colour = V5_new)) + geom_bar(stat = "identity") + xlab("Chromosome Coordinate") + ylab("Per nucleotide rNMP Coverage") +
 		
 				 #Separate strands and specify y-axis limit
-				 facet_wrap(~V5_new, ncol = 1, labeller = labeller(V5_new = labels)) + scale_y_continuous(expand = c(0.015, 0), limits = c(0, ylimit), labels = scales::percent) +
+				 facet_wrap(~V5_new, ncol = 1, labeller = labeller(V5_new = labels)) + scale_y_continuous(expand = c(0.015, 0), limits = c(0, ylimit), labels = function(x) paste0(x, "%")) +
 
 				 #Decrease space between barcharts and x/y axes
 				 scale_x_continuous(expand = c(0.015, 0)) + theme(text = element_text(size = 20), legend.position = "none", panel.background = element_blank()) + scale_colour_manual(values = c("blue", "green3")) +
