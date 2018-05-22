d9132f60779ac5263ad80e6103011331c3e5c43e
Alli Gombolay <agombolay3@gmail.com>
2018-05-22 12:54:58 -0400

    Update sequence.R

--- modules/sequence.R
+++ modules/sequence.R
@@ -66,7 +66,7 @@ for(file in input_files){
 					   #Simplify background theme, increase font size, and specify y-axis limits
 					   theme(text = element_text(size = 20)) + scale_y_continuous(limits = c(0, ylimit)) +
 					   
-					   theme(axis.line = element_line(color="black", size = 2))
+					   theme(axis.line = element_line(color="black", size = 1))
 			
 					   #Replace default background with black lines for the axes
 					   #annotate("segment", x = -Inf, xend = Inf, y = -Inf, yend = -Inf) + annotate("segment", x = -Inf, xend = -Inf, y = -Inf, yend = Inf)
