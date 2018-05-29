26e12f9e09a68c165e6e932927f2739e00b2b91d
Alli Gombolay <agombolay3@gmail.com>
2018-05-28 22:05:01 -0400

    Update coordinate.sh

--- modules/coordinate.sh
+++ modules/coordinate.sh
@@ -54,11 +54,11 @@ elif [[ $technique == "emRiboSeq" ]]; then
 	awk -v "OFS=\t" '$6 == "+" {print $1,($2 - 1),$2,$4,$5,"-"}' $output/temp1.bed | awk -v "OFS=\t" '$2 >= 0 { print }' >> $output/temp2.bed
 	
 	#Sort file by chromosome and coordinate
-	sort -k1,1 -k2,2n -k6 $output/temp2.bed > $output/temp3.bed
+	#sort -k1,1 -k2,2n -k6 $output/temp2.bed > $output/temp3.bed
 
 	#Remove coordinates of rNMPs if the end position is greater than length of chromosome
 	#join -t $'\t' <(sort $output/reference.bed) <(sort $output/temp2.bed) | awk -v "OFS=\t" '$2 >= $4 { print $1,$3,$4,$5,$6,$7 }' > $output/temp3.bed
-	join -t $'\t' <($output/reference.bed) <($output/temp3.bed) | awk -v "OFS=\t" '$2 >= $4 { print $1,$3,$4,$5,$6,$7 }' > $output/$sample.bed
+	join -t $'\t' <(sort -k1,1 $output/reference.bed) <(sort -k1,1 -k2,2n -k6 $output/temp3.bed) | awk -v "OFS=\t" '$2 >= $4 { print $1,$3,$4,$5,$6,$7 }' > $output/$sample.bed
 	
 elif [[ $technique == "HydEn-seq" ]] || [[ $technique == "Pu-seq" ]]; then
 	
