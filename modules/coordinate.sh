7cd09a5c6cacc7d45692b31a483ac4e303d52c77
Alli Gombolay <agombolay3@gmail.com>
2018-06-13 11:53:43 -0400

    Update coordinate.sh

--- modules/coordinate.sh
+++ modules/coordinate.sh
@@ -70,7 +70,7 @@ awk -v "OFS=\t" -v total="$total" '{print $1,$2,$3,$4,$5/total*100}' $output/$sa
 
 #############################################################################################################################
 #Remove temporary files
-#rm -f $output/reference.bed $output/temp{1..2}.bed
+rm -f $output/reference.bed $output/temp{1..2}.bed
 
 #Print status
 echo "Status: Coordinates module for $sample is complete"
