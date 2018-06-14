a2da8ac1a82496287c670db4c0815714c2366f7d
Alli Gombolay <agombolay3@gmail.com>
2018-06-14 15:38:46 -0400

    Update coordinate.sh

--- modules/coordinate.sh
+++ modules/coordinate.sh
@@ -68,8 +68,8 @@ fi
 
 #Calculate per nucleotide coverage
 total=$(wc -l < $output/$sample.bed)
-cut -f1,2,3,6 $output/$sample.bed | uniq -c - | awk -v "OFS=\t" '{print $2,$3,$4,$5,$1}' > $output/$sample.counts.bed
-awk -v "OFS=\t" -v total="$total" '{print $1,$2,$3,$4,$5/total*100}' $output/$sample.counts.bed > $output/$sample.normalized.bed
+cut -f1,2,3,6 $output/$sample.bed | uniq -c - | awk -v "OFS=\t" '{print $2,$3,$4,$5,$1}' > $output/$sample.counts.tab
+awk -v "OFS=\t" -v total="$total" '{print $1,$2,$3,$4,$5/total*100}' $output/$sample.counts.bed > $output/$sample.normalized.tab
 
 #############################################################################################################################
 #Remove temporary files
