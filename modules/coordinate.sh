e04f10f12317043cfa902921aefe11bcb6198182
Alli Gombolay <agombolay3@gmail.com>
2018-06-14 17:07:00 -0400

    Update coordinate.sh

--- modules/coordinate.sh
+++ modules/coordinate.sh
@@ -49,6 +49,7 @@ elif [[ $technique == "emRiboSeq" ]]; then
 	awk -v "OFS=\t" '$6 == "+" {print $1,($2 - 1),$2,$4,$5,"-"}' $output/temp1.bed | awk -v "OFS=\t" '$2 >= 0 { print }' >> $output/temp2.bed
 
 	#Remove coordinates of rNMPs if the end position is greater than length of chromosome
+	#Must sort based on chromosome before joining files; otherwise, some data will be removed)
 	join -t $'\t' <(sort -k1 $output/reference.bed) <(sort -k1 $output/temp2.bed) | awk -v "OFS=\t" '$2 >= $4 { print $1,$3,$4,$5,$6,$7 }' > $output/temp3.bed
 	
 elif [[ $technique == "HydEn-seq" ]] || [[ $technique == "Pu-seq" ]]; then
@@ -63,9 +64,11 @@ elif [[ $technique == "HydEn-seq" ]] || [[ $technique == "Pu-seq" ]]; then
 	awk -v "OFS=\t" '$6 == "-" {print $1,$3,($3 + 1),$4,$5,"-"}' $output/temp1.bed | awk -v "OFS=\t" '$2 >= 0 { print }' >> $output/temp2.bed
 
 	#Remove coordinates of rNMPs if the end position is greater than length of chromosome
+	#Must sort based on chromosome before joining files; otherwise, some data will be removed)
 	join -t $'\t' <(sort -k1 $output/reference.bed) <(sort -k1 $output/temp2.bed) | awk -v "OFS=\t" '$2 >= $4 { print $1,$3,$4,$5,$6,$7 }' > $output/temp3.bed
 fi
 
+#Sort coordinates by chromosome, position, and strand
 sort -k1,1 -k2,2n -k 6 $output/temp3.bed > $output/$sample.bed
 
 #Calculate per nucleotide coverage
