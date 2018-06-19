aab014145863094d9297cc0a1384131c30bae141
Alli Gombolay <agombolay3@gmail.com>
2018-06-18 21:09:16 -0400

    Update alignment.sh

--- modules/alignment.sh
+++ modules/alignment.sh
@@ -20,9 +20,9 @@ if [[ ! $read2 ]]; then
 	
 	if [[ ! $pattern ]]; then
 	
-		bowtie2 --threads $threads -x $basename -U $read1 -S $output/aligned.sam 2> $output/alignment.log
-		samtools view -b -S -@ $threads $output/aligned.sam | samtools sort - -@ $threads -o $output/$sample.bam
-		samtools index $output/$sample.bam	
+		time bowtie2 --threads $threads -x $basename -U $read1 -S $output/aligned.sam 2> $output/alignment.log
+		time samtools view -b -S -@ $threads $output/aligned.sam | samtools sort - -@ $threads -o $output/$sample.bam
+		time samtools index $output/$sample.bam	
 		
 	elif [[ $pattern ]]; then
 		
