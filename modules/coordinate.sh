d06ae5f2ecdca459bc736a736f87c320966b4c67
Alli Gombolay <agombolay3@gmail.com>
2018-06-19 21:36:06 -0400

    Update coordinate.sh

--- modules/coordinate.sh
+++ modules/coordinate.sh
@@ -12,13 +12,13 @@
 . "$1"
 
 #Create output directory and remove any old files
-output=$repository/results/$sample/coordinates; rm -rf $output; mkdir -p $output
+output=$repository/results/$sample/coordinate; rm -rf $output; mkdir -p $output
 			
 #############################################################################################################################
 #Convert alignment file to BED format
 if [[ ! $read2 ]]; then
 	#Remove unaligned reads
-	time samtools view -b $quality -@ $threads $repository/results/$sample/alignment/$sample.bam > $output/temp.bam
+	samtools view -b $quality -@ $threads $repository/results/$sample/alignment/$sample.bam > $output/temp.bam
 
 elif [[ $read2 ]]; then
 	#Keep first read in pair
@@ -26,10 +26,10 @@ elif [[ $read2 ]]; then
 fi
 
 #Convert BAM file to BED file
-time bedtools bamtobed -i $output/temp.bam > $output/temp1.bed
+bedtools bamtobed -i $output/temp.bam > $output/temp1.bed
 
 #Create FASTA index file and BED file for reference
-time samtools faidx $fasta && cut -f 1,2 $fasta.fai > $output/reference.bed
+samtools faidx $fasta && cut -f 1,2 $fasta.fai > $output/reference.bed
 	
 #Determine coordinates for each technique
 if [[ $technique == "ribose-seq" ]]; then
@@ -46,15 +46,15 @@ if [[ $technique == "ribose-seq" ]]; then
 elif [[ $technique == "emRiboSeq" ]]; then
 
 	#Obtain coordinates of rNMPs located on POSITIVE strand of DNA
-	time awk -v "OFS=\t" '$6 == "-" {print $1, $3, ($3 + 1), $4, $5, "+"}' $output/temp1.bed | awk -v "OFS=\t" '$2 >= 0 { print }' > $output/temp2.bed 
+	awk -v "OFS=\t" '$6 == "-" {print $1, $3, ($3 + 1), $4, $5, "+"}' $output/temp1.bed | awk -v "OFS=\t" '$2 >= 0 { print }' > $output/temp2.bed 
 	
 	#Obtain coordinates of rNMPs located on NEGATIVE strand of DNA
-	time awk -v "OFS=\t" '$6 == "+" {print $1, ($2 - 1), $2, $4, $5, "-"}' $output/temp1.bed | awk -v "OFS=\t" '$2 >= 0 { print }' >> $output/temp2.bed
+	awk -v "OFS=\t" '$6 == "+" {print $1, ($2 - 1), $2, $4, $5, "-"}' $output/temp1.bed | awk -v "OFS=\t" '$2 >= 0 { print }' >> $output/temp2.bed
 
 	#Remove coordinates of rNMPs if the end position is greater than length of chromosome
 	#Sort by chromosome, position, and strand before joining files; otherwise, some data will be removed
 	#Previous steps disorder temp2.bed since coordinates on forward/reverse strands are treated differently
-	time join -t $'\t' $output/reference.bed <(sort -k1,1 -k2,2n -k 6 $output/temp2.bed) | awk -v "OFS=\t" '$2 >= $4 { print $1, $3, $4, $5, $6, $7 }' > $output/$sample.bed
+	join -t $'\t' $output/reference.bed <(sort -k1,1 -k2,2n -k 6 $output/temp2.bed) | awk -v "OFS=\t" '$2 >= $4 { print $1, $3, $4, $5, $6, $7 }' > $output/$sample.bed
 	
 elif [[ $technique == "HydEn-seq" ]] || [[ $technique == "Pu-seq" ]]; then
 	
@@ -71,13 +71,13 @@ elif [[ $technique == "HydEn-seq" ]] || [[ $technique == "Pu-seq" ]]; then
 fi
 
 #Calculate raw and normalized (per 100) counts of rNMPs
-time total=$(wc -l < $output/$sample.bed)
-time cut -f1,2,3,6 $output/$sample.bed | uniq -c - | awk -v "OFS=\t" '{print $2, $3, $4, $5, $1}' > $output/$sample.counts.tab
-time awk -v "OFS=\t" -v total="$total" '{print $1, $2, $3, $4, $5/total*100}' $output/$sample.counts.tab > $output/$sample.normalized.tab
+total=$(wc -l < $output/$sample.bed)
+cut -f1,2,3,6 $output/$sample.bed | uniq -c - | awk -v "OFS=\t" '{print $2, $3, $4, $5, $1}' > $output/$sample.counts.tab
+awk -v "OFS=\t" -v total="$total" '{print $1, $2, $3, $4, $5/total*100}' $output/$sample.counts.tab > $output/$sample.normalized.tab
 
 #############################################################################################################################
 #Remove temporary files
-time rm -f $output/temp{1..2}.bed $output/temp.bam
+rm -f $output/temp{1..2}.bed $output/temp.bam
 
 #Print status
 echo "Status: Coordinates Module for $sample is complete"
