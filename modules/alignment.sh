91fa92ff33f4f23cd217f42fdcd0ba82eb82dec7
Alli Gombolay <agombolay3@gmail.com>
2018-07-07 15:21:30 -0400

    Update alignment.sh

--- modules/alignment.sh
+++ modules/alignment.sh
@@ -13,7 +13,7 @@
 . "$1"
 
 #Create output directory
-output=$repository/results-$quality/$sample/alignment; rm -rf $output; mkdir -p $output
+output=$repository/results/$sample/alignment; rm -rf $output; mkdir -p $output
 
 #############################################################################################################################
 if [[ ! $read2 ]]; then
