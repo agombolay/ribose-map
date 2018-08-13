674a2d7e1b152d016716d8e160b456e737f818b1
Alli Gombolay <agombolay3@gmail.com>
2018-08-12 20:47:48 -0400

    Update coordinate.sh

--- modules/coordinate.sh
+++ modules/coordinate.sh
@@ -30,44 +30,40 @@ bedtools bamtobed -i $output/temp.bam > $output/temp1.bed
 	
 #Determine coordinates for each technique
 if [[ $technique == "ribose-seq" ]]; then
-	
-	#Obtain coordinates of rNMPs located on POSITIVE strand of DNA
-	mawk -v "OFS=\t" '$6 == "-" {print $1, ($3 - 1), $3, $4, $5, "+"}' $output/temp1.bed > $output/temp2.bed
-	
-	#Obtain coordinates of rNMPs located on NEGATIVE strand of DNA
-	mawk -v "OFS=\t" '$6 == "+" {print $1, $2, ($2 + 1), $4, $5, "-"}' $output/temp1.bed >> $output/temp2.bed
-	
-	#Sort coordinates by chromosome, position, and strand
-	sort -k1,1 -k2,2n -k 6 $output/temp2.bed > $output/$sample.bed
+
+	#Obtain coordinates of rNMPs depending on the strand of DNA and sort data
+	mawk -v "OFS=\t" '{if ($6 == "-") print $1, ($3 - 1), $3, $4, $5, "+"; else if ($6 == "+") print $1, $2, ($2 + 1), $4, $5, "-";}' $output/temp1.bed | sort -k1,1 -k2,2n -k 6 > $output/$sample.bed
 	
 elif [[ $technique == "emRiboSeq" ]]; then
 
-	if [[ $check == "no" ]]; then
-		#Obtain coordinates of rNMPs depending on the strand of DNA
-		mawk -v "OFS=\t" '{if ($6 == "-") print $1, $3, ($3 + 1), $4, $5, "+"; else if ($6 == "+") print $1, ($2 - 1), $2, $4, $5, "-";}' $output/test.bed | sort -k1,1 -k2,2n -k 6 > $output/$sample.bed
+	if [[ ! $check ]]; then
+		#Obtain coordinates of rNMPs depending on the strand of DNA and sort data
+		mawk -v "OFS=\t" '{if ($6 == "-") print $1, $3, ($3 + 1), $4, $5, "+"; else if ($6 == "+") print $1, ($2 - 1), $2, $4, $5, "-";}' $output/temp1.bed | sort -k1,1 -k2,2n -k 6 > $output/$sample.bed
 	
-	elif [[ $check == "yes" ]]; then
+	elif [[ $check ]]; then
 		#Create FASTA index file and BED file for reference
 		samtools faidx $fasta && cut -f 1,2 $fasta.fai > $output/reference.bed
 
-		#Obtain coordinates of rNMPs depending on the strand of DNA and sort data before join
-		mawk -v "OFS=\t" '{if ($6 == "-") print $1, $3, ($3 + 1), $4, $5, "+"; else if ($6 == "+") print $1, ($2 - 1), $2, $4, $5, "-";}' $output/test.bed | sort -k1,1 -k2,2n -k 6 > $output/temporary.bed
+		#Obtain coordinates of rNMPs depending on the strand of DNA and sort data
+		mawk -v "OFS=\t" '{if ($6 == "-") print $1, $3, ($3 + 1), $4, $5, "+"; else if ($6 == "+") print $1, ($2 - 1), $2, $4, $5, "-";}' $output/temp1.bed | sort -k1,1 -k2,2n -k 6 > $output/temporary.bed
 
 		#Remove coordinates of rNMPs if the end position is greater than length of chromosome
-		join -t $'\t' $output/reference.bed $output/temporary.bed | mawk -v "OFS=\t" '$2 >= $4 { print $1, $3, $4, $5, $6, $7 }' > $output/$sample.bed
+		join -t $'\t' $output/reference.bed $output/temporary.bed | mawk -v "OFS=\t" '$3 >= 0 && $2 >= $4 { print $1, $3, $4, $5, $6, $7 }' > $output/$sample.bed
 
 elif [[ $technique == "HydEn-seq" ]] || [[ $technique == "Pu-seq" ]]; then
 	
-	#Obtain coordinates of rNMPs located on POSITIVE strand of DNA
-	mawk -v "OFS=\t" '$6 == "+" {print $1, ($2 - 1), $2, $4, $5, "+"}' $output/temp1.bed | awk -v "OFS=\t" '$2 >= 0 { print }' > $output/temp2.bed 
+	if [[ ! $check ]]; then
 	
-	#Obtain coordinates of rNMPs located on NEGATIVE strand of DNA
-	mawk -v "OFS=\t" '$6 == "-" {print $1, $3, ($3 + 1), $4, $5, "-"}' $output/temp1.bed | awk -v "OFS=\t" '$2 >= 0 { print }' >> $output/temp2.bed
-
-	#Remove coordinates of rNMPs if the end position is greater than length of chromosome
-	#Sort by chromosome, position, and strand before joining files; otherwise, some data will be removed
-	#Previous steps disorder temp2.bed since coordinates on forward/reverse strands are treated differently
-	join -t $'\t' $output/reference.bed <(sort -k1,1 -k2,2n -k 6 $output/temp2.bed) | awk -v "OFS=\t" '$2 >= $4 { print $1, $3, $4, $5, $6, $7 }' > $output/$sample.bed
+		#Obtain coordinates of rNMPs depending on the strand of DNA and sort data
+		mawk -v "OFS=\t" '{if ($6 == "+") print $1, ($2 - 1), $2, $4, $5, "+"; else if ($6 == "-") print $1, $3, ($3 + 1), $4, $5, "-";}' $output/temp1.bed | sort -k1,1 -k2,2n -k 6 > $output/$sample.bed
+	
+	elif [[ $check ]]; then
+	
+		#Obtain coordinates of rNMPs depending on the strand of DNA and sort data
+		mawk -v "OFS=\t" '{if ($6 == "+") print $1, ($2 - 1), $2, $4, $5, "+"; else if ($6 == "-") print $1, $3, ($3 + 1), $4, $5, "-";}' $output/temp1.bed | sort -k1,1 -k2,2n -k 6 > $output/temporary.bed
+	
+		#Remove coordinates of rNMPs if the end position is greater than length of chromosome
+		join -t $'\t' $output/reference.bed $output/temporary.bed | awk -v "OFS=\t" '$3 >= 0 && $2 >= $4 { print $1, $3, $4, $5, $6, $7 }' > $output/$sample.bed
 fi
 
 #Calculate raw and normalized (per 100) counts of rNMPs
