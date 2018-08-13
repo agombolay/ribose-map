78c3c82c79c9f8c2685f739a95e8875b042886ce
Alli Gombolay <agombolay3@gmail.com>
2018-08-13 00:11:59 -0400

    Update coordinate.sh

--- modules/coordinate.sh
+++ modules/coordinate.sh
@@ -67,7 +67,7 @@ elif [[ $technique == "HydEn-seq" ]] || [[ $technique == "Pu-seq" ]]; then
 		mawk -v "OFS=\t" '{if ($6 == "+") print $1, ($2 - 1), $2, $4, $5, "+"; else if ($6 == "-") print $1, $3, ($3 + 1), $4, $5, "-";}' $output/reads.bed | sort -k1,1 -k2,2n -k 6 > $output/temporary.bed
 	
 		#Remove coordinates of rNMPs if the end position is greater than length of chromosome
-		join -t $'\t' $output/reference.bed $output/temporary.bed | awk -v "OFS=\t" '$3 >= 0 && $2 >= $4 { print $1, $3, $4, $5, $6, $7 }' > $output/$sample.bed
+		join -t $'\t' $output/reference.bed $output/temporary.bed | mawk -v "OFS=\t" '$3 >= 0 && $2 >= $4 { print $1, $3, $4, $5, $6, $7 }' > $output/$sample.bed
 	fi
 fi
 
