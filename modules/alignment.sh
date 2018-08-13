bba7271c50df3de361de7e3a70d2c7a4c072961f
Alli Gombolay <agombolay3@gmail.com>
2018-08-12 23:36:25 -0400

    Update alignment.sh

--- modules/alignment.sh
+++ modules/alignment.sh
@@ -37,10 +37,10 @@ if [[ ! $read2 ]]; then
 		if [[ ! $barcode ]]; then
 		
 			bowtie2 --threads $threads -x $basename -U $output/extracted1.fq -S $output/aligned.sam 2> $output/alignment.log
-			samtools view -b -S -@ $threads $output/aligned.sam | samtools sort - -@ $threads -o $output/sorted.bam
+			samtools view -b -S $output/aligned.sam | samtools sort - -o $output/sorted.bam
 			samtools index $output/sorted.bam
 	
-			umi_tools dedup -v 0 -I $output/sorted.bam | samtools sort - -@ $threads -o $output/$sample.bam
+			umi_tools dedup -v 0 -I $output/sorted.bam | samtools sort - -o $output/$sample.bam
 			samtools index $output/$sample.bam
 		
 		elif [[ $barcode ]]; then
@@ -48,10 +48,10 @@ if [[ ! $read2 ]]; then
 			grep -B 1 -A 2 ^$barcode $output/extracted1.fq | sed '/^--$/d' | seqtk trimfq -b ${#barcode} - > $output/demultiplexed1.fq
   
 			bowtie2 --threads $threads -x $basename -U $output/demultiplexed1.fq -S $output/aligned.sam 2> $output/alignment.log
-			samtools view -b -S -@ $threads $output/aligned.sam | samtools sort - -@ $threads -o $output/sorted.bam
+			samtools view -b -S $output/aligned.sam | samtools sort - -o $output/sorted.bam
 			samtools index $output/sorted.bam
 	
-			umi_tools dedup -v 0 -I $output/sorted.bam | samtools sort - -@ $threads -o $output/$sample.bam
+			umi_tools dedup -v 0 -I $output/sorted.bam | samtools sort - -o $output/$sample.bam
 			samtools index $output/$sample.bam
 		fi
 	fi
@@ -77,10 +77,10 @@ elif [[ $read2 ]]; then
 		if [[ ! $barcode ]]; then
 		
 			bowtie2 --threads $threads -x $basename -1 $output/extracted1.fq -2 $output/extracted2.fq -S $output/aligned.sam 2> $output/alignment.log
-			samtools view -b -S -@ $threads $output/aligned.sam | samtools sort - -@ $threads -o $output/sorted.bam
+			samtools view -b -S $output/aligned.sam | samtools sort - -o $output/sorted.bam
 			samtools index $output/sorted.bam
 	
-			umi_tools dedup -v 0 --paired -I $output/sorted.bam | samtools sort - -@ $threads -o $output/$sample.bam
+			umi_tools dedup -v 0 --paired -I $output/sorted.bam | samtools sort - -o $output/$sample.bam
 			samtools index $output/$sample.bam
 		
 		elif [[ $barcode ]]; then
@@ -88,10 +88,10 @@ elif [[ $read2 ]]; then
 			grep -B 1 -A 2 ^$barcode $output/extracted1.fq | sed '/^--$/d' | seqtk trimfq -b ${#barcode} - > $output/demultiplexed1.fq
 			
 			bowtie2 --threads $threads -x $basename -1 $output/demultiplexed1.fq -2 $output/extracted2.fq -S $output/aligned.sam 2> $output/alignment.log
-			samtools view -b -S -@ $threads $output/aligned.sam | samtools sort - -@ $threads -o $output/sorted.bam
+			samtools view -b -S $output/aligned.sam | samtools sort - -o $output/sorted.bam
 			samtools index $output/sorted.bam
 	
-			umi_tools dedup -v 0 --paired -I $output/sorted.bam | samtools sort - -@ $threads -o $output/$sample.bam
+			umi_tools dedup -v 0 --paired -I $output/sorted.bam | samtools sort - -o $output/$sample.bam
 			samtools index $output/$sample.bam
 		fi
 	fi
