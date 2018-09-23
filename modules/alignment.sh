289a9f264c400386328460f6dff3fe54656a1722
Alli Gombolay <agombolay3@gmail.com>
2018-09-23 13:44:33 -0400

    Update alignment.sh

--- modules/alignment.sh
+++ modules/alignment.sh
@@ -22,11 +22,11 @@ if [[ ! $read2 ]]; then
 
 		if [[ ! $sort ]]; then
 			bowtie2 --threads $threads -x $basename -U $read1 -S $output/aligned.sam 2> $output/alignment.log
-			samtools view -b -S $output/aligned.sam -o $output/$sample.bam
+			samtools view -bS -@ $threads $output/aligned.sam -o $output/$sample.bam
 
 		elif [[ $sort ]]; then
 			bowtie2 --threads $threads -x $basename -U $read1 -S $output/aligned.sam 2> $output/alignment.log
-			samtools view -b -S $output/aligned.sam | samtools sort - -o $output/$sample.bam
+			samtools view -bS -@ $threads $output/aligned.sam | samtools sort - -o $output/$sample.bam
 			samtools index $output/$sample.bam	
 		fi
 		
