e9f83f26483cd1922a794de803011a93de4863dc
Alli Gombolay <agombolay3@gmail.com>
2018-10-13 16:12:41 -0400

    Update sequence.sh

--- modules/sequence.sh
+++ modules/sequence.sh
@@ -16,11 +16,11 @@ output=$repository/results/$sample/sequence-$quality
 rm -rf $output; mkdir -p $output
 
 #############################################################################################################################
-if technique; then
-	#Create FASTA index file and BED file for reference
-	samtools faidx $fasta && cut -f 1,2 $fasta.fai > $output/reference.bed
+#Create FASTA index file and BED file for reference
+samtools faidx $fasta && cut -f 1,2 $fasta.fai > $output/reference.bed
 
-	#Create file of rNMP counts in case user did not use Coordinate Module
+if [[ ! $technique ]]; then
+	#Create file of rNMP counts if user did not use Coordinate Module
 	cut -f1,2,3,6 $repository/results/$sample/coordinate-$quality/$sample.bed | uniq -c - | mawk -v "OFS=\t" '{print $2, $3, $4, $5, $1}' > $output/$sample.counts.tab
 fi
 
