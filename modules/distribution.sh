ad9bacf2474162597a95c1464d91af442d4aad6b
Alli Gombolay <agombolay3@gmail.com>
2018-10-13 16:40:59 -0400

    Update distribution.sh

--- modules/distribution.sh
+++ modules/distribution.sh
@@ -16,7 +16,9 @@ output=$repository/results/$sample/distribution-$quality
 rm -rf $output; mkdir -p $output
 
 #############################################################################################################################
-mawk -v "OFS=\t" -v total="$(wc -l < $output/$sample.bed)" '{print $1, $2, $3, $4, $5/total*100}' $output/$sample.counts.tab > $output/$sample.normalized.tab
+#Calculate normalized counts of rNMPs
+mawk -v "OFS=\t" -v total="$(wc -l < $repository/results/$sample/coordinate$quality/$sample.bed)" '{print $1, $2, $3, $4, $5/total*100}' \
+$repository/results/$sample/coordinate$quality/$sample.counts.tab > $repository/results/$sample/coordinate$quality/$sample.normalized.tab
 
 #Create FASTA index file and BED file for reference
 samtools faidx $fasta && cut -f 1,2 $fasta.fai > $output/reference.bed
