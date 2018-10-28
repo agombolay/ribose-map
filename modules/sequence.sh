bb6f7ccce644cc8d41257ec23dd7900f4ff75b00
Alli Gombolay <agombolay3@gmail.com>
2018-10-28 18:00:33 -0400

    Update sequence.sh

--- modules/sequence.sh
+++ modules/sequence.sh
@@ -252,17 +252,13 @@ for nuc in "A" "C" "G" "T" "Combined"; do
 					C=$(paste $output/C_Up.txt $output/C_Ribo.txt $output/C_Down.txt)
 					G=$(paste $output/G_Up.txt $output/G_Ribo.txt $output/G_Down.txt)
 					T=$(paste $output/T_Up.txt $output/T_Ribo.txt $output/T_Down.txt)
+
+					#Add positions and frequencies of nucleotides in correct order to create dataset (not normalized to anything)
+					paste <(echo "$(seq -100 1 100)") <(cat <(echo "$Up") <(echo "$Ribo") <(echo "$Down")) >> $output/$sample.$nuc.$region.raw.tab
 					
-					for i in $A; do echo $i/$A_BkgFreq | bc -l; done
-					for i in $C; do echo $i/$C_BkgFreq | bc -l; done
-					for i in $G; do echo $i/$G_BkgFreq | bc -l; done
-					for i in $T; do echo $i/$T_BkgFreq | bc -l; done
-					
-					#Add positions and frequencies of nucleotides in correct order to create dataset (normalized)
-					paste <(echo "$(seq -100 1 100)") <(cat <(echo "$Up") <(echo "$Ribo") <(echo "$Down")) >> $output/$sample.$nuc.$region.unnormalized.tab
-					
-					#Add positions and frequencies of nucleotides in correct order to create dataset (unnormalized)
-					paste <(echo "$(seq -100 1 100)") <(cat <(echo "$Up") <(echo "$Ribo") <(echo "$Down")) >> $output/$sample.$nuc.$region.unnormalized.tab
+					#Add positions and frequencies of nucleotides in correct order to create dataset (normalized to reference genome)
+					paste <(echo "$(seq -100 1 100)") <(for i in $A; do echo $i/$A_BkgFreq | bc -l; done) <(for j in $C; do echo $j/$C_BkgFreq | bc -l; done) \
+					<(for k in $G; do echo $k/$G_BkgFreq | bc -l; done) <(for l in $T; do echo $l/$T_BkgFreq | bc -l; done)) >> $output/$sample.$nuc.$region.normalized.tab
 					
 #############################################################################################################################
 					#Print status
