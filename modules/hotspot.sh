3aee437b37f10f6abe274e13c6d81fc51dbcb4e6
Alli Gombolay <agombolay3@gmail.com>
2018-11-18 19:41:27 -0500

    Update hotspot.sh

--- modules/hotspot.sh
+++ modules/hotspot.sh
@@ -23,85 +23,85 @@ output=$directory/hotspots; rm -rf $output; mkdir $output
 for option in "all" "percentile"; do
 
 ######################################################################################################################################################
-		if [[ $option == "all" ]]; then
+	if [[ $option == "all" ]]; then
 			
-			#CREATE FILE 1
-			#Combine files into one
-			cat $directory/counts/*.tab >> $output/combined.unsorted.$option.tsv
+		#CREATE FILE 1
+		#Combine files into one
+		cat $directory/counts/*.tab >> $output/combined.unsorted.$option.tsv
 
-			#Print total number of libraries and number of libraries that have a particular coordinate
-			sort -k1,1 -k2,2n -k4,4 -k5 $output/combined.unsorted.$option.tsv | cut -f1,2,3,4,5,6 | uniq -c | awk -v "OFS=\t" \
-			-v "x=$(ls $directory/counts/*.tab | wc -l)" '{print $2 "_" $3 "_" $4 "_" $5, $6, $7, $1, x}' > $output/combined.sorted.$option.tsv
+		#Print total number of libraries and number of libraries that have a particular coordinate
+		sort -k1,1 -k2,2n -k4,4 -k5 $output/combined.unsorted.$option.tsv | cut -f1,2,3,4,5,6 | uniq -c | awk -v "OFS=\t" \
+		-v "x=$(ls $directory/counts/*.tab | wc -l)" '{print $2 "_" $3 "_" $4 "_" $5, $6, $7, $1, x}' > $output/combined.sorted.$option.tsv
 
-			#CREATE FILE 2
-			#Combine coordinates into one column using the underscore character and add name of library
-			for file in $(ls $directory/counts/*.tab); do
+		#CREATE FILE 2
+		#Combine coordinates into one column using the underscore character and add name of library
+		for file in $(ls $directory/counts/*.tab); do
 
-				sample=$(basename "$file" | cut -d. -f1)
-				cut -f1,2,3,4 $file | awk -v "OFS=\t" -v "y=$sample" '{print $1 "_" $2 "_" $3 "_" $4, y}' | sort > $output/$sample.temp1.$option.tsv
+			sample=$(basename "$file" | cut -d. -f1)
+			cut -f1,2,3,4 $file | awk -v "OFS=\t" -v "y=$sample" '{print $1 "_" $2 "_" $3 "_" $4, y}' | sort > $output/$sample.temp1.$option.tsv
 
-			done
+		done
 
-			#Join first two temporary files together based on their chromosomal coordinates
-			file1=$(ls $output/*.temp1.$option.tsv | head -1)
-			file2=$(ls $output/*.temp1.$option.tsv | head -2 | tail -1)
+		#Join first two temporary files together based on their chromosomal coordinates
+		file1=$(ls $output/*.temp1.$option.tsv | head -1)
+		file2=$(ls $output/*.temp1.$option.tsv | head -2 | tail -1)
 
-			command="join -t $'\t' $file1 $file2 -a1 -a2"
-			eval $command
+		command="join -t $'\t' $file1 $file2 -a1 -a2"
+		eval $command
 
-			#Join previous output with 3rd, 4th files etc. until all files have been joined
-			for file in $(ls $output/*.temp1.$option.tsv | tail -n+3); do
-				command+="| join -t $'\t' $file - -a1 -a2"
-			done
+		#Join previous output with 3rd, 4th files etc. until all files have been joined
+		for file in $(ls $output/*.temp1.$option.tsv | tail -n+3); do
+			command+="| join -t $'\t' $file - -a1 -a2"
+		done
 
-			command+="> $output/combined.joined.$option.tsv"
-			eval $command
+		command+="> $output/combined.joined.$option.tsv"
+		eval $command
 
-			#Join two files together into one
-			join -t $'\t' <(sort $output/combined.sorted.$option.tsv) <(sort $output/combined.joined.$option.tsv) | sed 's/_/\t/g' | sort -nrk 5,5 \
-			> $output/hotspots.$option.tsv
+		#Join two files together into one
+		join -t $'\t' <(sort $output/combined.sorted.$option.tsv) <(sort $output/combined.joined.$option.tsv) | sed 's/_/\t/g' | sort -nrk 5,5 \
+		> $output/hotspots.$option.tsv
 
 ######################################################################################################################################################
-		elif [[ $option == "percentile" ]]; then
+	elif [[ $option == "percentile" ]]; then
 			
-			#CREATE FILE 1
-			#Combine files into one
-			/users/alligombolay/desktop/hotspots/percentile.sh $directory
-			cat $directory/percentiles/*top1%.tsv >> $output/combined.unsorted.$option.tsv
+		#CREATE FILE 1
+		#Combine files into one
+		/users/alligombolay/desktop/hotspots/percentile.sh $directory
+		cat $directory/percentiles/*top1%.tsv >> $output/combined.unsorted.$option.tsv
 
-			#Print total number of libraries and number of libraries that have a particular coordinate
-			sort -k1,1 -k2,2n -k4,4 -k5 $output/combined.unsorted.$option.tsv | cut -f1,2,3,4,5,6 | uniq -c | awk -v "OFS=\t" \
-			-v "x=$(ls $directory/counts/*.tab | wc -l)" '{print $2 "_" $3 "_" $4 "_" $5, $6, $7, $1, x}' > $output/combined.sorted.$option.tsv
+		#Print total number of libraries and number of libraries that have a particular coordinate
+		sort -k1,1 -k2,2n -k4,4 -k5 $output/combined.unsorted.$option.tsv | cut -f1,2,3,4,5,6 | uniq -c | awk -v "OFS=\t" \
+		-v "x=$(ls $directory/counts/*.tab | wc -l)" '{print $2 "_" $3 "_" $4 "_" $5, $6, $7, $1, x}' > $output/combined.sorted.$option.tsv
 
-			#CREATE FILE 2
-			#Combine coordinates into one column using the underscore character and add name of library
-			for file in $(ls $directory/percentiles/*top1%.tsv); do
+		#CREATE FILE 2
+		#Combine coordinates into one column using the underscore character and add name of library
+		for file in $(ls $directory/percentiles/*top1%.tsv); do
 
-				sample=$(basename "$file" | cut -d. -f1)
-				cut -f1,2,3,4 $file | awk -v "OFS=\t" -v "y=$sample" '{print $1 "_" $2 "_" $3 "_" $4, y}' | sort > $output/$sample.temp1.$option.tsv
+			sample=$(basename "$file" | cut -d. -f1)
+			cut -f1,2,3,4 $file | awk -v "OFS=\t" -v "y=$sample" '{print $1 "_" $2 "_" $3 "_" $4, y}' | sort > $output/$sample.temp1.$option.tsv
 
-			done
+		done
 
-			#Join first two temporary files together based on their chromosomal coordinates
-			file1=$(ls $output/*.temp1.$option.tsv | head -1)
-			file2=$(ls $output/*.temp1.$option.tsv | head -2 | tail -1)
+		#Join first two temporary files together based on their chromosomal coordinates
+		file1=$(ls $output/*.temp1.$option.tsv | head -1)
+		file2=$(ls $output/*.temp1.$option.tsv | head -2 | tail -1)
 
-			command="join -t $'\t' $file1 $file2 -a1 -a2"
-			eval $command
+		command="join -t $'\t' $file1 $file2 -a1 -a2"
+		eval $command
 
-			#Join previous output with 3rd, 4th files etc. until all files have been joined
-			for file in $(ls $output/*.temp1.$option.tsv | tail -n+3); do
-				command+="| join -t $'\t' $file - -a1 -a2"
-			done
+		#Join previous output with 3rd, 4th files etc. until all files have been joined
+		for file in $(ls $output/*.temp1.$option.tsv | tail -n+3); do
+			command+="| join -t $'\t' $file - -a1 -a2"
+		done
 
-			command+="> $output/combined.joined.$option.tsv"
-			eval $command
+		command+="> $output/combined.joined.$option.tsv"
+		eval $command
 
-			#Join two files together into one
-			join -t $'\t' <(sort $output/combined.sorted.$option.tsv) <(sort $output/combined.joined.$option.tsv) | sed 's/_/\t/g' | sort -nrk 5,5 \
-			> $output/hotspots.$option.tsv
+		#Join two files together into one
+		join -t $'\t' <(sort $output/combined.sorted.$option.tsv) <(sort $output/combined.joined.$option.tsv) | sed 's/_/\t/g' | sort -nrk 5,5 \
+		> $output/hotspots.$option.tsv
 
-		fi
+	fi
 ######################################################################################################################################################
 done
 
