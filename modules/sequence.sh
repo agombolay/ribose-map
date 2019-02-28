e8df7f3949773f2e4d443cb85acb31ea4861b3eb
Alli Gombolay <agombolay3@gmail.com>
2019-02-28 11:19:52 -0500

    Update sequence.sh

--- modules/sequence.sh
+++ modules/sequence.sh
@@ -173,8 +173,8 @@ for nuc in "A" "C" "G" "T" "Combined"; do
 				if [[ -s $output/Coords.$nuc.$region.bed ]]; then
 			
 					#Obtain coordinates of flanking sequences and remove coordinates where start = end
-					bedtools flank -i $output/Coords.$nuc.$region.bed -s -g $output/reference.bed -l 100 -r 0 | awk '$2 != $3' > $output/Up.bed
-					bedtools flank -i $output/Coords.$nuc.$region.bed -s -g $output/reference.bed -l 0 -r 100 | awk '$2 != $3' > $output/Down.bed
+					bedtools flank -i $output/Coords.$nuc.$region.bed -s -g $repository/references/$(basename $fasta .fa).bed -l 100 -r 0 | awk '$2 != $3' > $output/Up.bed
+					bedtools flank -i $output/Coords.$nuc.$region.bed -s -g $repository/references/$(basename $fasta .fa).bed -l 0 -r 100 | awk '$2 != $3' > $output/Down.bed
 	
 					#Obtain nucleotides flanking rNMPs (reverse order of up) and insert tabs bases for easier parsing
 					bedtools getfasta -s -fi $repository/references/temp.fa -bed $output/Down.bed | grep -v '>' | sed 's/.../& /2g;s/./& /g' > $output/Down.tab
