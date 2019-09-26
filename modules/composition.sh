f42750f4375f1b83d8f304f0aa5f1340cc04973b
Alli Gombolay <agombolay3@gmail.com>
2019-09-26 14:47:48 -0400

    Update composition.sh

--- modules/composition.sh
+++ modules/composition.sh
@@ -52,6 +52,69 @@ done
 
 ######################################################################################################################################################
 
+#Create .fai file for reference
+samtools faidx $fasta
+
+#Create .bed file for reference
+cut -f 1,2 $fasta.fai > $(dirname $fasta)/$(basename $fasta .fa).bed
+
+#######################################################################################################
+
+#Subset FASTA file based on region
+for region in "nucleus" "$mito" "$other; do
+
+	if [[ $region == "nucleus" ]]; then
+			
+			chr=$(awk '{print $1}' $(dirname $fasta)/$(basename $fasta .fa).bed | grep -wv $mito - | grep -wv $other -)
+			samtools faidx $fasta $chr > $(dirname $fasta)/$(basename $fasta .fa)_nucleus.fa
+			samtools faidx $(dirname $fasta)/$(basename $fasta .fa)_nucleus.fa
+		fi
+		
+	elif [[ $region == "mitochondria" ]]; then
+		
+			chr=$(awk '{print $1}' $(dirname $fasta)/$(basename $fasta .fa).bed | grep -w $mito -)
+			samtools faidx $fasta $chr > $(dirname $fasta)/$(basename $fasta .fa)_mitochondria.fa
+			samtools faidx $(dirname $fasta)/$(basename $fasta .fa)_mitochondria.fa
+		fi
+
+	elif [[ $region == "other" ]]; then
+		
+			chr=$(awk '{print $1}' $(dirname $fasta)/$(basename $fasta .fa).bed | grep -w $other -)
+			samtools faidx $fasta $chr > $(dirname $fasta)/$(basename $fasta .fa)_mitochondria.fa
+			samtools faidx $(dirname $fasta)/$(basename $fasta .fa)_mitochondria.fa
+		fi
+	fi
+	
+#######################################################################################################
+
+	#Calculate counts of each nucleotide
+	A_Bkg=$(grep -v '>' $(dirname $fasta)/$(basename $fasta .fa)_$region.fa | grep -Eo 'A|a' - | wc -l)
+	C_Bkg=$(grep -v '>' $(dirname $fasta)/$(basename $fasta .fa)_$region.fa | grep -Eo 'C|c' - | wc -l)
+	G_Bkg=$(grep -v '>' $(dirname $fasta)/$(basename $fasta .fa)_$region.fa | grep -Eo 'G|g' - | wc -l)
+	T_Bkg=$(grep -v '>' $(dirname $fasta)/$(basename $fasta .fa)_$region.fa | grep -Eo 'T|t' - | wc -l)
+	
+	#Calculate total number of nucleotides
+	BkgTotal=$(($A_Bkg + $C_Bkg + $G_Bkg + $T_Bkg))
+		
+	#Calculate frequency of each nucleotide
+	A_BkgFreq=$(echo "($A_Bkg + $T_Bkg)/($BkgTotal*2)" | bc -l)
+	C_BkgFreq=$(echo "($C_Bkg + $G_Bkg)/($BkgTotal*2)" | bc -l)
+	G_BkgFreq=$(echo "($G_Bkg + $C_Bkg)/($BkgTotal*2)" | bc -l)
+	T_BkgFreq=$(echo "($T_Bkg + $A_Bkg)/($BkgTotal*2)" | bc -l)
+		
+	#Save background frequencies of dNMPs to TXT files
+	echo $A_BkgFreq | xargs printf "%.*f\n" 5 > $output/A_Bkg.txt
+	echo $C_BkgFreq | xargs printf "%.*f\n" 5 > $output/C_Bkg.txt
+	echo $G_BkgFreq | xargs printf "%.*f\n" 5 > $output/G_Bkg.txt
+	echo $T_BkgFreq | xargs printf "%.*f\n" 5 > $output/T_Bkg.txt
+
+	#Combine dNMP frequencies into one file
+	Bkg=$(paste $output/{A,C,G,T}_Bkg.txt)
+
+done
+
+#######################################################################################################
+
 #Remove temporary files
 rm $output/*.nucs.tab
 
