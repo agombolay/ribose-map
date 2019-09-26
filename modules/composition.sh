6357c875e7694c5e6b15febafb724d64a3e8aaa5
Alli Gombolay <agombolay3@gmail.com>
2019-09-26 15:17:49 -0400

    Update composition.sh

--- modules/composition.sh
+++ modules/composition.sh
@@ -71,9 +71,6 @@ for region in "nucleus" "$mito" "$other; do
 	echo $G_BkgFreq | xargs printf "%.*f\n" 5 > $output/G_Bkg.txt
 	echo $T_BkgFreq | xargs printf "%.*f\n" 5 > $output/T_Bkg.txt
 
-	#Combine dNMP frequencies into one file
-	Bkg=$(paste $output/{A,C,G,T}_Bkg.txt)
-
 ######################################################################################################################################################
 
 	#Separate BED file by oraganelle and get nucleotide for each chromosomal coordinate
