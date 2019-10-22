b1c61c9a52cef6b79654f008e7d6598c3d753cb8
Alli Gombolay <agombolay3@gmail.com>
2019-10-22 16:19:22 -0400

    Update composition.sh

--- modules/composition.sh
+++ modules/composition.sh
@@ -131,13 +131,13 @@ for file in $output/${sample}-*.nucs.tab; do
 	#Calculate total number of nucleotides
 	BkgTotal=$(($A_Bkg + $C_Bkg + $G_Bkg + $T_Bkg))
 		
-	#Calculate normalized frequencies of each nucleotide
+	#Calculate frequencies of each nucleotide
 	A_BkgFreq=$(echo "($A_Bkg + $T_Bkg)/($BkgTotal*2)" | bc -l)
 	C_BkgFreq=$(echo "($C_Bkg + $G_Bkg)/($BkgTotal*2)" | bc -l)
 	G_BkgFreq=$(echo "($G_Bkg + $C_Bkg)/($BkgTotal*2)" | bc -l)
 	T_BkgFreq=$(echo "($T_Bkg + $A_Bkg)/($BkgTotal*2)" | bc -l)
 		
-	#Save frequencies of each nucleotide to .txt file
+	#Save nucleotide frequencies to .txt file
 	echo $A_BkgFreq | xargs printf "%.*f\n" 5 > $output/A_Bkg.txt
 	echo $C_BkgFreq | xargs printf "%.*f\n" 5 > $output/C_Bkg.txt
 	echo $G_BkgFreq | xargs printf "%.*f\n" 5 > $output/G_Bkg.txt
@@ -173,7 +173,7 @@ for file in $output/${sample}-*.nucs.tab; do
 	G_RiboFreq2=$(echo "$G_RiboFreq1/($A_RiboFreq1 + $C_RiboFreq1 + $G_RiboFreq1 + $U_RiboFreq1)" | bc -l)
 	U_RiboFreq2=$(echo "$U_RiboFreq1/($A_RiboFreq1 + $C_RiboFreq1 + $G_RiboFreq1 + $U_RiboFreq1)" | bc -l)
 	
-	#Save frequencies of each nucleotide to .txt file
+	#Save nucleotide frequencies to .txt file
 	paste <(echo -e "rA") <(echo "$A_RiboFreq2") >> $output/${sample}-$region.counts.txt
 	paste <(echo -e "rC") <(echo "$C_RiboFreq2") >> $output/${sample}-$region.counts.txt
 	paste <(echo -e "rG") <(echo "$G_RiboFreq2") >> $output/${sample}-$region.counts.txt
