41e790106028adbd47d4bd45c720f3f9f64fb466
Alli Gombolay <agombolay3@gmail.com>
2019-10-24 12:35:00 -0400

    Update sequence.sh

--- modules/sequence.sh
+++ modules/sequence.sh
@@ -113,30 +113,29 @@ for nuc in "A" "C" "G" "T" "Combined"; do
 			
 				temp=$(echo $file | awk -F '[-]' '{print $2 $3 $4}')
 				region=$(basename $temp .nucs.tab)
+			
+				#Nucleotide Frequencies of Reference Genome
+			
+				#Calculate counts of each nucleotide
+				A_Bkg=$(grep -v '>' $(dirname $fasta)/$(basename $fasta .fa)_$region.fa | grep -Eo 'A|a' - | wc -l)
+				C_Bkg=$(grep -v '>' $(dirname $fasta)/$(basename $fasta .fa)_$region.fa | grep -Eo 'C|c' - | wc -l)
+				G_Bkg=$(grep -v '>' $(dirname $fasta)/$(basename $fasta .fa)_$region.fa | grep -Eo 'G|g' - | wc -l)
+				T_Bkg=$(grep -v '>' $(dirname $fasta)/$(basename $fasta .fa)_$region.fa | grep -Eo 'T|t' - | wc -l)
 	
-			#Calculate counts of each nucleotide
-			A_Bkg=$(grep -v '>' $(dirname $fasta)/$(basename $fasta .fa)_$region.fa | grep -Eo 'A|a' - | wc -l)
-			C_Bkg=$(grep -v '>' $(dirname $fasta)/$(basename $fasta .fa)_$region.fa | grep -Eo 'C|c' - | wc -l)
-			G_Bkg=$(grep -v '>' $(dirname $fasta)/$(basename $fasta .fa)_$region.fa | grep -Eo 'G|g' - | wc -l)
-			T_Bkg=$(grep -v '>' $(dirname $fasta)/$(basename $fasta .fa)_$region.fa | grep -Eo 'T|t' - | wc -l)
-	
-			#Calculate total number of nucleotides
-			BkgTotal=$(($A_Bkg + $C_Bkg + $G_Bkg + $T_Bkg))
+				#Calculate total number of nucleotides
+				BkgTotal=$(($A_Bkg + $C_Bkg + $G_Bkg + $T_Bkg))
 		
-			#Calculate frequency of each nucleotide
-			A_BkgFreq=$(echo "($A_Bkg + $T_Bkg)/($BkgTotal*2)" | bc -l)
-			C_BkgFreq=$(echo "($C_Bkg + $G_Bkg)/($BkgTotal*2)" | bc -l)
-			G_BkgFreq=$(echo "($G_Bkg + $C_Bkg)/($BkgTotal*2)" | bc -l)
-			T_BkgFreq=$(echo "($T_Bkg + $A_Bkg)/($BkgTotal*2)" | bc -l)
-		
-			#Save background frequencies of dNMPs to TXT files
-			echo $A_BkgFreq | xargs printf "%.*f\n" 5 > $output/A_Bkg.txt
-			echo $C_BkgFreq | xargs printf "%.*f\n" 5 > $output/C_Bkg.txt
-			echo $G_BkgFreq | xargs printf "%.*f\n" 5 > $output/G_Bkg.txt
-			echo $T_BkgFreq | xargs printf "%.*f\n" 5 > $output/T_Bkg.txt
-
-			#Combine dNMP frequencies into one file
-			Bkg=$(paste $output/{A,C,G,T}_Bkg.txt)
+				#Calculate frequencies of each nucleotide
+				A_BkgFreq=$(echo "($A_Bkg + $T_Bkg)/($BkgTotal*2)" | bc -l)
+				C_BkgFreq=$(echo "($C_Bkg + $G_Bkg)/($BkgTotal*2)" | bc -l)
+				G_BkgFreq=$(echo "($G_Bkg + $C_Bkg)/($BkgTotal*2)" | bc -l)
+				T_BkgFreq=$(echo "($T_Bkg + $A_Bkg)/($BkgTotal*2)" | bc -l)
+			
+				#Save nucleotide frequencies to .txt file
+				paste <(echo -e "A") <(echo "$A_BkgFreq" | xargs printf "%.*f\n" 5) > "${fasta%.*}"-Freqs.$region.txt
+				paste <(echo -e "C") <(echo "$C_BkgFreq" | xargs printf "%.*f\n" 5) >> "${fasta%.*}"-Freqs.$region.txt
+				paste <(echo -e "G") <(echo "$G_BkgFreq" | xargs printf "%.*f\n" 5) >> "${fasta%.*}"-Freqs.$region.txt
+				paste <(echo -e "U") <(echo "$T_BkgFreq" | xargs printf "%.*f\n" 5) >> "${fasta%.*}"-Freqs.$region.txt
 
 #############################################################################################################################
 			#STEP 2: Create and save file containing background dNMP frequencies
