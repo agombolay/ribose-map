45b4e3972fb9ce07c604fd829c2eb8ac9a4d15aa
Alli Gombolay <agombolay3@gmail.com>
2019-10-24 13:10:50 -0400

    Update sequence.sh

--- modules/sequence.sh
+++ modules/sequence.sh
@@ -106,38 +106,39 @@ fi
 #############################################################################################################################
 
 #STEP 1: Calculate frequencies of reference genome
-for nuc in "A" "C" "G" "T" "Combined"; do
-	for region in "nucleus" "mitochondria"; do
+for file in $(dirname $fasta)/$(basename $fasta .fa)_*.fa; do
 			
-			for file in $(dirname $fasta)/$(basename $fasta .fa)_*.fa; do
+	temp=$(echo $file | awk -F '[-]' '{print $2 $3 $4}')
+	region=$(basename $temp .nucs.tab)
 			
-				temp=$(echo $file | awk -F '[-]' '{print $2 $3 $4}')
-				region=$(basename $temp .nucs.tab)
+	#Nucleotide Frequencies of Reference Genome
 			
-				#Nucleotide Frequencies of Reference Genome
-			
-				#Calculate counts of each nucleotide
-				A_Bkg=$(grep -v '>' $(dirname $fasta)/$(basename $fasta .fa)_$region.fa | grep -Eo 'A|a' - | wc -l)
-				C_Bkg=$(grep -v '>' $(dirname $fasta)/$(basename $fasta .fa)_$region.fa | grep -Eo 'C|c' - | wc -l)
-				G_Bkg=$(grep -v '>' $(dirname $fasta)/$(basename $fasta .fa)_$region.fa | grep -Eo 'G|g' - | wc -l)
-				T_Bkg=$(grep -v '>' $(dirname $fasta)/$(basename $fasta .fa)_$region.fa | grep -Eo 'T|t' - | wc -l)
+	#Calculate counts of each nucleotide
+	A_Bkg=$(grep -v '>' $(dirname $fasta)/$(basename $fasta .fa)_$region.fa | grep -Eo 'A|a' - | wc -l)
+	C_Bkg=$(grep -v '>' $(dirname $fasta)/$(basename $fasta .fa)_$region.fa | grep -Eo 'C|c' - | wc -l)
+	G_Bkg=$(grep -v '>' $(dirname $fasta)/$(basename $fasta .fa)_$region.fa | grep -Eo 'G|g' - | wc -l)
+	T_Bkg=$(grep -v '>' $(dirname $fasta)/$(basename $fasta .fa)_$region.fa | grep -Eo 'T|t' - | wc -l)
 	
-				#Calculate total number of nucleotides
-				BkgTotal=$(($A_Bkg + $C_Bkg + $G_Bkg + $T_Bkg))
+	#Calculate total number of nucleotides
+	BkgTotal=$(($A_Bkg + $C_Bkg + $G_Bkg + $T_Bkg))
 		
-				#Calculate frequencies of each nucleotide
-				A_BkgFreq=$(echo "($A_Bkg + $T_Bkg)/($BkgTotal*2)" | bc -l)
-				C_BkgFreq=$(echo "($C_Bkg + $G_Bkg)/($BkgTotal*2)" | bc -l)
-				G_BkgFreq=$(echo "($G_Bkg + $C_Bkg)/($BkgTotal*2)" | bc -l)
-				T_BkgFreq=$(echo "($T_Bkg + $A_Bkg)/($BkgTotal*2)" | bc -l)
+	#Calculate frequencies of each nucleotide
+	A_BkgFreq=$(echo "($A_Bkg + $T_Bkg)/($BkgTotal*2)" | bc -l)
+	C_BkgFreq=$(echo "($C_Bkg + $G_Bkg)/($BkgTotal*2)" | bc -l)
+	G_BkgFreq=$(echo "($G_Bkg + $C_Bkg)/($BkgTotal*2)" | bc -l)
+	T_BkgFreq=$(echo "($T_Bkg + $A_Bkg)/($BkgTotal*2)" | bc -l)
 			
-				#Save nucleotide frequencies to .txt file
-				paste <(echo -e "A") <(echo "$A_BkgFreq" | xargs printf "%.*f\n" 5) > "${fasta%.*}"-Freqs.$region.txt
-				paste <(echo -e "C") <(echo "$C_BkgFreq" | xargs printf "%.*f\n" 5) >> "${fasta%.*}"-Freqs.$region.txt
-				paste <(echo -e "G") <(echo "$G_BkgFreq" | xargs printf "%.*f\n" 5) >> "${fasta%.*}"-Freqs.$region.txt
-				paste <(echo -e "U") <(echo "$T_BkgFreq" | xargs printf "%.*f\n" 5) >> "${fasta%.*}"-Freqs.$region.txt
-			
+	#Save nucleotide frequencies to .txt file
+	paste <(echo -e "A") <(echo "$A_BkgFreq" | xargs printf "%.*f\n" 5) > "${fasta%.*}"-Freqs.$region.txt
+	paste <(echo -e "C") <(echo "$C_BkgFreq" | xargs printf "%.*f\n" 5) >> "${fasta%.*}"-Freqs.$region.txt
+	paste <(echo -e "G") <(echo "$G_BkgFreq" | xargs printf "%.*f\n" 5) >> "${fasta%.*}"-Freqs.$region.txt
+	paste <(echo -e "U") <(echo "$T_BkgFreq" | xargs printf "%.*f\n" 5) >> "${fasta%.*}"-Freqs.$region.txt
+
+done
+
 #############################################################################################################################
+for nuc in "A" "C" "G" "T" "Combined"; do
+
 			#STEP 3: Calculate frequencies of rNMPs in libraries
 		
 			#Extract only unique coordinates from file and then subset them based on genomic region
