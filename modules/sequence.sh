bcd9e5f117291faeeee7fb3149d833b4e094648c
Alli Gombolay <agombolay3@gmail.com>
2019-10-31 11:18:05 -0400

    Update sequence.sh

--- modules/sequence.sh
+++ modules/sequence.sh
@@ -175,162 +175,174 @@ for file in $output/${sample}-*.coords.tab; do
 		
 		for nuc in "A" "C" "G" "T" "Combined"; do
 			
-		#Extract rNMP nucleotides from FASTA
-		bedtools getfasta -s -fi $(dirname $fasta)/$(basename $fasta .fa)_$region.fa -bed $output/Coords.$region.bed | grep -v '>' > $output/Ribos.txt
+			#Extract rNMP nucleotides from FASTA
+			bedtools getfasta -s -fi $(dirname $fasta)/$(basename $fasta .fa)-$region.fa -bed $file | grep -v '>' > $output/Ribos.txt
 			
-		#Calculate counts of rNMPs
-		A_Ribo=$(awk '$1 == "A" || $1 == "a"' $output/Ribos.txt | wc -l)
-		C_Ribo=$(awk '$1 == "C" || $1 == "c"' $output/Ribos.txt | wc -l)
-		G_Ribo=$(awk '$1 == "G" || $1 == "g"' $output/Ribos.txt | wc -l)
-		U_Ribo=$(awk '$1 == "T" || $1 == "t"' $output/Ribos.txt | wc -l)
+			#Calculate counts of rNMPs
+			A_Ribo=$(awk '$1 == "A" || $1 == "a"' $output/Ribos.txt | wc -l)
+			C_Ribo=$(awk '$1 == "C" || $1 == "c"' $output/Ribos.txt | wc -l)
+			G_Ribo=$(awk '$1 == "G" || $1 == "g"' $output/Ribos.txt | wc -l)
+			U_Ribo=$(awk '$1 == "T" || $1 == "t"' $output/Ribos.txt | wc -l)
 	
-		#Calculate total number of rNMPs
-		RiboTotal=$(($A_Ribo + $C_Ribo + $G_Ribo + $U_Ribo))
+			#Calculate total number of rNMPs
+			RiboTotal=$(($A_Ribo + $C_Ribo + $G_Ribo + $U_Ribo))
 	
-		#Calculate normalized frequency of each rNMP
-		#A_RiboFreq=$(echo "($A_Ribo/$RiboTotal)/$A_BkgFreq" | bc -l)
-		#C_RiboFreq=$(echo "($C_Ribo/$RiboTotal)/$C_BkgFreq" | bc -l)
-		#G_RiboFreq=$(echo "($G_Ribo/$RiboTotal)/$G_BkgFreq" | bc -l)
-		#U_RiboFreq=$(echo "($U_Ribo/$RiboTotal)/$T_BkgFreq" | bc -l)
+			#Calculate normalized frequency of each rNMP
+			#A_RiboFreq=$(echo "($A_Ribo/$RiboTotal)/$A_BkgFreq" | bc -l)
+			#C_RiboFreq=$(echo "($C_Ribo/$RiboTotal)/$C_BkgFreq" | bc -l)
+			#G_RiboFreq=$(echo "($G_Ribo/$RiboTotal)/$G_BkgFreq" | bc -l)
+			#U_RiboFreq=$(echo "($U_Ribo/$RiboTotal)/$T_BkgFreq" | bc -l)
 				
-		#Calculate NOT normalized frequency of each rNMP
-		A_RiboFreq=$(echo "($A_Ribo/$RiboTotal)" | bc -l)
-		C_RiboFreq=$(echo "($C_Ribo/$RiboTotal)" | bc -l)
-		G_RiboFreq=$(echo "($G_Ribo/$RiboTotal)" | bc -l)
-		U_RiboFreq=$(echo "($U_Ribo/$RiboTotal)" | bc -l)
+			#Calculate NOT normalized frequency of each rNMP
+			A_RiboFreq=$(echo "($A_Ribo/$RiboTotal)" | bc -l)
+			C_RiboFreq=$(echo "($C_Ribo/$RiboTotal)" | bc -l)
+			G_RiboFreq=$(echo "($G_Ribo/$RiboTotal)" | bc -l)
+			U_RiboFreq=$(echo "($U_Ribo/$RiboTotal)" | bc -l)
 			
-		#Save normalized frequencies of rNMPs to TXT files
-		if [[ $nuc == "A" ]]; then
-			echo $A_RiboFreq | xargs printf "%.*f\n" 5 > $output/A_Ribo.txt
-			echo 'NA' > $output/C_Ribo.txt
-			echo 'NA' > $output/G_Ribo.txt
-			echo 'NA' > $output/U_Ribo.txt
+			#Save normalized frequencies of rNMPs to TXT files
+			if [[ $nuc == "A" ]]; then
+				echo $A_RiboFreq | xargs printf "%.*f\n" 5 > $output/A_Ribo.txt
+				echo 'NA' > $output/C_Ribo.txt
+				echo 'NA' > $output/G_Ribo.txt
+				echo 'NA' > $output/U_Ribo.txt
 				
-		elif [[ $nuc == "C" ]]; then
-			echo $C_RiboFreq | xargs printf "%.*f\n" 5 > $output/C_Ribo.txt
-			echo 'NA' > $output/A_Ribo.txt
-			echo 'NA' > $output/G_Ribo.txt
-			echo 'NA' > $output/U_Ribo.txt
+			elif [[ $nuc == "C" ]]; then
+				echo $C_RiboFreq | xargs printf "%.*f\n" 5 > $output/C_Ribo.txt
+				echo 'NA' > $output/A_Ribo.txt
+				echo 'NA' > $output/G_Ribo.txt
+				echo 'NA' > $output/U_Ribo.txt
 				
-		elif [[ $nuc == "G" ]]; then
-			echo $G_RiboFreq | xargs printf "%.*f\n" 5 > $output/G_Ribo.txt
-			echo 'NA' > $output/A_Ribo.txt
-			echo 'NA' > $output/C_Ribo.txt
-			echo 'NA' > $output/U_Ribo.txt
+			elif [[ $nuc == "G" ]]; then
+				echo $G_RiboFreq | xargs printf "%.*f\n" 5 > $output/G_Ribo.txt
+				echo 'NA' > $output/A_Ribo.txt
+				echo 'NA' > $output/C_Ribo.txt
+				echo 'NA' > $output/U_Ribo.txt
 				
-		elif [[ $nuc == "T" ]]; then
-			echo $U_RiboFreq | xargs printf "%.*f\n" 5 > $output/U_Ribo.txt
-			echo 'NA' > $output/A_Ribo.txt
-			echo 'NA' > $output/C_Ribo.txt
-			echo 'NA' > $output/G_Ribo.txt
+			elif [[ $nuc == "T" ]]; then
+				echo $U_RiboFreq | xargs printf "%.*f\n" 5 > $output/U_Ribo.txt
+				echo 'NA' > $output/A_Ribo.txt
+				echo 'NA' > $output/C_Ribo.txt
+				echo 'NA' > $output/G_Ribo.txt
 				
-		elif [[ $nuc == "Combined" ]]; then
-			echo $A_RiboFreq | xargs printf "%.*f\n" 5 > $output/A_Ribo.txt
-			echo $C_RiboFreq | xargs printf "%.*f\n" 5 > $output/C_Ribo.txt
-			echo $G_RiboFreq | xargs printf "%.*f\n" 5 > $output/G_Ribo.txt
-			echo $U_RiboFreq | xargs printf "%.*f\n" 5 > $output/U_Ribo.txt
-		fi
+			elif [[ $nuc == "Combined" ]]; then
+				echo $A_RiboFreq | xargs printf "%.*f\n" 5 > $output/A_Ribo.txt
+				echo $C_RiboFreq | xargs printf "%.*f\n" 5 > $output/C_Ribo.txt
+				echo $G_RiboFreq | xargs printf "%.*f\n" 5 > $output/G_Ribo.txt
+				echo $U_RiboFreq | xargs printf "%.*f\n" 5 > $output/U_Ribo.txt
+			fi
 
-		#Combine rNMP frequencies into one file
-		Ribo=$(paste $output/{A,C,G,U}_Ribo.txt)
+			#Combine rNMP frequencies into one file
+			Ribo=$(paste $output/{A,C,G,U}_Ribo.txt)
 
 #############################################################################################################################
-				#STEP 4: Obtain coordinates/sequences of dNMPs +/- 100 bp from rNMPs
-
-				#Create 5 BED files, one for each nucleotide and one combined
-				if [[ $nuc == "A" ]]; then
-					bedtools getfasta -s -fi $(dirname $fasta)/$(basename $fasta .fa)_$region.fa -tab -bed $output/Coords.$region.bed | awk '$2 == "A" || $2 == "a"' | cut -f1 | sed 's/\:/\t/' | sed 's/\-/\t/' | sed 's/(/\t.\t.\t/;s/)//' > $output/Coords.$nuc.$region.bed
-				elif [[ $nuc == "C" ]]; then
-					bedtools getfasta -s -fi $(dirname $fasta)/$(basename $fasta .fa)_$region.fa -tab -bed $output/Coords.$region.bed | awk '$2 == "C" || $2 == "c"' | cut -f1 | sed 's/\:/\t/' | sed 's/\-/\t/' | sed 's/(/\t.\t.\t/;s/)//' > $output/Coords.$nuc.$region.bed
-				elif [[ $nuc == "G" ]]; then
-					bedtools getfasta -s -fi $(dirname $fasta)/$(basename $fasta .fa)_$region.fa -tab -bed $output/Coords.$region.bed | awk '$2 == "G" || $2 == "g"' | cut -f1 | sed 's/\:/\t/' | sed 's/\-/\t/' | sed 's/(/\t.\t.\t/;s/)//' > $output/Coords.$nuc.$region.bed
-				elif [[ $nuc == "T" ]]; then
-					bedtools getfasta -s -fi $(dirname $fasta)/$(basename $fasta .fa)_$region.fa -tab -bed $output/Coords.$region.bed | awk '$2 == "T" || $2 == "t"' | cut -f1 | sed 's/\:/\t/' | sed 's/\-/\t/' | sed 's/(/\t.\t.\t/;s/)//' > $output/Coords.$nuc.$region.bed
-				elif [[ $nuc == "Combined" ]]; then
-					cp $output/Coords.$region.bed $output/Coords.$nuc.$region.bed 
-				fi
-		
-				#Continue only for BED files > 0
-				if [[ -s $output/Coords.$nuc.$region.bed ]]; then
 			
-					#Obtain coordinates of flanking sequences and remove coordinates where start = end
-					bedtools flank -i $output/Coords.$nuc.$region.bed -s -g $(dirname $fasta)/$(basename $fasta .fa).bed -l 100 -r 0 | awk '$2 != $3' > $output/Up.bed
-					bedtools flank -i $output/Coords.$nuc.$region.bed -s -g $(dirname $fasta)/$(basename $fasta .fa).bed -l 0 -r 100 | awk '$2 != $3' > $output/Down.bed
+			#STEP 4: Obtain coordinates/sequences of dNMPs +/- 100 bp from rNMPs
+
+			#Create 5 BED files, one for each nucleotide and one combined
+			if [[ $nuc == "A" ]]; then
+				bedtools getfasta -s -fi $(dirname $fasta)/$(basename $fasta .fa)-$region.fa -tab -bed $file | awk '$2 == "A" || $2 == "a"' | cut -f1 | sed 's/\:/\t/' | sed 's/\-/\t/' | sed 's/(/\t.\t.\t/;s/)//' > $output/Coords.$nuc.$region.bed
+			
+			elif [[ $nuc == "C" ]]; then
+				bedtools getfasta -s -fi $(dirname $fasta)/$(basename $fasta .fa)-$region.fa -tab -bed $file | awk '$2 == "C" || $2 == "c"' | cut -f1 | sed 's/\:/\t/' | sed 's/\-/\t/' | sed 's/(/\t.\t.\t/;s/)//' > $output/Coords.$nuc.$region.bed
+			
+			elif [[ $nuc == "G" ]]; then
+				bedtools getfasta -s -fi $(dirname $fasta)/$(basename $fasta .fa)-$region.fa -tab -bed $file | awk '$2 == "G" || $2 == "g"' | cut -f1 | sed 's/\:/\t/' | sed 's/\-/\t/' | sed 's/(/\t.\t.\t/;s/)//' > $output/Coords.$nuc.$region.bed
+			
+			elif [[ $nuc == "T" ]]; then
+				bedtools getfasta -s -fi $(dirname $fasta)/$(basename $fasta .fa)-$region.fa -tab -bed $file | awk '$2 == "T" || $2 == "t"' | cut -f1 | sed 's/\:/\t/' | sed 's/\-/\t/' | sed 's/(/\t.\t.\t/;s/)//' > $output/Coords.$nuc.$region.bed
+			
+			elif [[ $nuc == "Combined" ]]; then
+				cp $file $output/Coords.$nuc.$region.bed 
+			fi
+		
+			#Continue only for BED files > 0
+			if [[ -s $output/Coords.$nuc.$region.bed ]]; then
+			
+				#Obtain coordinates of flanking sequences and remove coordinates where start = end
+				bedtools flank -i $output/Coords.$nuc.$region.bed -s -g $(dirname $fasta)/$(basename $fasta .fa).bed -l 100 -r 0 | awk '$2 != $3' > $output/Up.bed
+				bedtools flank -i $output/Coords.$nuc.$region.bed -s -g $(dirname $fasta)/$(basename $fasta .fa).bed -l 0 -r 100 | awk '$2 != $3' > $output/Down.bed
 	
-					#Obtain nucleotides flanking rNMPs (reverse order of up) and insert tabs bases for easier parsing
-					bedtools getfasta -s -fi $(dirname $fasta)/$(basename $fasta .fa)_$region.fa -bed $output/Down.bed | grep -v '>' | sed 's/.../& /2g;s/./& /g' > $output/Down.tab
-					bedtools getfasta -s -fi $(dirname $fasta)/$(basename $fasta .fa)_$region.fa -bed $output/Up.bed | grep -v '>' | rev | sed 's/.../& /2g;s/./& /g' > $output/Up.tab
+				#Obtain nucleotides flanking rNMPs (reverse order of up) and insert tabs bases for easier parsing
+				bedtools getfasta -s -fi $(dirname $fasta)/$(basename $fasta .fa)_$region.fa -bed $output/Down.bed | grep -v '>' | sed 's/.../& /2g;s/./& /g' > $output/Down.tab
+				bedtools getfasta -s -fi $(dirname $fasta)/$(basename $fasta .fa)_$region.fa -bed $output/Up.bed | grep -v '>' | rev | sed 's/.../& /2g;s/./& /g' > $output/Up.tab
 				
 #############################################################################################################################
-					#STEP 6: Calculate frequencies of dNMPs +/- 100 base pairs from rNMPs
+				#STEP 6: Calculate frequencies of dNMPs +/- 100 base pairs from rNMPs
 
-					for dir in "Up" "Down"; do
+				for dir in "Up" "Down"; do
 		
-						for i in {1..100}; do
+					for i in {1..100}; do
 		
-							#Calculate count of each dNMP
-							A_Flank=$(awk -v field=$i '{ print $field }' $output/$dir.tab | grep -Eo 'A|a' | wc -l)
-							C_Flank=$(awk -v field=$i '{ print $field }' $output/$dir.tab | grep -Eo 'C|c' | wc -l)
-							G_Flank=$(awk -v field=$i '{ print $field }' $output/$dir.tab | grep -Eo 'G|g' | wc -l)
-							T_Flank=$(awk -v field=$i '{ print $field }' $output/$dir.tab | grep -Eo 'T|t' | wc -l)
+						#Calculate count of each dNMP
+						A_Flank=$(awk -v field=$i '{ print $field }' $output/$dir.tab | grep -Eo 'A|a' | wc -l)
+						C_Flank=$(awk -v field=$i '{ print $field }' $output/$dir.tab | grep -Eo 'C|c' | wc -l)
+						G_Flank=$(awk -v field=$i '{ print $field }' $output/$dir.tab | grep -Eo 'G|g' | wc -l)
+						T_Flank=$(awk -v field=$i '{ print $field }' $output/$dir.tab | grep -Eo 'T|t' | wc -l)
 
-							#Calculate total number of dNMPs
-							FlankTotal=$(($A_Flank + $C_Flank + $G_Flank + $T_Flank))
+						#Calculate total number of dNMPs
+						FlankTotal=$(($A_Flank + $C_Flank + $G_Flank + $T_Flank))
 
-							#Calculate normalized frequencies of dNMPs
-							if [[ $FlankTotal != 0 ]]; then
-								#A_FlankFreq=$(echo "($A_Flank/$FlankTotal)/$A_BkgFreq" | bc -l)
-								#C_FlankFreq=$(echo "($C_Flank/$FlankTotal)/$C_BkgFreq" | bc -l)
-								#G_FlankFreq=$(echo "($G_Flank/$FlankTotal)/$G_BkgFreq" | bc -l)
-								#T_FlankFreq=$(echo "($T_Flank/$FlankTotal)/$T_BkgFreq" | bc -l)
+						#Calculate normalized frequencies of dNMPs
+						if [[ $FlankTotal != 0 ]]; then
+						
+							#A_FlankFreq=$(echo "($A_Flank/$FlankTotal)/$A_BkgFreq" | bc -l)
+							#C_FlankFreq=$(echo "($C_Flank/$FlankTotal)/$C_BkgFreq" | bc -l)
+							#G_FlankFreq=$(echo "($G_Flank/$FlankTotal)/$G_BkgFreq" | bc -l)
+							#T_FlankFreq=$(echo "($T_Flank/$FlankTotal)/$T_BkgFreq" | bc -l)
 								
-								A_FlankFreq=$(echo "($A_Flank/$FlankTotal)" | bc -l)
-								C_FlankFreq=$(echo "($C_Flank/$FlankTotal)" | bc -l)
-								G_FlankFreq=$(echo "($G_Flank/$FlankTotal)" | bc -l)
-								T_FlankFreq=$(echo "($T_Flank/$FlankTotal)" | bc -l)
+							A_FlankFreq=$(echo "($A_Flank/$FlankTotal)" | bc -l)
+							C_FlankFreq=$(echo "($C_Flank/$FlankTotal)" | bc -l)
+							G_FlankFreq=$(echo "($G_Flank/$FlankTotal)" | bc -l)
+							T_FlankFreq=$(echo "($T_Flank/$FlankTotal)" | bc -l)
 				
-							elif [[ $FlankTotal == 0 ]]; then
-								A_FlankFreq='NA'
-								C_FlankFreq='NA'
-								G_FlankFreq='NA'
-								T_FlankFreq='NA'
-							fi
+						elif [[ $FlankTotal == 0 ]]; then
+							
+							A_FlankFreq='NA'
+							C_FlankFreq='NA'
+							G_FlankFreq='NA'
+							T_FlankFreq='NA'
+						fi
 				
-							#Save normalized dNMPs frequencies to TXT files
-							if [[ $A_FlankFreq != 'NA' ]]; then
-								echo $A_FlankFreq | xargs printf "%.*f\n" 5 >> $output/A_$dir.txt
-							elif [[ $A_FlankFreq == 'NA' ]]; then
-								echo $A_FlankFreq >> $output/A_$dir.txt
-							fi
+						#Save normalized dNMPs frequencies to TXT files
+						if [[ $A_FlankFreq != 'NA' ]]; then
+							echo $A_FlankFreq | xargs printf "%.*f\n" 5 >> $output/A_$dir.txt
+						
+						elif [[ $A_FlankFreq == 'NA' ]]; then
+							echo $A_FlankFreq >> $output/A_$dir.txt
+						fi
 				
-							if [[ $C_FlankFreq != 'NA' ]]; then
-								echo $C_FlankFreq | xargs printf "%.*f\n" 5 >> $output/C_$dir.txt
-							elif [[ $C_FlankFreq == 'NA' ]]; then
-								echo $C_FlankFreq >> $output/C_$dir.txt
-							fi
+						if [[ $C_FlankFreq != 'NA' ]]; then
+							echo $C_FlankFreq | xargs printf "%.*f\n" 5 >> $output/C_$dir.txt
+						
+						elif [[ $C_FlankFreq == 'NA' ]]; then
+							echo $C_FlankFreq >> $output/C_$dir.txt
+						fi
 				
-							if [[ $G_FlankFreq != 'NA' ]]; then
-								echo $G_FlankFreq | xargs printf "%.*f\n" 5 >> $output/G_$dir.txt
-							elif [[ $G_FlankFreq == 'NA' ]]; then
-								echo $G_FlankFreq >> $output/G_$dir.txt
-							fi
+						if [[ $G_FlankFreq != 'NA' ]]; then
+							echo $G_FlankFreq | xargs printf "%.*f\n" 5 >> $output/G_$dir.txt
+						
+						elif [[ $G_FlankFreq == 'NA' ]]; then
+							echo $G_FlankFreq >> $output/G_$dir.txt
+						fi
 				
-							if [[ $T_FlankFreq != 'NA' ]]; then
-								echo $T_FlankFreq | xargs printf "%.*f\n" 5 >> $output/T_$dir.txt
-							elif [[ $T_FlankFreq == 'NA' ]]; then
-								echo $T_FlankFreq >> $output/T_$dir.txt
-							fi
+						if [[ $T_FlankFreq != 'NA' ]]; then
+							echo $T_FlankFreq | xargs printf "%.*f\n" 5 >> $output/T_$dir.txt
+						
+						elif [[ $T_FlankFreq == 'NA' ]]; then
+							echo $T_FlankFreq >> $output/T_$dir.txt
+						fi
 		
-							#Combine dNMP frequencies into one file per location
-							if [[ $dir == "Up" ]]; then
-								#Print upstream frequencies in reverse order
-								Up=$(paste $output/{A,C,G,T}_Up.txt | tac -)
-							elif [[ $dir == "Down" ]]; then
-								Down=$(paste $output/{A,C,G,T}_Down.txt)
-							fi
+						#Combine dNMP frequencies into one file per location
+						if [[ $dir == "Up" ]]; then
+							#Print upstream frequencies in reverse order
+							Up=$(paste $output/{A,C,G,T}_Up.txt | tac -)
+						
+						elif [[ $dir == "Down" ]]; then
+							Down=$(paste $output/{A,C,G,T}_Down.txt)
+						fi
 
-						done
 					done
+				done
 
 #############################################################################################################################
 					#STEP 7: Create and save dataset file containing nucleotide frequencies
