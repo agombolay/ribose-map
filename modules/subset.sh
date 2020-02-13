38496d843b47b47b60ab464925d2e7f035326509
Alli Gombolay <agombolay3@gmail.com>
2020-02-13 14:57:23 -0500

    Update subset.sh

--- modules/subset.sh
+++ modules/subset.sh
@@ -14,13 +14,13 @@ if [[ $other ]]; then
 	grep -Ewv $other_new $output/$sample.bed > $output/${sample}-chromosomes.coords.bed
 	cut -f1,2,3,6 $output/${sample}-chromosomes.coords.bed | uniq -c - | awk -v "OFS=\t" '{print $2, $3, $4, $5, $1}' - | sort -k5,5n - > $output/${sample}-chromosomes.counts.tab
 
-	#Create FASTA file for reference
+	#Create FASTA and FAI files for Chromosomes
 	chr=$(awk '{print $1}' $(dirname $fasta)/$(basename $fasta .fa).bed | grep -Ewv $other_new -)
 
 	samtools faidx $fasta $chr > $(dirname $fasta)/$(basename $fasta .fa)-chromosomes.fa
 	samtools faidx $(dirname $fasta)/$(basename $fasta .fa)-chromosomes.fa
 	
-	#Create BED file for reference
+	#Create BED file for Chromosomes
 	cut -f 1,2 $fasta.fai > $(dirname $fasta)/$(basename $fasta .fa).bed
 
 	#Other
@@ -29,13 +29,13 @@ if [[ $other ]]; then
 		grep -w $region $output/$sample.bed > $output/${sample}-$region.coords.bed
 		cut -f1,2,3,6 $output/${sample}-$region.coords.bed | uniq -c - | awk -v "OFS=\t" '{print $2, $3, $4, $5, $1}' - | sort -k5,5n - > $output/${sample}-$region.counts.tab
 		
-		#Create FASTA file for reference
+		#Create FASTA and FAI files for Other
 		chr=$(awk '{print $1}' $(dirname $fasta)/$(basename $fasta .fa).bed | grep -w $region -)
 
 		samtools faidx $fasta $chr > $(dirname $fasta)/$(basename $fasta .fa)-$region.fa
 		samtools faidx $(dirname $fasta)/$(basename $fasta .fa)-$region.fa
 		
-		#Create BED file for reference
+		#Create BED file for Other
 		cut -f 1,2 $fasta.fai > $(dirname $fasta)/$(basename $fasta .fa).bed
 
 	done
