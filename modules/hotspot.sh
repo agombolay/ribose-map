194da39bd3959fb355d4f783fc75f7564683449f
Alli Gombolay <agombolay3@gmail.com>
2020-02-13 11:51:53 -0500

    Update hotspot.sh

--- modules/hotspot.sh
+++ modules/hotspot.sh
@@ -7,7 +7,7 @@ rm -r $output; mkdir -p $output
 ######################################################################################################################################################
 
 #Calculate index
-index=$(echo "$(wc -l < $output/${sample}-$region.counts.tab)*0.99" | bc)
+index=$(echo "$(wc -l < $repository/results/$sample/coordinate$quality/${sample}-$region.counts.tab)*0.99" | bc)
 
 #Test if index is integer or floating
 integer=$(echo $index | grep -E '[0-9]+\.[0]{2}' -)
