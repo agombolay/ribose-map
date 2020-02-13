c626d8adcf793fd33f448a5c479165e920e2b788
Alli Gombolay <agombolay3@gmail.com>
2020-02-13 15:00:13 -0500

    Update hotspot.sh

--- modules/hotspot.sh
+++ modules/hotspot.sh
@@ -52,7 +52,7 @@ for region in $other "chromosomes"; do
 	for file in $(ls $output/${sample}-$region.top.bed); do
 
 		#Get sequence of rNMPs and the 3 nucleotides upstream and downstream from them
-		bedtools slop -s -i $file -g $output/$(basename $fasta .fa).bed -b 3 > $output/$(basename $file .bed).slop.bed
+		bedtools slop -s -i $file -g $(dirname $fasta)/$(basename $fasta .fa).bed -b 3 > $output/$(basename $file .bed).slop.bed
 	
 		bedtools getfasta -s -fi $fasta -bed $output/$(basename $file .bed).slop.bed | awk '/>/{$0 = ">" ++i substr($0, 2)} 1' - > $output/$(basename $file .bed).flank.txt
 
