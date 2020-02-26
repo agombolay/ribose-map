c75736362e7a283eb0df8226d16511f7c0fe2773
Alli Gombolay <agombolay3@gmail.com>
2020-02-26 13:35:58 -0500

    Update sequence.sh

--- modules/sequence.sh
+++ modules/sequence.sh
@@ -116,39 +116,6 @@ fi
 
 #############################################################################################################################
 
-#Calculate frequencies of reference genome
-for file in $(dirname $fasta)/$(basename $fasta .fa)-*.fa; do
-			
-	temp=$(echo $file | awk -F '[-]' '{print $2 $3 $4}')
-	region=$(basename $temp .fa)
-			
-	#Nucleotide Frequencies of Reference Genome
-			
-	#Calculate counts of each nucleotide
-	A_Bkg=$(grep -v '>' $file | grep -Eo 'A|a' - | wc -l)
-	C_Bkg=$(grep -v '>' $file | grep -Eo 'C|c' - | wc -l)
-	G_Bkg=$(grep -v '>' $file | grep -Eo 'G|g' - | wc -l)
-	T_Bkg=$(grep -v '>' $file | grep -Eo 'T|t' - | wc -l)
-	
-	#Calculate total number of nucleotides
-	BkgTotal=$(($A_Bkg + $C_Bkg + $G_Bkg + $T_Bkg))
-		
-	#Calculate frequencies of each nucleotide
-	A_BkgFreq=$(echo "($A_Bkg + $T_Bkg)/($BkgTotal*2)" | bc -l)
-	C_BkgFreq=$(echo "($C_Bkg + $G_Bkg)/($BkgTotal*2)" | bc -l)
-	G_BkgFreq=$(echo "($G_Bkg + $C_Bkg)/($BkgTotal*2)" | bc -l)
-	T_BkgFreq=$(echo "($T_Bkg + $A_Bkg)/($BkgTotal*2)" | bc -l)
-			
-	#Save nucleotide frequencies to .txt file
-	paste <(echo -e "A") <(echo "$A_BkgFreq" | xargs printf "%.*f\n" 5) > $(dirname $fasta)/$(basename $fasta .fa)-$region.txt
-	paste <(echo -e "C") <(echo "$C_BkgFreq" | xargs printf "%.*f\n" 5) >> $(dirname $fasta)/$(basename $fasta .fa)-$region.txt
-	paste <(echo -e "G") <(echo "$G_BkgFreq" | xargs printf "%.*f\n" 5) >> $(dirname $fasta)/$(basename $fasta .fa)-$region.txt
-	paste <(echo -e "U") <(echo "$T_BkgFreq" | xargs printf "%.*f\n" 5) >> $(dirname $fasta)/$(basename $fasta .fa)-$region.txt
-
-done
-
-#############################################################################################################################
-
 #Calculate frequencies of rNMPs
 for file in $output/${sample}-*.bed; do
 
