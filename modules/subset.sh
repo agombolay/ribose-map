b6ac5dae6e3b8a319738fa8a161ecfbf1b0c4a7b
Alli Gombolay <agombolay3@gmail.com>
2020-04-27 13:43:49 -0400

    Update subset.sh

--- modules/subset.sh
+++ modules/subset.sh
@@ -19,7 +19,7 @@ if [[ $other ]]; then
 	cut -f1,2,3,6 $repository/results/$sample/coordinate$quality/${sample}-chromosomes.coords.bed | uniq -c - | awk -v "OFS=\t" '{print $2, $3, $4, ".", ".", $5, $1}' - | sort -k7,7n - > $repository/results/$sample/coordinate$quality/${sample}-chromosomes.counts.tab
 
 	#Create FASTA and FAI files for Chromosomes
-	chr=$(awk '{print $1}' $(dirname $fasta)/$(basename $fasta .fa).bed | grep -Ewv $other_new -)
+	chr=$(awk '{print $1}' $(dirname $fasta)/$(basename $fasta .fa).chrom.sizes | grep -Ewv $other_new -)
 
 	samtools faidx $fasta $chr > $(dirname $fasta)/$(basename $fasta .fa)-chromosomes.fa
 	samtools faidx $(dirname $fasta)/$(basename $fasta .fa)-chromosomes.fa
@@ -31,7 +31,7 @@ if [[ $other ]]; then
 		cut -f1,2,3,6 $repository/results/$sample/coordinate$quality/${sample}-$region.coords.bed | uniq -c - | awk -v "OFS=\t" '{print $2, $3, $4, ".", ".", $5, $1}' - | sort -k7,7n - > $repository/results/$sample/coordinate$quality/${sample}-$region.counts.tab
 		
 		#Create FASTA and FAI files for Other
-		chr=$(awk '{print $1}' $(dirname $fasta)/$(basename $fasta .fa).bed | grep -w $region -)
+		chr=$(awk '{print $1}' $(dirname $fasta)/$(basename $fasta .fa).chrom.sizes | grep -w $region -)
 
 		samtools faidx $fasta $chr > $(dirname $fasta)/$(basename $fasta .fa)-$region.fa
 		samtools faidx $(dirname $fasta)/$(basename $fasta .fa)-$region.fa
