b7958d69dc4a91954ff17eaf697edf7af6192795
Alli Gombolay <agombolay3@gmail.com>
2020-05-11 19:17:44 -0400

    Update distribution.sh

--- modules/distribution.sh
+++ modules/distribution.sh
@@ -21,7 +21,7 @@ rm -r $output; mkdir -p $output
 for region in $other "chromosomes"; do
 
 	#Calculate normalized counts of rNMPs
-	mawk -v "OFS=\t" -v total="$(wc -l < $repository/results/$sample/coordinate$quality/$sample-$region.coords.bed)" '{print $1, $2, $3, $4, $5/total*100}' \
+	mawk -v "OFS=\t" -v total="$(wc -l < $repository/results/$sample/coordinate$quality/$sample-$region.coords.bed)" '{print $1, $2, $3, $6, $7/total*100}' \
 	$repository/results/$sample/coordinate$quality/$sample-$region.counts.tab > $repository/results/$sample/coordinate$quality/$sample-$region.normalized.tab
 
 	#Save coverage of rNMPs per chromosome to separate files
