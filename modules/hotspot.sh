7a61f8b74b6c668c7232111436a93fabfe08817b
Alli Gombolay <agombolay3@gmail.com>
2020-07-07 14:35:48 -0400

    Update hotspot.sh

--- modules/hotspot.sh
+++ modules/hotspot.sh
@@ -10,10 +10,9 @@ rm -r $output; mkdir -p $output
 #############################################################################################################################################################################################################################################
 
 for region in $other "chromosomes"; do
-
+	
 	#Calculate index
-	#index=$(echo "$(wc -l < $repository/results/$sample/coordinate$quality/${sample}-$region.counts.tab)*0.99" | bc)
-	index=$(echo "$(wc -l < $repository/results/$sample/coordinate$quality/${sample}-$region.counts.tab)*$percentile" | bc)
+	index=$(echo "$(wc -l < $repository/results/$sample/coordinate$quality/${sample}-$region.$type.tab)*$percentile" | bc)
 
 	#Test if index is integer or floating
 	integer=$(echo $index | grep -E '[0-9]+\.[0]{2}' -)
