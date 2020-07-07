2c49861b150684b4f795f8c8caef224592614f61
Alli Gombolay <agombolay3@gmail.com>
2020-07-07 18:20:52 -0400

    Update subset.sh

--- modules/subset.sh
+++ modules/subset.sh
@@ -29,22 +29,22 @@ if [[ $other ]]; then
 		#Save frequencies of rNMPs to TXT files
 		if [[ $nuc == "A" ]]; then
 			#Create BED file for only A ribonucleotide
-			bedtools getfasta -s -fi $(dirname $fasta)/$(basename $fasta .fa)-$region.fa -tab -bed $repository/results/$sample/coordinate$quality/${sample}-chromosomes.bed | awk '$2 == "A" || $2 == "a"' | cut -f1 | sed 's/\:/\t/' | sed 's/\-/\t/' | sed 's/(/\t.\t.\t/;s/)//' > $repository/results/$sample/coordinate$quality/${sample}-Ribo.chromosomes.$nuc.bed
+			bedtools getfasta -s -fi $(dirname $fasta)/$(basename $fasta .fa)-chromosomes.fa -tab -bed $repository/results/$sample/coordinate$quality/${sample}-chromosomes.bed | awk '$2 == "A" || $2 == "a"' | cut -f1 | sed 's/\:/\t/' | sed 's/\-/\t/' | sed 's/(/\t.\t.\t/;s/)//' > $repository/results/$sample/coordinate$quality/${sample}-Ribo.chromosomes.$nuc.bed
 			cut -f1,2,3,6 $repository/results/$sample/coordinate$quality/${sample}-Ribo.chromosomes.$nuc.bed | uniq -c - | awk -v "OFS=\t" '{print $2, $3, $4, ".", ".", $5, $1}' - | sort -k7,7n - > $repository/results/$sample/coordinate$quality/${sample}-Ribo.chromosomes.$nuc.tab
 			
 		elif [[ $nuc == "C" ]]; then
 			#Create BED file for only C ribonucleotide
-			bedtools getfasta -s -fi $(dirname $fasta)/$(basename $fasta .fa)-$region.fa -tab -bed $repository/results/$sample/coordinate$quality/${sample}-chromosomes.bed | awk '$2 == "C" || $2 == "c"' | cut -f1 | sed 's/\:/\t/' | sed 's/\-/\t/' | sed 's/(/\t.\t.\t/;s/)//' > $repository/results/$sample/coordinate$quality/${sample}-Ribo.chromosomes.$nuc.bed
+			bedtools getfasta -s -fi $(dirname $fasta)/$(basename $fasta .fa)-chromosomes.fa -tab -bed $repository/results/$sample/coordinate$quality/${sample}-chromosomes.bed | awk '$2 == "C" || $2 == "c"' | cut -f1 | sed 's/\:/\t/' | sed 's/\-/\t/' | sed 's/(/\t.\t.\t/;s/)//' > $repository/results/$sample/coordinate$quality/${sample}-Ribo.chromosomes.$nuc.bed
 			cut -f1,2,3,6 $repository/results/$sample/coordinate$quality/${sample}-Ribo.chromosomes.$nuc.bed | uniq -c - | awk -v "OFS=\t" '{print $2, $3, $4, ".", ".", $5, $1}' - | sort -k7,7n - > $repository/results/$sample/coordinate$quality/${sample}-Ribo.chromosomes.$nuc.tab
 			
 		elif [[ $nuc == "G" ]]; then	
 			#Create BED file for only G ribonucleotide
-			bedtools getfasta -s -fi $(dirname $fasta)/$(basename $fasta .fa)-$region.fa -tab -bed $repository/results/$sample/coordinate$quality/${sample}-chromosomes.bed | awk '$2 == "G" || $2 == "g"' | cut -f1 | sed 's/\:/\t/' | sed 's/\-/\t/' | sed 's/(/\t.\t.\t/;s/)//' > $repository/results/$sample/coordinate$quality/${sample}-Ribo.chromosomes.$nuc.bed
+			bedtools getfasta -s -fi $(dirname $fasta)/$(basename $fasta .fa)-chromosomes.fa -tab -bed $repository/results/$sample/coordinate$quality/${sample}-chromosomes.bed | awk '$2 == "G" || $2 == "g"' | cut -f1 | sed 's/\:/\t/' | sed 's/\-/\t/' | sed 's/(/\t.\t.\t/;s/)//' > $repository/results/$sample/coordinate$quality/${sample}-Ribo.chromosomes.$nuc.bed
 			cut -f1,2,3,6 $repository/results/$sample/coordinate$quality/${sample}-Ribo.chromosomes.$nuc.bed | uniq -c - | awk -v "OFS=\t" '{print $2, $3, $4, ".", ".", $5, $1}' - | sort -k7,7n - > $repository/results/$sample/coordinate$quality/${sample}-Ribo.chromosomes.$nuc.tab
 			
 		elif [[ $nuc == "U" ]]; then	
 			#Create BED file for only U ribonucleotide
-			bedtools getfasta -s -fi $(dirname $fasta)/$(basename $fasta .fa)-$region.fa -tab -bed $repository/results/$sample/coordinate$quality/${sample}-chromosomes.bed | awk '$2 == "T" || $2 == "t"' | cut -f1 | sed 's/\:/\t/' | sed 's/\-/\t/' | sed 's/(/\t.\t.\t/;s/)//' > $repository/results/$sample/coordinate$quality/${sample}-Ribo.chromosomes.$nuc.bed
+			bedtools getfasta -s -fi $(dirname $fasta)/$(basename $fasta .fa)-chromosomes.fa -tab -bed $repository/results/$sample/coordinate$quality/${sample}-chromosomes.bed | awk '$2 == "T" || $2 == "t"' | cut -f1 | sed 's/\:/\t/' | sed 's/\-/\t/' | sed 's/(/\t.\t.\t/;s/)//' > $repository/results/$sample/coordinate$quality/${sample}-Ribo.chromosomes.$nuc.bed
 			cut -f1,2,3,6 $repository/results/$sample/coordinate$quality/${sample}-Ribo.chromosomes.$nuc.bed | uniq -c - | awk -v "OFS=\t" '{print $2, $3, $4, ".", ".", $5, $1}' - | sort -k7,7n - > $repository/results/$sample/coordinate$quality/${sample}-Ribo.chromosomes.$nuc.tab
 		fi
 	done
@@ -97,22 +97,22 @@ else
 		#Save frequencies of rNMPs to TXT files
 		if [[ $nuc == "A" ]]; then
 			#Create BED file for only A ribonucleotide
-			bedtools getfasta -s -fi $(dirname $fasta)/$(basename $fasta .fa)-$region.fa -tab -bed $repository/results/$sample/coordinate$quality/${sample}-chromosomes.bed | awk '$2 == "A" || $2 == "a"' | cut -f1 | sed 's/\:/\t/' | sed 's/\-/\t/' | sed 's/(/\t.\t.\t/;s/)//' > $repository/results/$sample/coordinate$quality/${sample}-Ribo.chromosomes.$nuc.bed
+			bedtools getfasta -s -fi $(dirname $fasta)/$(basename $fasta .fa)-chromosomes.fa -tab -bed $repository/results/$sample/coordinate$quality/${sample}-chromosomes.bed | awk '$2 == "A" || $2 == "a"' | cut -f1 | sed 's/\:/\t/' | sed 's/\-/\t/' | sed 's/(/\t.\t.\t/;s/)//' > $repository/results/$sample/coordinate$quality/${sample}-Ribo.chromosomes.$nuc.bed
 			cut -f1,2,3,6 $repository/results/$sample/coordinate$quality/${sample}-Ribo.chromosomes.$nuc.bed | uniq -c - | awk -v "OFS=\t" '{print $2, $3, $4, ".", ".", $5, $1}' - | sort -k7,7n - > $repository/results/$sample/coordinate$quality/${sample}-Ribo.chromosomes.$nuc.tab
 
 		elif [[ $nuc == "C" ]]; then
 			#Create BED file for only C ribonucleotide
-			bedtools getfasta -s -fi $(dirname $fasta)/$(basename $fasta .fa)-$region.fa -tab -bed $repository/results/$sample/coordinate$quality/${sample}-chromosomes.bed | awk '$2 == "C" || $2 == "c"' | cut -f1 | sed 's/\:/\t/' | sed 's/\-/\t/' | sed 's/(/\t.\t.\t/;s/)//' > $repository/results/$sample/coordinate$quality/${sample}-Ribo.chromosomes.$nuc.bed
+			bedtools getfasta -s -fi $(dirname $fasta)/$(basename $fasta .fa)-chromosomes.fa -tab -bed $repository/results/$sample/coordinate$quality/${sample}-chromosomes.bed | awk '$2 == "C" || $2 == "c"' | cut -f1 | sed 's/\:/\t/' | sed 's/\-/\t/' | sed 's/(/\t.\t.\t/;s/)//' > $repository/results/$sample/coordinate$quality/${sample}-Ribo.chromosomes.$nuc.bed
 			cut -f1,2,3,6 $repository/results/$sample/coordinate$quality/${sample}-Ribo.chromosomes.$nuc.bed | uniq -c - | awk -v "OFS=\t" '{print $2, $3, $4, ".", ".", $5, $1}' - | sort -k7,7n - > $repository/results/$sample/coordinate$quality/${sample}-Ribo.chromosomes.$nuc.tab
 
 		elif [[ $nuc == "G" ]]; then	
 			#Create BED file for only G ribonucleotide
-			bedtools getfasta -s -fi $(dirname $fasta)/$(basename $fasta .fa)-$region.fa -tab -bed $repository/results/$sample/coordinate$quality/${sample}-chromosomes.bed | awk '$2 == "G" || $2 == "g"' | cut -f1 | sed 's/\:/\t/' | sed 's/\-/\t/' | sed 's/(/\t.\t.\t/;s/)//' > $repository/results/$sample/coordinate$quality/${sample}-Ribo.chromosomes.$nuc.bed
+			bedtools getfasta -s -fi $(dirname $fasta)/$(basename $fasta .fa)-chromosomes.fa -tab -bed $repository/results/$sample/coordinate$quality/${sample}-chromosomes.bed | awk '$2 == "G" || $2 == "g"' | cut -f1 | sed 's/\:/\t/' | sed 's/\-/\t/' | sed 's/(/\t.\t.\t/;s/)//' > $repository/results/$sample/coordinate$quality/${sample}-Ribo.chromosomes.$nuc.bed
 			cut -f1,2,3,6 $repository/results/$sample/coordinate$quality/${sample}-Ribo.chromosomes.$nuc.bed | uniq -c - | awk -v "OFS=\t" '{print $2, $3, $4, ".", ".", $5, $1}' - | sort -k7,7n - > $repository/results/$sample/coordinate$quality/${sample}-Ribo.chromosomes.$nuc.tab
 
 		elif [[ $nuc == "U" ]]; then	
 			#Create BED file for only U ribonucleotide
-			bedtools getfasta -s -fi $(dirname $fasta)/$(basename $fasta .fa)-$region.fa -tab -bed $repository/results/$sample/coordinate$quality/${sample}-chromosomes.bed | awk '$2 == "T" || $2 == "t"' | cut -f1 | sed 's/\:/\t/' | sed 's/\-/\t/' | sed 's/(/\t.\t.\t/;s/)//' > $repository/results/$sample/coordinate$quality/${sample}-Ribo.chromosomes.$nuc.bed
+			bedtools getfasta -s -fi $(dirname $fasta)/$(basename $fasta .fa)-chromosomes.fa -tab -bed $repository/results/$sample/coordinate$quality/${sample}-chromosomes.bed | awk '$2 == "T" || $2 == "t"' | cut -f1 | sed 's/\:/\t/' | sed 's/\-/\t/' | sed 's/(/\t.\t.\t/;s/)//' > $repository/results/$sample/coordinate$quality/${sample}-Ribo.chromosomes.$nuc.bed
 			cut -f1,2,3,6 $repository/results/$sample/coordinate$quality/${sample}-Ribo.chromosomes.$nuc.bed | uniq -c - | awk -v "OFS=\t" '{print $2, $3, $4, ".", ".", $5, $1}' - | sort -k7,7n - > $repository/results/$sample/coordinate$quality/${sample}-Ribo.chromosomes.$nuc.tab
 		fi
 	done
