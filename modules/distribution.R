a5de02b1145ef8da440ae8a3bd19823169075b99
Alli Gombolay <agombolay3@gmail.com>
2020-09-28 13:19:03 -0400

    Update distribution.R

--- modules/distribution.R
+++ modules/distribution.R
@@ -20,16 +20,6 @@ output <- file.path(repository, "results", sample, paste("distribution", quality
 input_files <- list.files(path = output, pattern = ".tab", full.names = TRUE, recursive = FALSE)
 
 ####################################################################################################################################################################
-#Find maximum y-axis value
-#maximum <- c()
-
-#for(file in input_files){
-#	data = read.table(file, sep = "\t", header = FALSE)
-#	maximum <- c(maximum, max(data$V5))
-#}
-#ylimit <- max(maximum)
-
-####################################################################################################################################################################
 
 for(file in input_files){
 	
@@ -67,31 +57,7 @@ for(file in input_files){
 
 		ggsave(filename = file.path(output, paste(file_path_sans_ext(basename(file)), ".png", sep = "")), plot = scaled, width = 15)
 		
-####################################################################################################################################################################
-	} else if (scale == 'no'){
-	
-		unscaled <- ggplot(data, aes(V3, V5, colour = V4_new)) + xlab("Chromosome Coordinate") + ylab("Per Nucleotide rNMP Coverage (%)") +
-
-				   #Plot forward and reverse strands on same plot
-				   facet_wrap(~V4_new, ncol = 1, labeller = labeller(V4_new = labels)) + scale_colour_manual(values = c("#0072B2", "#009E73")) +
-
-				   #Add lines to represent axes (must add for facet wrap)
-				   annotate("segment", x = -Inf, xend = Inf, y = -Inf, yend = -Inf) + annotate("segment", x = -Inf, xend = -Inf, y = -Inf, yend = Inf) +
-
-				   #Decrease space between barcharts and x-axis and y-axis
-				   geom_bar(stat = "identity") + scale_x_continuous(expand = c(0.01, 0)) + scale_y_continuous(expand = c(0.01, 0)) +
-		
-				   theme(
-				   	 axis.title = element_text(color = "black", size = 25), axis.text = element_text(color = "black", size = 25), axis.line = element_line(size = 1),
-					 axis.ticks = element_line(colour = "black", size = 1), axis.ticks.length = unit(.4, "cm"), strip.text = element_text(color = "black", size = 25),
-					 axis.title.y = element_text(margin = margin(t = 0, r = 20, b = 0, l = 0)), axis.title.x = element_text(margin = margin(t = 20, r = 0, b = 0, l = 0)),
-				   	 legend.position = "none", panel.background = element_blank()
-				   )
-
-		ggsave(filename = file.path(output, paste(file_path_sans_ext(basename(file)), ".png", sep = "")), plot = unscaled, width = 15)
-
 }
 }
-
 message("Status: Distribution Module plotting for ", sample, " is complete")
 warnings()
