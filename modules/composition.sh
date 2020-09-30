25ab45eeba8569781f4e630fdfdecfe09d742b6d
Alli Gombolay <agombolay3@gmail.com>
2020-09-30 17:31:23 -0400

    Update composition.sh

--- modules/composition.sh
+++ modules/composition.sh
@@ -13,7 +13,7 @@ rm -r $output; mkdir -p $output
 
 ######################################################################################################################################################
 
-for region in $unit "chromosomes"; do
+for region in $units "chromosomes"; do
 
 	#Get nucleotide for each genomic coordinate
 	bedtools getfasta -s -fi $fasta -bed $repository/results/$sample/coordinate$quality/${sample}-$region.coords.bed | grep -v '>' > $output/${sample}-$region.nucs.tab
