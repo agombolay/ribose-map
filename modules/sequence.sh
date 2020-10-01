205b5fa79c96c0744218d36b6c01c1e3b4f49a03
Alli Gombolay <agombolay3@gmail.com>
2020-09-30 23:00:16 -0400

    Update sequence.sh

--- modules/sequence.sh
+++ modules/sequence.sh
@@ -81,7 +81,7 @@ for region in $unit "chromosomes"; do
 			echo $U_RiboFreq | xargs printf "%.*f\n" 5 > $output/${sample}-Ribo$nuc.$region.U.txt
 				
 			#Create BED file for all ribonucleotides
-			cp $repository/results/$sample/coordinate$quality/${sample}-$region.counts.tab $output/${sample}-Ribo.$region.$nuc.bed
+			cp $repository/results/$sample/coordinate$quality/${sample}-$region.tab $output/${sample}-Ribo.$region.$nuc.bed
 		fi
 		
 #############################################################################################################################
