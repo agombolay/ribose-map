4759a646c447b368cc392ebd193d3e40b4287bf8
Alli Gombolay <agombolay3@gmail.com>
2020-10-23 21:16:00 -0400

    Update distribution.sh

--- modules/distribution.sh
+++ modules/distribution.sh
@@ -29,7 +29,7 @@ for region in $unit "chromosomes"; do
 	
 	done
 
-	if [[ $(wc -l < $repository/results/$sample/coordinate$quality/$sample.counts.tab) > 0 ]]; then
+	if [[ $(wc -l < $repository/results/$sample/coordinate$quality/$sample.tab) > 0 ]]; then
 
 		#Add trackline for forward strand to input into UCSC genome browser
 		echo "track type=bedGraph name="$sample-ForwardStrand" description="$sample-ForwardStrand" color=0,128,0 visibility=full" > $output/$sample-Forward.bg
@@ -38,10 +38,10 @@ for region in $unit "chromosomes"; do
 		echo "track type=bedGraph name="$sample-ReverseStrand" description="$sample-ReverseStrand" color=0,0,255 visibility=full" > $output/$sample-Reverse.bg
 		
 		#Rearrange forward strand file so format is the same as bedgraph format
-		awk -v "OFS=\t" '$4 == "+" {print $1, $2, $3, $5}' $repository/results/$sample/coordinate$quality/$sample.counts.tab >> $output/$sample-Forward.bg
+		awk -v "OFS=\t" '$4 == "+" {print $1, $2, $3, $5}' $repository/results/$sample/coordinate$quality/$sample.tab >> $output/$sample-Forward.bg
 
 		#Rearrange reverse strand file so format is the same as bedgraph format
-		awk -v "OFS=\t" '$4 == "-" {print $1, $2, $3, $5}' $repository/results/$sample/coordinate$quality/$sample.counts.tab >> $output/$sample-Reverse.bg
+		awk -v "OFS=\t" '$4 == "-" {print $1, $2, $3, $5}' $repository/results/$sample/coordinate$quality/$sample.tab >> $output/$sample-Reverse.bg
 
 	fi
 
