3180c7b334e8dfd2a70973127438f9222ff6b62a
Alli Gombolay <agombolay3@gmail.com>
2021-05-05 13:06:55 -0400

    Update subset.sh

--- modules/subset.sh
+++ modules/subset.sh
@@ -55,7 +55,7 @@ if [[ $units ]]; then
 		grep -w $region $repository/results/$sample/coordinate$quality/$sample.bed > $repository/results/$sample/coordinate$quality/${sample}-$region.bed
 		cut -f1,2,3,6 $repository/results/$sample/coordinate$quality/${sample}-$region.bed | uniq -c - | awk -v "OFS=\t" '{print $2, $3, $4, ".", ".", $5, $1}' - | sort -k7,7n - > $repository/results/$sample/coordinate$quality/${sample}-$region.Combined.tab
 		
-		#Create FASTA and FAI files for unit
+		#Create FASTA and FAI files for units
 		chr=$(awk '{print $1}' $(dirname $fasta)/$(basename $fasta .fa).chrom.sizes | grep -w $region -)
 
 		samtools faidx $fasta $chr > $repository/$(basename $fasta .fa)-$region.fa
