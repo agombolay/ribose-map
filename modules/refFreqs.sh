6960569ab95ab4c288bdc0768ab525bec795af2e
Alli Gombolay <agombolay3@gmail.com>
2022-02-21 21:48:27 -0500

    Update refFreqs.sh

--- modules/refFreqs.sh
+++ modules/refFreqs.sh
@@ -12,27 +12,30 @@
 
 for file in $(dirname $fasta)/*.fa; do
 
-	#region=$(basename $file .fa | cut -d "-" -f2)
-		
-	#Calculate counts of each nucleotide
-	A_Bkg=$(grep -v '>' $file | grep -Eo 'A|a' - | wc -l)
-	C_Bkg=$(grep -v '>' $file | grep -Eo 'C|c' - | wc -l)
-	G_Bkg=$(grep -v '>' $file | grep -Eo 'G|g' - | wc -l)
-	T_Bkg=$(grep -v '>' $file | grep -Eo 'T|t' - | wc -l)
+	if [ ! -f "$(dirname $fasta)/$(basename $file .fa).txt" ]; then
 	
-	#Calculate total number of nucleotides
-	BkgTotal=$(($A_Bkg + $C_Bkg + $G_Bkg + $T_Bkg))
+		#region=$(basename $file .fa | cut -d "-" -f2)
 		
-	#Calculate frequencies of each nucleotide
-	A_BkgFreq=$(echo "($A_Bkg + $T_Bkg)/($BkgTotal*2)" | bc -l)
-	C_BkgFreq=$(echo "($C_Bkg + $G_Bkg)/($BkgTotal*2)" | bc -l)
-	G_BkgFreq=$(echo "($G_Bkg + $C_Bkg)/($BkgTotal*2)" | bc -l)
-	T_BkgFreq=$(echo "($T_Bkg + $A_Bkg)/($BkgTotal*2)" | bc -l)
+		#Calculate counts of each nucleotide
+		A_Bkg=$(grep -v '>' $file | grep -Eo 'A|a' - | wc -l)
+		C_Bkg=$(grep -v '>' $file | grep -Eo 'C|c' - | wc -l)
+		G_Bkg=$(grep -v '>' $file | grep -Eo 'G|g' - | wc -l)
+		T_Bkg=$(grep -v '>' $file | grep -Eo 'T|t' - | wc -l)
+	
+		#Calculate total number of nucleotides
+		BkgTotal=$(($A_Bkg + $C_Bkg + $G_Bkg + $T_Bkg))
 		
-	#Save nucleotide frequencies to .txt file
-	paste <(echo -e "A") <(echo "$A_BkgFreq" | xargs printf "%.*f\n" 5) > $(dirname $fasta)/$(basename $file .fa).txt
-	paste <(echo -e "C") <(echo "$C_BkgFreq" | xargs printf "%.*f\n" 5) >> $(dirname $fasta)/$(basename $file .fa).txt
-	paste <(echo -e "G") <(echo "$G_BkgFreq" | xargs printf "%.*f\n" 5) >> $(dirname $fasta)/$(basename $file .fa).txt
-	paste <(echo -e "U") <(echo "$T_BkgFreq" | xargs printf "%.*f\n" 5) >> $(dirname $fasta)/$(basename $file .fa).txt
-
+		#Calculate frequencies of each nucleotide
+		A_BkgFreq=$(echo "($A_Bkg + $T_Bkg)/($BkgTotal*2)" | bc -l)
+		C_BkgFreq=$(echo "($C_Bkg + $G_Bkg)/($BkgTotal*2)" | bc -l)
+		G_BkgFreq=$(echo "($G_Bkg + $C_Bkg)/($BkgTotal*2)" | bc -l)
+		T_BkgFreq=$(echo "($T_Bkg + $A_Bkg)/($BkgTotal*2)" | bc -l)
+		
+		#Save nucleotide frequencies to .txt file
+		paste <(echo -e "A") <(echo "$A_BkgFreq" | xargs printf "%.*f\n" 5) > $(dirname $fasta)/$(basename $file .fa).txt
+		paste <(echo -e "C") <(echo "$C_BkgFreq" | xargs printf "%.*f\n" 5) >> $(dirname $fasta)/$(basename $file .fa).txt
+		paste <(echo -e "G") <(echo "$G_BkgFreq" | xargs printf "%.*f\n" 5) >> $(dirname $fasta)/$(basename $file .fa).txt
+		paste <(echo -e "U") <(echo "$T_BkgFreq" | xargs printf "%.*f\n" 5) >> $(dirname $fasta)/$(basename $file .fa).txt
+	
+	fi
 done
